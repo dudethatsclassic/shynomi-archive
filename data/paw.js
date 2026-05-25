c4a693145600c9c6b624a1dba69837ae7c3aaf9c
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:56 -0400

    Auto-export data: Paw

--- data/paw.js
+++ data/paw.js
@@ -2,54 +2,11 @@ const ARTIST_DATA = {
   "id": "paw",
   "name": "Paw",
   "drive": "E",
-  "showCount": 3,
-  "firstYear": 1993,
-  "lastYear": 1995,
-  "years": [
-    1993,
-    1995
-  ],
-  "shows": [
-    {
-      "year": 1993,
-      "month": 7,
-      "day": 11,
-      "dateStr": "7-11-1993",
-      "city": "New York",
-      "state": "NY",
-      "location": "New York, NY",
-      "venue": "CBGB",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sony PC62 \u00b7 george n."
-    },
-    {
-      "year": 1993,
-      "month": 8,
-      "day": 25,
-      "dateStr": "8-25-1993",
-      "city": "San Francisco",
-      "state": "CA",
-      "location": "San Francisco, CA",
-      "venue": "Slim's - ANA(M)",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "ECM-909 \u00b7 tjcruz"
-    },
-    {
-      "year": 1995,
-      "month": 11,
-      "day": 25,
-      "dateStr": "11-25-1995",
-      "city": "Brisbane",
-      "state": "Australia",
-      "location": "Brisbane, Australia",
-      "venue": "Livid Festival",
-      "version": "FM",
-      "format": "FLAC",
-      "notes": "Triple J Live at the Wireless Broadcast"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "label",
