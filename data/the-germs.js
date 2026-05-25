ed79919ac37867a87463b808d05ea823dc2cd6b9
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:15:21 -0400

    Auto-export data: The Germs

--- data/the-germs.js
+++ data/the-germs.js
@@ -2,67 +2,11 @@ const ARTIST_DATA = {
   "id": "the-germs",
   "name": "The Germs",
   "drive": "C",
-  "showCount": 4,
-  "firstYear": 1979,
-  "lastYear": 1980,
-  "years": [
-    1979,
-    1980
-  ],
-  "shows": [
-    {
-      "year": 1979,
-      "month": 9,
-      "day": 22,
-      "dateStr": "9-22-1979",
-      "city": "Hollywood",
-      "state": "CA",
-      "location": "Hollywood , CA",
-      "venue": "Hong Kong Cafe",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "djm5000"
-    },
-    {
-      "year": 1980,
-      "month": 3,
-      "day": 13,
-      "dateStr": "3-13-1980",
-      "city": "Redondo Beach",
-      "state": "CA",
-      "location": "Redondo Beach, CA",
-      "venue": "Club Fleetwood",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": ""
-    },
-    {
-      "year": 1980,
-      "month": 5,
-      "day": 6,
-      "dateStr": "5-6-1980",
-      "city": "Redondo Beach",
-      "state": "CA",
-      "location": "Redondo Beach, CA",
-      "venue": "Club Fleetwood",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": ""
-    },
-    {
-      "year": 1980,
-      "month": 12,
-      "day": 3,
-      "dateStr": "12-3-1980",
-      "city": "West Hollywood",
-      "state": "CA",
-      "location": "West Hollywood, CA",
-      "venue": "The Starwood",
-      "version": "Silver CD",
-      "format": "FLAC",
-      "notes": "Rhino RHM2 524568"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
