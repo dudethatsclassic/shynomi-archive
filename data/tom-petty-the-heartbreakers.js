4d1069735f1e6c8a9d36c316b3df15e607ec7c8a
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:30 -0400

    Auto-export data: Tom Petty & the Heartbreakers

---
+++ data/tom-petty-the-heartbreakers.js
@@ -0,0 +1,48 @@
+const ARTIST_DATA = {
+  "id": "tom-petty-the-heartbreakers",
+  "name": "Tom Petty & the Heartbreakers",
+  "drive": "F",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "release",
+      "label": "Running Down A Dream (DVD)",
+      "version": "Official"
+    },
+    {
+      "type": "label",
+      "label": "The Old Grey Whistle Test (June, 1978)"
+    },
+    {
+      "type": "release",
+      "label": "Tom Petty - VH1 Documentaries (1995)",
+      "version": "TV"
+    },
+    {
+      "type": "release",
+      "label": "Tom Petty - The Bootleg Series Vol. 1-3 (Live 1976-1981)",
+      "version": "Compilation"
+    },
+    {
+      "type": "release",
+      "label": "Tom Petty - The Bootleg Series Vol. 4-8 (Live 1982-1993)",
+      "version": "Compilation"
+    },
+    {
+      "type": "label",
+      "label": "XX-XX-1981 - \"Hard Promises\" Demos"
+    },
+    {
+      "type": "label",
+      "label": "Tom Petty - Somewhere You Feel Free (Documentary) (2021)"
+    },
+    {
+      "type": "label",
+      "label": "Tom Petty - Insomnia (1981 Outtake)"
+    }
+  ]
+};
