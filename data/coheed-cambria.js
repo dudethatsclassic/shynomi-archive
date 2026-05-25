e1b2dc86241db7d7ac6cdcf4a8a2a552cbef2dfd
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:14 -0400

    Auto-export data: Coheed & Cambria

---
+++ data/coheed-cambria.js
@@ -0,0 +1,18 @@
+const ARTIST_DATA = {
+  "id": "coheed-cambria",
+  "name": "Coheed & Cambria",
+  "drive": "C",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "release",
+      "label": "Live at the Starland Ballroom (DVD)",
+      "version": "Official",
+      "format": "DVD"
+    }
+  ]
+};
