77d53bf9a681cac937963bc46a84c121ec1adead
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:45:26 -0400

    Auto-export data: Kevin Martin & The Hiwatts

---
+++ data/kevin-martin-the-hiwatts.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "kevin-martin-the-hiwatts",
+  "name": "Kevin Martin & The Hiwatts",
+  "drive": "B",
+  "showCount": 1,
+  "firstYear": 2004,
+  "lastYear": 2004,
+  "years": [
+    2004
+  ],
+  "shows": [
+    {
+      "year": 2004,
+      "month": 10,
+      "day": 26,
+      "dateStr": "10-26-2004",
+      "city": "Cologne",
+      "state": "Germany",
+      "location": "Cologne, Germany",
+      "venue": "Prime Club",
+      "notes": "unk · kevdogg transfer",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
