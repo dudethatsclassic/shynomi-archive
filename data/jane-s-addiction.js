0572f15827d559792daa0a41e0c212a7e710b2ed
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:15:43 -0400

    Auto-export data: Jane's Addiction

---
+++ data/jane-s-addiction.js
@@ -0,0 +1,18 @@
+const ARTIST_DATA = {
+  "id": "jane-s-addiction",
+  "name": "Jane's Addiction",
+  "drive": "C",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "release",
+      "label": "Jane's Addiction - Skin And Bones (YTH-081)",
+      "version": "Silver CD",
+      "format": "FLAC"
+    }
+  ]
+};
