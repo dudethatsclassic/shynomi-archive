141e137f929037ad4c62f94f98b92599e4ab630c
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:15:22 -0400

    Auto-export data: Scream

---
+++ data/scream.js
@@ -0,0 +1,18 @@
+const ARTIST_DATA = {
+  "id": "scream",
+  "name": "Scream",
+  "drive": "C",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "release",
+      "label": "Scream - Still Screaming (LP, Dischord, Dischord 9, US) (1983)",
+      "version": "Vinyl",
+      "format": "FLAC"
+    }
+  ]
+};
