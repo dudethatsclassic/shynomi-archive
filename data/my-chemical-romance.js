e2148d300a97ff01d1998312332ddd1745246003
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:31 -0400

    Auto-export data: My Chemical Romance

--- data/my-chemical-romance.js
+++ data/my-chemical-romance.js
@@ -2,55 +2,11 @@ const ARTIST_DATA = {
   "id": "my-chemical-romance",
   "name": "My Chemical Romance",
   "drive": "A",
-  "showCount": 3,
-  "firstYear": 2007,
-  "lastYear": 2022,
-  "years": [
-    2007,
-    2011,
-    2022
-  ],
-  "shows": [
-    {
-      "year": 2007,
-      "month": 7,
-      "day": 6,
-      "dateStr": "7-6-2007",
-      "city": "Roskilde",
-      "state": "Denmark",
-      "location": "Roskilde, Denmark",
-      "venue": "Roskilde Festival - Arena",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "CSC \u00b7 Unknown"
-    },
-    {
-      "year": 2011,
-      "month": 7,
-      "day": 4,
-      "dateStr": "7-4-2011",
-      "city": "Roskilde",
-      "state": "Denmark",
-      "location": "Roskilde, Denmark",
-      "venue": "Roskilde Festival - Orange Stage",
-      "version": "AUD",
-      "format": "",
-      "notes": "CSC \u00b7 jtl"
-    },
-    {
-      "year": 2022,
-      "month": 10,
-      "day": 17,
-      "dateStr": "10-17-2022",
-      "city": "Inglewood",
-      "state": "CA",
-      "location": "Inglewood, CA",
-      "venue": "Kia Forum",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "SP-CMC-4U \u00b7 Unknown"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
