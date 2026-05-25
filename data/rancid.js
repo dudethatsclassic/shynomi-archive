77bf8ab84cef684e92c8435e9c803771ec9e5e71
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:58 -0400

    Auto-export data: Rancid

--- data/rancid.js
+++ data/rancid.js
@@ -2,55 +2,11 @@ const ARTIST_DATA = {
   "id": "rancid",
   "name": "Rancid",
   "drive": "D",
-  "showCount": 3,
-  "firstYear": 1994,
-  "lastYear": 1996,
-  "years": [
-    1994,
-    1995,
-    1996
-  ],
-  "shows": [
-    {
-      "year": 1994,
-      "month": 9,
-      "day": 24,
-      "dateStr": "9-24-1994",
-      "city": "New York",
-      "state": "NY",
-      "location": "New York, NY",
-      "venue": "Wetlands Preserve - ANA",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "x \u00b7 Unknown"
-    },
-    {
-      "year": 1995,
-      "month": 10,
-      "day": 11,
-      "dateStr": "10-11-1995",
-      "city": "Seattle",
-      "state": "WA",
-      "location": "Seattle, WA",
-      "venue": "DV8 - ANA",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "x \u00b7 Unknown"
-    },
-    {
-      "year": 1996,
-      "month": 3,
-      "day": 27,
-      "dateStr": "3-27-1996",
-      "city": "Asbury Park",
-      "state": "NJ",
-      "location": "Asbury Park, NJ",
-      "venue": "The Stone Pony",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sony D-6C \u00b7 Unknown"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
