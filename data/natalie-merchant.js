f348d60432c5440fe59d8554de520c85d83714c4
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:33 -0400

    Auto-export data: Natalie Merchant

--- data/natalie-merchant.js
+++ data/natalie-merchant.js
@@ -2,55 +2,11 @@ const ARTIST_DATA = {
   "id": "natalie-merchant",
   "name": "Natalie Merchant",
   "drive": "D",
-  "showCount": 3,
-  "firstYear": 1996,
-  "lastYear": 2001,
-  "years": [
-    1996,
-    1999,
-    2001
-  ],
-  "shows": [
-    {
-      "year": 1996,
-      "month": 7,
-      "day": 7,
-      "dateStr": "7-7-1996",
-      "city": "Milwaukee",
-      "state": "WI",
-      "location": "Milwaukee, WI",
-      "venue": "Marcus Amphitheater",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Schoeps MK4 \u00b7 Unknown"
-    },
-    {
-      "year": 1999,
-      "month": 2,
-      "day": 25,
-      "dateStr": "2-25-1999",
-      "city": "Portland",
-      "state": "ME",
-      "location": "Portland, ME",
-      "venue": "Cumberland County Civic Center - DAT",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "x \u00b7 Unknown"
-    },
-    {
-      "year": 2001,
-      "month": 11,
-      "day": 8,
-      "dateStr": "11-8-2001",
-      "city": "New York",
-      "state": "NY",
-      "location": "New York, NY",
-      "venue": "Beacon Theatre",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Neumann AK40s \u00b7 Unknown"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
