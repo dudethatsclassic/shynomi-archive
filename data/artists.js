fdda4791f2bbcd8898e19fb1c69436ac20eb156d
dudethatsclassic <natescottsmith@icloud.com>
2026-05-24 20:50:43 -0400

    Add files via upload

---
+++ data/artists.js
@@ -0,0 +1,618 @@
+const ARTISTS = [
+  {
+    "id": "",
+    "name": "",
+    "showCount": 349,
+    "firstYear": 1989,
+    "lastYear": 2025
+  },
+  {
+    "id": "311",
+    "name": "311",
+    "showCount": 54,
+    "firstYear": 1991,
+    "lastYear": 2026
+  },
+  {
+    "id": "a-perfect-circle",
+    "name": "A Perfect Circle",
+    "showCount": 366,
+    "firstYear": 1999,
+    "lastYear": 2024
+  },
+  {
+    "id": "aerosmith",
+    "name": "Aerosmith",
+    "showCount": 17,
+    "firstYear": 1976,
+    "lastYear": 2019
+  },
+  {
+    "id": "alanis-morissette",
+    "name": "Alanis Morissette",
+    "showCount": 69,
+    "firstYear": 1995,
+    "lastYear": 2025
+  },
+  {
+    "id": "alice-in-chains",
+    "name": "Alice In Chains",
+    "showCount": 263,
+    "firstYear": 1987,
+    "lastYear": 2025
+  },
+  {
+    "id": "aphex-twin",
+    "name": "Aphex Twin",
+    "showCount": 17,
+    "firstYear": 1993,
+    "lastYear": 2019
+  },
+  {
+    "id": "bands-of-the-90s",
+    "name": "Bands of the 90s",
+    "showCount": 791,
+    "firstYear": 1980,
+    "lastYear": 2026
+  },
+  {
+    "id": "beastie-boys",
+    "name": "Beastie Boys",
+    "showCount": 63,
+    "firstYear": 1985,
+    "lastYear": 2009
+  },
+  {
+    "id": "beck",
+    "name": "Beck",
+    "showCount": 309,
+    "firstYear": 1993,
+    "lastYear": 2025
+  },
+  {
+    "id": "bjork",
+    "name": "Bjork",
+    "showCount": 48,
+    "firstYear": 1988,
+    "lastYear": 2022
+  },
+  {
+    "id": "black-sabbath",
+    "name": "Black Sabbath",
+    "showCount": 17,
+    "firstYear": 1969,
+    "lastYear": 2025
+  },
+  {
+    "id": "blind-melon",
+    "name": "Blind Melon",
+    "showCount": 45,
+    "firstYear": 1992,
+    "lastYear": 2013
+  },
+  {
+    "id": "bob-dylan",
+    "name": "Bob Dylan",
+    "showCount": 26,
+    "firstYear": 1962,
+    "lastYear": 2019
+  },
+  {
+    "id": "bush",
+    "name": "Bush",
+    "showCount": 93,
+    "firstYear": 1995,
+    "lastYear": 2025
+  },
+  {
+    "id": "country-various",
+    "name": "COUNTRY (Various)",
+    "showCount": 109,
+    "firstYear": 1968,
+    "lastYear": 2026
+  },
+  {
+    "id": "cake",
+    "name": "Cake",
+    "showCount": 29,
+    "firstYear": 1994,
+    "lastYear": 2025
+  },
+  {
+    "id": "candlebox",
+    "name": "Candlebox",
+    "showCount": 96,
+    "firstYear": 1994,
+    "lastYear": 2025
+  },
+  {
+    "id": "charles-mingus",
+    "name": "Charles Mingus",
+    "showCount": 33,
+    "firstYear": 1964,
+    "lastYear": 2003
+  },
+  {
+    "id": "chick-corea",
+    "name": "Chick Corea",
+    "showCount": 40,
+    "firstYear": 1978,
+    "lastYear": 1994
+  },
+  {
+    "id": "coldplay",
+    "name": "Coldplay",
+    "showCount": 747,
+    "firstYear": 1999,
+    "lastYear": 2025
+  },
+  {
+    "id": "counting-crows",
+    "name": "Counting Crows",
+    "showCount": 1729,
+    "firstYear": 1992,
+    "lastYear": 2025
+  },
+  {
+    "id": "dave-matthews-band",
+    "name": "Dave Matthews Band",
+    "showCount": 321,
+    "firstYear": 1991,
+    "lastYear": 2025
+  },
+  {
+    "id": "death-cab-for-cutie",
+    "name": "Death Cab For Cutie",
+    "showCount": 178,
+    "firstYear": 1998,
+    "lastYear": 2024
+  },
+  {
+    "id": "deftones",
+    "name": "Deftones",
+    "showCount": 300,
+    "firstYear": 1992,
+    "lastYear": 2026
+  },
+  {
+    "id": "eels",
+    "name": "Eels",
+    "showCount": 53,
+    "firstYear": 1994,
+    "lastYear": 2023
+  },
+  {
+    "id": "failure",
+    "name": "Failure",
+    "showCount": 38,
+    "firstYear": 1994,
+    "lastYear": 2026
+  },
+  {
+    "id": "faith-no-more",
+    "name": "Faith No More",
+    "showCount": 183,
+    "firstYear": 1983,
+    "lastYear": 2015
+  },
+  {
+    "id": "foo-fighters",
+    "name": "Foo Fighters",
+    "showCount": 1081,
+    "firstYear": 1979,
+    "lastYear": 2026
+  },
+  {
+    "id": "fountains-of-wayne",
+    "name": "Fountains of Wayne",
+    "showCount": 37,
+    "firstYear": 1997,
+    "lastYear": 2012
+  },
+  {
+    "id": "garbage",
+    "name": "Garbage",
+    "showCount": 443,
+    "firstYear": 1995,
+    "lastYear": 2025
+  },
+  {
+    "id": "gin-blossoms",
+    "name": "Gin Blossoms",
+    "showCount": 64,
+    "firstYear": 1993,
+    "lastYear": 2025
+  },
+  {
+    "id": "guster",
+    "name": "Guster",
+    "showCount": 832,
+    "firstYear": 1992,
+    "lastYear": 2025
+  },
+  {
+    "id": "hip-hop",
+    "name": "Hip-Hop",
+    "showCount": 53,
+    "firstYear": 1985,
+    "lastYear": 2025
+  },
+  {
+    "id": "hole",
+    "name": "Hole",
+    "showCount": 143,
+    "firstYear": 1989,
+    "lastYear": 2011
+  },
+  {
+    "id": "hum",
+    "name": "Hum",
+    "showCount": 56,
+    "firstYear": 1994,
+    "lastYear": 2017
+  },
+  {
+    "id": "incubus",
+    "name": "Incubus",
+    "showCount": 170,
+    "firstYear": 1995,
+    "lastYear": 2025
+  },
+  {
+    "id": "janes-addiction",
+    "name": "Jane's Addiction",
+    "showCount": 310,
+    "firstYear": 1986,
+    "lastYear": 2024
+  },
+  {
+    "id": "jimi-hendrix",
+    "name": "Jimi Hendrix",
+    "showCount": 229,
+    "firstYear": 1965,
+    "lastYear": 2010
+  },
+  {
+    "id": "john-mayer",
+    "name": "John Mayer",
+    "showCount": 375,
+    "firstYear": 1999,
+    "lastYear": 2025
+  },
+  {
+    "id": "korn",
+    "name": "Korn",
+    "showCount": 54,
+    "firstYear": 1995,
+    "lastYear": 2025
+  },
+  {
+    "id": "linkin-park",
+    "name": "Linkin Park",
+    "showCount": 170,
+    "firstYear": 2000,
+    "lastYear": 2026
+  },
+  {
+    "id": "local-h",
+    "name": "Local H",
+    "showCount": 550,
+    "firstYear": 1991,
+    "lastYear": 2026
+  },
+  {
+    "id": "metallica",
+    "name": "Metallica",
+    "showCount": 2256,
+    "firstYear": 1982,
+    "lastYear": 2025
+  },
+  {
+    "id": "miles-davis",
+    "name": "Miles Davis",
+    "showCount": 158,
+    "firstYear": 1946,
+    "lastYear": 1991
+  },
+  {
+    "id": "my-bloody-valentine",
+    "name": "My Bloody Valentine",
+    "showCount": 154,
+    "firstYear": 1986,
+    "lastYear": 2026
+  },
+  {
+    "id": "nine-inch-nails",
+    "name": "Nine Inch Nails",
+    "showCount": 1010,
+    "firstYear": 1988,
+    "lastYear": 2990
+  },
+  {
+    "id": "nirvana",
+    "name": "Nirvana",
+    "showCount": 1236,
+    "firstYear": 1969,
+    "lastYear": 2025
+  },
+  {
+    "id": "oasis",
+    "name": "Oasis",
+    "showCount": 786,
+    "firstYear": 1992,
+    "lastYear": 2025
+  },
+  {
+    "id": "our-lady-peace",
+    "name": "Our Lady Peace",
+    "showCount": 56,
+    "firstYear": 1995,
+    "lastYear": 2026
+  },
+  {
+    "id": "punk-various",
+    "name": "PUNK (Various)",
+    "showCount": 69,
+    "firstYear": 1976,
+    "lastYear": 1996
+  },
+  {
+    "id": "pavement",
+    "name": "Pavement",
+    "showCount": 132,
+    "firstYear": 1989,
+    "lastYear": 2024
+  },
+  {
+    "id": "pearl-jam",
+    "name": "Pearl Jam",
+    "showCount": 2139,
+    "firstYear": 1984,
+    "lastYear": 2025
+  },
+  {
+    "id": "primus",
+    "name": "Primus",
+    "showCount": 970,
+    "firstYear": 1987,
+    "lastYear": 2025
+  },
+  {
+    "id": "queens-of-the-stone-age",
+    "name": "Queens of the Stone Age",
+    "showCount": 63,
+    "firstYear": 1999,
+    "lastYear": 2024
+  },
+  {
+    "id": "rem",
+    "name": "R.E.M.",
+    "showCount": 803,
+    "firstYear": 1980,
+    "lastYear": 2989
+  },
+  {
+    "id": "radiohead",
+    "name": "Radiohead",
+    "showCount": 1330,
+    "firstYear": 1992,
+    "lastYear": 2025
+  },
+  {
+    "id": "rage-against-the-machine",
+    "name": "Rage Against the Machine",
+    "showCount": 242,
+    "firstYear": 1991,
+    "lastYear": 2025
+  },
+  {
+    "id": "ray-charles",
+    "name": "Ray Charles",
+    "showCount": 20,
+    "firstYear": 1953,
+    "lastYear": 2002
+  },
+  {
+    "id": "red-hot-chili-peppers",
+    "name": "Red Hot Chili Peppers",
+    "showCount": 1195,
+    "firstYear": 1983,
+    "lastYear": 2025
+  },
+  {
+    "id": "ryan-adams",
+    "name": "Ryan Adams",
+    "showCount": 699,
+    "firstYear": 1994,
+    "lastYear": 2025
+  },
+  {
+    "id": "shoegaze-various",
+    "name": "SHOEGAZE (Various)",
+    "showCount": 323,
+    "firstYear": 1982,
+    "lastYear": 2025
+  },
+  {
+    "id": "sigur-ros",
+    "name": "Sigur Ros",
+    "showCount": 322,
+    "firstYear": 1999,
+    "lastYear": 2025
+  },
+  {
+    "id": "silverchair",
+    "name": "Silverchair",
+    "showCount": 89,
+    "firstYear": 1995,
+    "lastYear": 2007
+  },
+  {
+    "id": "slipknot",
+    "name": "Slipknot",
+    "showCount": 56,
+    "firstYear": 1998,
+    "lastYear": 2025
+  },
+  {
+    "id": "smashing-pumpkins",
+    "name": "Smashing Pumpkins",
+    "showCount": 2103,
+    "firstYear": 1988,
+    "lastYear": 2025
+  },
+  {
+    "id": "social-distortion",
+    "name": "Social Distortion",
+    "showCount": 90,
+    "firstYear": 1981,
+    "lastYear": 2025
+  },
+  {
+    "id": "sonic-youth",
+    "name": "Sonic Youth",
+    "showCount": 297,
+    "firstYear": 1981,
+    "lastYear": 2011
+  },
+  {
+    "id": "soundgarden",
+    "name": "Soundgarden",
+    "showCount": 496,
+    "firstYear": 1988,
+    "lastYear": 2022
+  },
+  {
+    "id": "stone-temple-pilots",
+    "name": "Stone Temple Pilots",
+    "showCount": 94,
+    "firstYear": 1993,
+    "lastYear": 2025
+  },
+  {
+    "id": "sufjan-stevens",
+    "name": "Sufjan Stevens",
+    "showCount": 36,
+    "firstYear": 2004,
+    "lastYear": 2018
+  },
+  {
+    "id": "the-beatles",
+    "name": "The Beatles",
+    "showCount": 16,
+    "firstYear": 1963,
+    "lastYear": 1969
+  },
+  {
+    "id": "the-black-crowes",
+    "name": "The Black Crowes",
+    "showCount": 289,
+    "firstYear": 1990,
+    "lastYear": 2026
+  },
+  {
+    "id": "the-cranberries",
+    "name": "The Cranberries",
+    "showCount": 56,
+    "firstYear": 1991,
+    "lastYear": 2012
+  },
+  {
+    "id": "the-flaming-lips",
+    "name": "The Flaming Lips",
+    "showCount": 223,
+    "firstYear": 1984,
+    "lastYear": 2992
+  },
+  {
+    "id": "the-jayhawks",
+    "name": "The Jayhawks",
+    "showCount": 35,
+    "firstYear": 1985,
+    "lastYear": 2025
+  },
+  {
+    "id": "the-offspring",
+    "name": "The Offspring",
+    "showCount": 99,
+    "firstYear": 1991,
+    "lastYear": 2025
+  },
+  {
+    "id": "the-presidents-of-the-usa",
+    "name": "The Presidents of the U.S.A.",
+    "showCount": 52,
+    "firstYear": 1994,
+    "lastYear": 2014
+  },
+  {
+    "id": "the-shins",
+    "name": "The Shins",
+    "showCount": 32,
+    "firstYear": 2000,
+    "lastYear": 2017
+  },
+  {
+    "id": "the-wallflowers",
+    "name": "The Wallflowers",
+    "showCount": 41,
+    "firstYear": 1992,
+    "lastYear": 2025
+  },
+  {
+    "id": "thelonious-monk",
+    "name": "Thelonious Monk",
+    "showCount": 33,
+    "firstYear": 1955,
+    "lastYear": 1975
+  },
+  {
+    "id": "toad-the-wet-sprocket",
+    "name": "Toad the Wet Sprocket",
+    "showCount": 334,
+    "firstYear": 1986,
+    "lastYear": 2025
+  },
+  {
+    "id": "tom-petty--the-heartbreakers",
+    "name": "Tom Petty & the Heartbreakers",
+    "showCount": 324,
+    "firstYear": 1971,
+    "lastYear": 2017
+  },
+  {
+    "id": "tool",
+    "name": "Tool",
+    "showCount": 1392,
+    "firstYear": 1991,
+    "lastYear": 2025
+  },
+  {
+    "id": "tori-amos",
+    "name": "Tori Amos",
+    "showCount": 113,
+    "firstYear": 1991,
+    "lastYear": 2022
+  },
+  {
+    "id": "various-artists",
+    "name": "Various Artists",
+    "showCount": 1792,
+    "firstYear": 1949,
+    "lastYear": 2026
+  },
+  {
+    "id": "weezer",
+    "name": "Weezer",
+    "showCount": 337,
+    "firstYear": 1993,
+    "lastYear": 2025
+  },
+  {
+    "id": "wilco",
+    "name": "Wilco",
+    "showCount": 1603,
+    "firstYear": 1988,
+    "lastYear": 2026
+  }
+];
