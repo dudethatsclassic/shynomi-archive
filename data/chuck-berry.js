4a936248225a58843ecec2742ef23c0747f37dba
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:13 -0400

    Auto-export data: Chuck Berry

--- data/chuck-berry.js
+++ data/chuck-berry.js
@@ -2,27 +2,11 @@ const ARTIST_DATA = {
   "id": "chuck-berry",
   "name": "Chuck Berry",
   "drive": "D",
-  "showCount": 1,
-  "firstYear": 1982,
-  "lastYear": 1982,
-  "years": [
-    1982
-  ],
-  "shows": [
-    {
-      "year": 1982,
-      "month": 9,
-      "day": 16,
-      "dateStr": "9-16-1982",
-      "city": "Albuquerque",
-      "state": "NM",
-      "location": "Albuquerque, NM",
-      "venue": "State Fairgrounds",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Aiwa HS-J1 \u00b7 langdon"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
