6f11af5f5b9fa92ca47a8d41a0929866bd5f4bdd
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:49 -0400

    Auto-export data: Elastica

--- data/elastica.js
+++ data/elastica.js
@@ -2,54 +2,11 @@ const ARTIST_DATA = {
   "id": "elastica",
   "name": "Elastica",
   "drive": "E",
-  "showCount": 3,
-  "firstYear": 1995,
-  "lastYear": 1999,
-  "years": [
-    1995,
-    1999
-  ],
-  "shows": [
-    {
-      "year": 1995,
-      "month": 3,
-      "day": 17,
-      "dateStr": "3-17-1995",
-      "city": "Austin",
-      "state": "TX",
-      "location": "Austin, TX",
-      "venue": "Liberty Lunch SXSW Festival",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "MarcSounds \u00b7 markp"
-    },
-    {
-      "year": 1995,
-      "month": 6,
-      "day": 25,
-      "dateStr": "6-25-1995",
-      "city": "Glastonbury",
-      "state": "UK",
-      "location": "Glastonbury, UK",
-      "venue": "Glastonbury Festival",
-      "version": "FM",
-      "format": "M4A",
-      "notes": "BBC Radio 6 \u00b7 pawel"
-    },
-    {
-      "year": 1999,
-      "month": 8,
-      "day": 27,
-      "dateStr": "8-27-1999",
-      "city": "Caversham",
-      "state": "UK",
-      "location": "Caversham, UK",
-      "venue": "Reading Festival",
-      "version": "FM",
-      "format": "M4A",
-      "notes": "pawel"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
