798f9a9db44dbb05cc30306ddd97d0caea6668fe
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:08 -0400

    Auto-export data: Arlo Guthrie

--- data/arlo-guthrie.js
+++ data/arlo-guthrie.js
@@ -2,27 +2,11 @@ const ARTIST_DATA = {
   "id": "arlo-guthrie",
   "name": "Arlo Guthrie",
   "drive": "D",
-  "showCount": 1,
-  "firstYear": 2017,
-  "lastYear": 2017,
-  "years": [
-    2017
-  ],
-  "shows": [
-    {
-      "year": 2017,
-      "month": 7,
-      "day": 29,
-      "dateStr": "7-29-2017",
-      "city": "Woonsocket",
-      "state": "RI",
-      "location": "Woonsocket, RI",
-      "venue": "Stadium Theater Performing Arts Center",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": ""
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
