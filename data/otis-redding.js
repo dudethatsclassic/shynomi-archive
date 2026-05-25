9b551f182ccd8b650be7a038767c188bee231411
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:34 -0400

    Auto-export data: Otis Redding

--- data/otis-redding.js
+++ data/otis-redding.js
@@ -2,41 +2,11 @@ const ARTIST_DATA = {
   "id": "otis-redding",
   "name": "Otis Redding",
   "drive": "C",
-  "showCount": 2,
-  "firstYear": 1966,
-  "lastYear": 1967,
-  "years": [
-    1966,
-    1967
-  ],
-  "shows": [
-    {
-      "year": 1966,
-      "month": 4,
-      "day": 10,
-      "dateStr": "4-10-1966",
-      "city": "West Hollywood",
-      "state": "CA",
-      "location": "West Hollywood, CA",
-      "venue": "Whisky A Go-Go",
-      "version": "SBD",
-      "format": "FLAC",
-      "notes": ""
-    },
-    {
-      "year": 1967,
-      "month": 6,
-      "day": 4,
-      "dateStr": "6-4-1967",
-      "city": "Stockholm",
-      "state": "Sweden",
-      "location": "Stockholm, Sweden",
-      "venue": "Konserthusett",
-      "version": "SBD",
-      "format": "FLAC",
-      "notes": ""
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "release",
