31de09b6c1afefebb0c5b332c193b7b20b454f7e
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:51 -0400

    Auto-export data: For Squirrels

---
+++ data/for-squirrels.js
@@ -0,0 +1,17 @@
+const ARTIST_DATA = {
+  "id": "for-squirrels",
+  "name": "For Squirrels",
+  "drive": "F",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "label",
+      "label": "For Squirrels - Milk From A Fountain [Silver]",
+      "format": "FLAC"
+    }
+  ]
+};
