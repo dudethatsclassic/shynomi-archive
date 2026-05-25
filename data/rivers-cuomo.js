61d80dcce97f515b29b74a496c94c510833fec7b
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:38 -0400

    Auto-export data: Rivers Cuomo

--- data/rivers-cuomo.js
+++ data/rivers-cuomo.js
@@ -2,27 +2,11 @@ const ARTIST_DATA = {
   "id": "rivers-cuomo",
   "name": "Rivers Cuomo",
   "drive": "C",
-  "showCount": 1,
-  "firstYear": 2018,
-  "lastYear": 2018,
-  "years": [
-    2018
-  ],
-  "shows": [
-    {
-      "year": 2018,
-      "month": 5,
-      "day": 30,
-      "dateStr": "5-30-2018",
-      "city": "Los Angeles",
-      "state": "CA",
-      "location": "Los Angeles, CA",
-      "venue": "The Troubador",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sonics · jb"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "label",
