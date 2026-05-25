e2a9d3c2c79419560683bd00c556411a3558408f
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:16:28 -0400

    Auto-export data: Neil Young & Pearl Jam

---
+++ data/neil-young-pearl-jam.js
@@ -0,0 +1,23 @@
+const ARTIST_DATA = {
+  "id": "neil-young-pearl-jam",
+  "name": "Neil Young & Pearl Jam",
+  "drive": "D",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "release",
+      "label": "The European Concerts (3CD)",
+      "version": "Silver CD",
+      "format": "FLAC"
+    },
+    {
+      "type": "label",
+      "label": "Mirror Ball Tour Compilation (a.v.)",
+      "format": "FLAC"
+    }
+  ]
+};
