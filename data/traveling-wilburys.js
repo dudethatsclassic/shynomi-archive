eefcfc2fe4b7cef00556ec64b4a1424592c4d95c
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:31 -0400

    Auto-export data: Traveling Wilburys

---
+++ data/traveling-wilburys.js
@@ -0,0 +1,29 @@
+const ARTIST_DATA = {
+  "id": "traveling-wilburys",
+  "name": "Traveling Wilburys",
+  "drive": "B",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "release",
+      "label": "Traveling Wilburys - Recovered Treasures",
+      "version": "SBD"
+    },
+    {
+      "type": "label",
+      "label": "Traveling Wilburys - Family Tree (Volume I)"
+    },
+    {
+      "type": "label",
+      "label": "Traveling Wilburys - Family Tree (Volume II)"
+    },
+    {
+      "type": "label",
+      "label": "Traveling Wilburys - The Unreleased Masters (Captain Acid Remaster)"
+    }
+  ]
+};
