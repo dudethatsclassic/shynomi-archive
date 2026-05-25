085304cf8337d36a2bb40f9d52625a96bb628deb
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:17:39 -0400

    Auto-export data: Pat Wilson

--- data/pat-wilson.js
+++ data/pat-wilson.js
@@ -2,27 +2,11 @@ const ARTIST_DATA = {
   "id": "pat-wilson",
   "name": "Pat Wilson",
   "drive": "C",
-  "showCount": 1,
-  "firstYear": 2002,
-  "lastYear": 2002,
-  "years": [
-    2002
-  ],
-  "shows": [
-    {
-      "year": 2002,
-      "month": 7,
-      "day": 27,
-      "dateStr": "7-27-2002",
-      "city": "Columbia",
-      "state": "MD",
-      "location": "Columbia, MD",
-      "venue": "Merriweather Post Pavilion",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Electret"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "label",
