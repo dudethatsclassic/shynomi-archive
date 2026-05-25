0334ecbf579c9dbe9711569dd822b5617b5d985f
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:16:30 -0400

    Auto-export data: The Presidents of the U.S.A.

---
+++ data/the-presidents-of-the-u-s-a.js
@@ -0,0 +1,29 @@
+const ARTIST_DATA = {
+  "id": "the-presidents-of-the-u-s-a",
+  "name": "The Presidents of the U.S.A.",
+  "drive": "C",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "release",
+      "label": "No Strings Attached (1995-12-17)",
+      "version": "Silver CD",
+      "format": "FLAC"
+    },
+    {
+      "type": "label",
+      "label": "Various Demos, Etc.",
+      "format": "MP3"
+    },
+    {
+      "type": "release",
+      "label": "Froggystyle EP",
+      "version": "SBD",
+      "format": "FLAC"
+    }
+  ]
+};
