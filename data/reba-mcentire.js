0e62a8d9496401b8f9d228174792be36e1ce8f6b
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:46:06 -0400

    Auto-export data: Reba McEntire

--- data/reba-mcentire.js
+++ data/reba-mcentire.js
@@ -2,13 +2,27 @@ const ARTIST_DATA = {
   "id": "reba-mcentire",
   "name": "Reba McEntire",
   "drive": "C",
-  "showCount": 1,
-  "firstYear": 2002,
+  "showCount": 2,
+  "firstYear": 1985,
   "lastYear": 2002,
   "years": [
+    1985,
     2002
   ],
   "shows": [
+    {
+      "year": 1985,
+      "month": null,
+      "day": null,
+      "dateStr": "XX-XX-1985",
+      "city": "Pasadena",
+      "state": "TX",
+      "location": "Pasadena, TX",
+      "venue": "Gilley's",
+      "notes": "85-46",
+      "version": "preFM",
+      "format": "FLAC"
+    },
     {
       "year": 2002,
       "month": 8,
@@ -18,9 +32,10 @@ const ARTIST_DATA = {
       "state": "IL",
       "location": "Rosemont, IL",
       "venue": "Allstate Arena",
+      "notes": "Sony MZ-NF610 · roy martin",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sony MZ-NF610 \u00b7 roy martin"
+      "format": "FLAC"
     }
-  ]
+  ],
+  "misc": []
 };
