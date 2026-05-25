a0af3ed3e566f3f107507e09450c8bc93028e2eb
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:38 -0400

    Auto-export data: Sheryl Crow

--- data/sheryl-crow.js
+++ data/sheryl-crow.js
@@ -2,69 +2,11 @@ const ARTIST_DATA = {
   "id": "sheryl-crow",
   "name": "Sheryl Crow",
   "drive": "A",
-  "showCount": 4,
-  "firstYear": 1994,
-  "lastYear": 2025,
-  "years": [
-    1994,
-    1995,
-    2001,
-    2025
-  ],
-  "shows": [
-    {
-      "year": 1994,
-      "month": 7,
-      "day": 31,
-      "dateStr": "7-31-1994",
-      "city": "Mountain View",
-      "state": "CA",
-      "location": "Mountain View, CA",
-      "venue": "Shoreline Amphitheater",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Shure VP88 \u00b7 mhg"
-    },
-    {
-      "year": 1995,
-      "month": 3,
-      "day": 11,
-      "dateStr": "3-11-1995",
-      "city": "Las Vegas",
-      "state": "NV",
-      "location": "Las Vegas, NV",
-      "venue": "Hardrock's The Joint (M)",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sony ECM 909A \u00b7 marco moir"
-    },
-    {
-      "year": 2001,
-      "month": 4,
-      "day": 30,
-      "dateStr": "4-30-2001",
-      "city": "New York",
-      "state": "NY",
-      "location": "New York, NY",
-      "venue": "Shine Club",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "Unknown"
-    },
-    {
-      "year": 2025,
-      "month": 9,
-      "day": 18,
-      "dateStr": "9-18-2025",
-      "city": "Noblesville",
-      "state": "IN",
-      "location": "Noblesville, IN",
-      "venue": "Ruoff Music Cemter",
-      "version": "AUD",
-      "format": "FLAC",
-      "notes": "SP-CMC-08s \u00b7 ironchef"
-    }
-  ],
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
   "misc": [
     {
       "type": "label",
