f0d80955d655e5814790b3a50ab1c1cda182f75c
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:45:54 -0400

    Auto-export data: Garth Brooks

--- data/garth-brooks.js
+++ data/garth-brooks.js
@@ -2,15 +2,29 @@ const ARTIST_DATA = {
   "id": "garth-brooks",
   "name": "Garth Brooks",
   "drive": "C",
-  "showCount": 4,
-  "firstYear": 1996,
+  "showCount": 5,
+  "firstYear": 1986,
   "lastYear": 2023,
   "years": [
+    1986,
     1996,
     2022,
     2023
   ],
   "shows": [
+    {
+      "year": 1986,
+      "month": null,
+      "day": null,
+      "dateStr": "XX-XX-1986",
+      "city": "Stillwater",
+      "state": "OK",
+      "location": "Stillwater, OK",
+      "venue": "Willie's Saloon",
+      "notes": "unk",
+      "version": "FM",
+      "format": "FLAC"
+    },
     {
       "year": 1996,
       "month": 4,
@@ -20,9 +34,9 @@ const ARTIST_DATA = {
       "state": "WI",
       "location": "Milwaukee, WI",
       "venue": "Bradley Center",
+      "notes": "Sonic Studios · frankmhere",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sonic Studios \u00b7 frankmhere"
+      "format": "FLAC"
     },
     {
       "year": 2022,
@@ -33,9 +47,9 @@ const ARTIST_DATA = {
       "state": "TN",
       "location": "Nashville, TN",
       "venue": "Nissan Stadium",
+      "notes": "SP-OMC-8 · unk",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "SP-OMC-8 \u00b7 Unknown"
+      "format": "FLAC"
     },
     {
       "year": 2022,
@@ -46,9 +60,9 @@ const ARTIST_DATA = {
       "state": "NY",
       "location": "Orchard Park, NY",
       "venue": "Highmark Stadium",
+      "notes": "CA-11s · heywood",
       "version": "AUD",
-      "format": "FLAC 24",
-      "notes": "CA-11s \u00b7 heywood"
+      "format": "FLAC 24"
     },
     {
       "year": 2023,
@@ -59,9 +73,10 @@ const ARTIST_DATA = {
       "state": "NV",
       "location": "Las Vegas, NV",
       "venue": "The Colosseum at Caesars Palace",
+      "notes": "SP-CMC-4U · ijwthstd",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "SP-CMC-4U \u00b7 ijwthstd"
+      "format": "FLAC"
     }
-  ]
+  ],
+  "misc": []
 };
