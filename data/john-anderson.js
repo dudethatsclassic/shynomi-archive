c7cf9298ada94fe382f911e625633a7d93eaeed1
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:45:56 -0400

    Auto-export data: John Anderson

---
+++ data/john-anderson.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "john-anderson",
+  "name": "John Anderson",
+  "drive": "C",
+  "showCount": 1,
+  "firstYear": 1986,
+  "lastYear": 1986,
+  "years": [
+    1986
+  ],
+  "shows": [
+    {
+      "year": 1986,
+      "month": null,
+      "day": null,
+      "dateStr": "XX-XX-1986",
+      "city": "Pasadena",
+      "state": "TX",
+      "location": "Pasadena, TX",
+      "venue": "Gilley's",
+      "notes": "Radio show 86-50 · Vinyl source",
+      "version": "preFM",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
