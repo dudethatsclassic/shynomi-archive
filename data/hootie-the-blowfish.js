504deafc4a255fc0fa5baff1cc5baa35c5c2ca82
dudethatsclassic <natescottsmith@icloud.com>
2026-05-25 11:18:22 -0400

    Auto-export data: Hootie & the Blowfish

---
+++ data/hootie-the-blowfish.js
@@ -0,0 +1,17 @@
+const ARTIST_DATA = {
+  "id": "hootie-the-blowfish",
+  "name": "Hootie & the Blowfish",
+  "drive": "C",
+  "showCount": 0,
+  "firstYear": null,
+  "lastYear": null,
+  "years": [],
+  "shows": [],
+  "misc": [
+    {
+      "type": "label",
+      "label": "Hootie & the Blowfish - Live in Charleston (2006)",
+      "format": "FLAC"
+    }
+  ]
+};
