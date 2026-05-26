f0af8602c8509f2a8049d329ffdb64e51ff4623c
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:10:48 -0400

    Auto-export data: Old 97's

---
+++ data/old-97-s.js
@@ -0,0 +1,97 @@
+const ARTIST_DATA = {
+  "id": "old-97-s",
+  "name": "Old 97's",
+  "drive": "E",
+  "showCount": 6,
+  "firstYear": 1996,
+  "lastYear": 2026,
+  "years": [
+    1996,
+    1998,
+    2008,
+    2011,
+    2014,
+    2026
+  ],
+  "shows": [
+    {
+      "year": 1996,
+      "month": 3,
+      "day": 16,
+      "dateStr": "3-16-1996",
+      "city": "Austin",
+      "state": "TX",
+      "location": "Austin, TX",
+      "venue": "Split Rail",
+      "notes": "Sonic Studios · markp",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1998,
+      "month": 4,
+      "day": 18,
+      "dateStr": "4-18-1998",
+      "city": "Madison",
+      "state": "WI",
+      "location": "Madison, WI",
+      "venue": "Rathskeller",
+      "notes": "University",
+      "version": "SBD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2008,
+      "month": 10,
+      "day": 3,
+      "dateStr": "10-3-2008",
+      "city": "Carrboro",
+      "state": "NC",
+      "location": "Carrboro, NC",
+      "venue": "Cat's Cradle",
+      "notes": "Peluso CEMC-6-CK4 · larry tucker",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2011,
+      "month": 7,
+      "day": 16,
+      "dateStr": "7-16-2011",
+      "city": "Greenfield",
+      "state": "MA",
+      "location": "Greenfield, MA",
+      "venue": "Green River Festival",
+      "notes": "Zoom H2 · unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2014,
+      "month": 6,
+      "day": 1,
+      "dateStr": "6-1-2014",
+      "city": "Annapolis",
+      "state": "MD",
+      "location": "Annapolis, MD",
+      "venue": "Annapolis Smokehouse and Tavern",
+      "notes": "MBHO603A-KA200 · kubacheck",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2026,
+      "month": 4,
+      "day": 4,
+      "dateStr": "4-4-2026",
+      "city": "Washington",
+      "state": "DC",
+      "location": "Washington, DC",
+      "venue": "930 Club",
+      "notes": "Gefell MV400L · alex leary",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
