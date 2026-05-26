58bcded49ff4029c805260fb466d3677ab7f850c
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 17:42:38 -0400

    Auto-export data: Duster

---
+++ data/duster.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "duster",
+  "name": "Duster",
+  "drive": "A",
+  "showCount": 1,
+  "firstYear": 2024,
+  "lastYear": 2024,
+  "years": [
+    2024
+  ],
+  "shows": [
+    {
+      "year": 2024,
+      "month": 4,
+      "day": 27,
+      "dateStr": "2024-04-27",
+      "city": "Las Vegas",
+      "state": "NV",
+      "location": "Las Vegas, NV",
+      "venue": "Las Vegas Fairgrounds",
+      "notes": "Sick · Schoeps MK4 · loughney",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
