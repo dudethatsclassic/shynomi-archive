f393dc5305ba755c06b05da70cd9849a8889009b
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:10:49 -0400

    Auto-export data: Orgy

--- data/orgy.js
+++ data/orgy.js
@@ -18,9 +18,10 @@ const ARTIST_DATA = {
       "state": "CA",
       "location": "West Hollywood, CA",
       "venue": "Whisky A Go Go",
+      "notes": "DPA 4081 · faninor",
       "version": "AUD",
-      "format": "FLAC 24",
-      "notes": "DPA 4081 \u00b7 faninor"
+      "format": "FLAC 24"
     }
-  ]
+  ],
+  "misc": []
 };
