b85d6683b0df850f2541e966808425a5081b23e3
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 18:56:28 -0400

    Auto-export data: Darius Rucker

---
+++ data/darius-rucker.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "darius-rucker",
+  "name": "Darius Rucker",
+  "drive": "C",
+  "showCount": 1,
+  "firstYear": 2023,
+  "lastYear": 2023,
+  "years": [
+    2023
+  ],
+  "shows": [
+    {
+      "year": 2023,
+      "month": 6,
+      "day": 24,
+      "dateStr": "6-24-2023",
+      "city": "Canandaigua",
+      "state": "NY",
+      "location": "Canandaigua, NY",
+      "venue": "CMAC",
+      "notes": "Sennheiser AMBEO · thriftynomad",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
