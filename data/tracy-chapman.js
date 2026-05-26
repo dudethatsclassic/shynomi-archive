44b769bb2e9ec21e69d7c26ab1ca683be8f835aa
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:11:29 -0400

    Auto-export data: Tracy Chapman

---
+++ data/tracy-chapman.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "tracy-chapman",
+  "name": "Tracy Chapman",
+  "drive": "E",
+  "showCount": 1,
+  "firstYear": 1988,
+  "lastYear": 1988,
+  "years": [
+    1988
+  ],
+  "shows": [
+    {
+      "year": 1988,
+      "month": 5,
+      "day": 7,
+      "dateStr": "5-7-1988",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Cabaret Metro",
+      "notes": "ANA(M) · unk · aadam jacobs",
+      "version": "",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
