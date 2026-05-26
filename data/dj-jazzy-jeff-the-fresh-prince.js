fbdea1dafc937ee01a6c73eeb392de0d6570e10b
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:18:38 -0400

    Auto-export data: DJ Jazzy Jeff & the Fresh Prince

---
+++ data/dj-jazzy-jeff-the-fresh-prince.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "dj-jazzy-jeff-the-fresh-prince",
+  "name": "DJ Jazzy Jeff & the Fresh Prince",
+  "drive": "D",
+  "showCount": 1,
+  "firstYear": 1988,
+  "lastYear": 1988,
+  "years": [
+    1988
+  ],
+  "shows": [
+    {
+      "year": 1988,
+      "month": 6,
+      "day": 27,
+      "dateStr": "6-27-1988",
+      "city": "Los Angeles",
+      "state": "CA",
+      "location": "Los Angeles, CA",
+      "venue": "Greek Theatre",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
