bdcab803cda43f8bd25529276044d01ad42def2b
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:11:00 -0400

    Auto-export data: Saosin

---
+++ data/saosin.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "saosin",
+  "name": "Saosin",
+  "drive": "A",
+  "showCount": 1,
+  "firstYear": 2025,
+  "lastYear": 2025,
+  "years": [
+    2025
+  ],
+  "shows": [
+    {
+      "year": 2025,
+      "month": 7,
+      "day": 27,
+      "dateStr": "7-27-2025",
+      "city": "Long Beach",
+      "state": "CA",
+      "location": "Long Beach, CA",
+      "venue": "Shoreline Waterfront",
+      "notes": "Warped · DPA 4081 · faninor",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
