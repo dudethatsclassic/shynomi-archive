b077b79bcaf0d5fd07def3ea1198b0d1ad6637be
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:11:00 -0400

    Auto-export data: Sam and Dave

---
+++ data/sam-and-dave.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "sam-and-dave",
+  "name": "Sam and Dave",
+  "drive": "F",
+  "showCount": 1,
+  "firstYear": 1979,
+  "lastYear": 1979,
+  "years": [
+    1979
+  ],
+  "shows": [
+    {
+      "year": 1979,
+      "month": 10,
+      "day": 27,
+      "dateStr": "10-27-1979",
+      "city": "New York",
+      "state": "NY",
+      "location": "New York, NY",
+      "venue": "Trax",
+      "notes": "WPIX 102FM",
+      "version": "FM",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
