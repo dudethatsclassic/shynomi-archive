9329889039101dacf33cd9c0e7d723cac248e935
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 17:42:59 -0400

    Auto-export data: Madder Rose

---
+++ data/madder-rose.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "madder-rose",
+  "name": "Madder Rose",
+  "drive": "E",
+  "showCount": 1,
+  "firstYear": 1994,
+  "lastYear": 1994,
+  "years": [
+    1994
+  ],
+  "shows": [
+    {
+      "year": 1994,
+      "month": 6,
+      "day": 9,
+      "dateStr": "6-9-1994",
+      "city": "San Francisco",
+      "state": "CA",
+      "location": "San Francisco, CA",
+      "venue": "Slim's",
+      "notes": "ANA(M) · ECM-909 · tjcruz",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
