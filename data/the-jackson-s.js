d101ff9dfebb8a24a6c8b89e4fa731f0348dde1e
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:10:12 -0400

    Auto-export data: The Jackson's

---
+++ data/the-jackson-s.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "the-jackson-s",
+  "name": "The Jackson's",
+  "drive": "C",
+  "showCount": 1,
+  "firstYear": 1981,
+  "lastYear": 1981,
+  "years": [
+    1981
+  ],
+  "shows": [
+    {
+      "year": 1981,
+      "month": 9,
+      "day": 5,
+      "dateStr": "9-5-1981",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Chicago Stadium",
+      "notes": "ANA(M) · jems transfer",
+      "version": "SBD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
