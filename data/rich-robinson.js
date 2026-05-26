36b73b50f7e0859ce09623551f09b0d0d1bb6ac8
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 17:41:25 -0400

    Auto-export data: Rich Robinson

---
+++ data/rich-robinson.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "rich-robinson",
+  "name": "Rich Robinson",
+  "drive": "E",
+  "showCount": 1,
+  "firstYear": 2015,
+  "lastYear": 2015,
+  "years": [
+    2015
+  ],
+  "shows": [
+    {
+      "year": 2015,
+      "month": 5,
+      "day": 27,
+      "dateStr": "5-27-2015",
+      "city": "Washington",
+      "state": "DC",
+      "location": "Washington, DC",
+      "venue": "The Hamilton",
+      "notes": "DPA 4023 · alex leary",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
