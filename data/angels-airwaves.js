37dfd9e8e29006ad1739f0a438a3bef9504df4c0
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:09:22 -0400

    Auto-export data: Angels & Airwaves

---
+++ data/angels-airwaves.js
@@ -0,0 +1,54 @@
+const ARTIST_DATA = {
+  "id": "angels-airwaves",
+  "name": "Angels & Airwaves",
+  "drive": "B",
+  "showCount": 3,
+  "firstYear": 2012,
+  "lastYear": 2019,
+  "years": [
+    2012,
+    2019
+  ],
+  "shows": [
+    {
+      "year": 2012,
+      "month": 3,
+      "day": 1,
+      "dateStr": "3-1-2012",
+      "city": "Melbourne",
+      "state": "Australia",
+      "location": "Melbourne, Australia",
+      "venue": "The Forum",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2019,
+      "month": 9,
+      "day": 20,
+      "dateStr": "9-20-2019",
+      "city": "Detroit",
+      "state": "MI",
+      "location": "Detroit, MI",
+      "venue": "The Fillmore",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2019,
+      "month": 12,
+      "day": 7,
+      "dateStr": "12-7-2019",
+      "city": "Anaheim",
+      "state": "CA",
+      "location": "Anaheim, CA",
+      "venue": "Honda Center",
+      "notes": "KROQ",
+      "version": "Webcast",
+      "format": "TS"
+    }
+  ],
+  "misc": []
+};
