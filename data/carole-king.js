7a6af4e59873d9840fec019b30e0045d9398a0b5
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:09:47 -0400

    Auto-export data: Carole King

--- data/carole-king.js
+++ data/carole-king.js
@@ -2,13 +2,27 @@ const ARTIST_DATA = {
   "id": "carole-king",
   "name": "Carole King",
   "drive": "C",
-  "showCount": 1,
-  "firstYear": 1973,
+  "showCount": 2,
+  "firstYear": 1971,
   "lastYear": 1973,
   "years": [
+    1971,
     1973
   ],
   "shows": [
+    {
+      "year": 1971,
+      "month": 5,
+      "day": null,
+      "dateStr": "5-XX-1971",
+      "city": "Los Angeles",
+      "state": "CA",
+      "location": "Los Angeles, CA",
+      "venue": "Troubadour [Vinyl] *Fit For A King",
+      "notes": "",
+      "version": "",
+      "format": "FLAC"
+    },
     {
       "year": 1973,
       "month": 5,
@@ -18,9 +32,10 @@ const ARTIST_DATA = {
       "state": "MI",
       "location": "Clarkston, MI",
       "venue": "Pine Knob Music Theater",
+      "notes": "",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": ""
+      "format": "FLAC"
     }
-  ]
+  ],
+  "misc": []
 };
