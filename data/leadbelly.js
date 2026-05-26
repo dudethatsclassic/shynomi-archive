086b915e7a00d443ce47b748164289e609b2f25f
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:19:16 -0400

    Auto-export data: Leadbelly

--- data/leadbelly.js
+++ data/leadbelly.js
@@ -2,13 +2,27 @@ const ARTIST_DATA = {
   "id": "leadbelly",
   "name": "Leadbelly",
   "drive": "E",
-  "showCount": 1,
-  "firstYear": 1949,
+  "showCount": 2,
+  "firstYear": 1940,
   "lastYear": 1949,
   "years": [
+    1940,
     1949
   ],
   "shows": [
+    {
+      "year": 1940,
+      "month": null,
+      "day": null,
+      "dateStr": "XX-XX-1940",
+      "city": "New York",
+      "state": "NY",
+      "location": "New York, NY",
+      "venue": "WNYC Radio",
+      "notes": "Folk · with Woody Guthrie",
+      "version": "FM",
+      "format": "FLAC"
+    },
     {
       "year": 1949,
       "month": 6,
@@ -17,10 +31,11 @@ const ARTIST_DATA = {
       "city": "Austin",
       "state": "TX",
       "location": "Austin, TX",
-      "venue": "Hogg Auditorium - University of Texas",
+      "venue": "Hogg Auditorium",
+      "notes": "University · KUT Radio Broadcast",
       "version": "FM",
-      "format": "FLAC",
-      "notes": "KUT Radio Broadcast"
+      "format": "FLAC"
     }
-  ]
+  ],
+  "misc": []
 };
