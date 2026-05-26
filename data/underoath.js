9cba10d517dcb83e497629911c5311133b838282
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 08:11:34 -0400

    Auto-export data: Underoath

---
+++ data/underoath.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "underoath",
+  "name": "Underoath",
+  "drive": "A",
+  "showCount": 1,
+  "firstYear": 2025,
+  "lastYear": 2025,
+  "years": [
+    2025
+  ],
+  "shows": [
+    {
+      "year": 2025,
+      "month": 3,
+      "day": 28,
+      "dateStr": "3-28-2025",
+      "city": "Inglewood",
+      "state": "CA",
+      "location": "Inglewood, CA",
+      "venue": "Kia Forum",
+      "notes": "DPA 4081 · faninor",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
