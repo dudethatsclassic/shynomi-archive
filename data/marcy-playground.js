775aa5320e866d3a6f6b0aa36d6ec464a08cb8ee
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 17:43:00 -0400

    Auto-export data: Marcy Playground

---
+++ data/marcy-playground.js
@@ -0,0 +1,41 @@
+const ARTIST_DATA = {
+  "id": "marcy-playground",
+  "name": "Marcy Playground",
+  "drive": "E",
+  "showCount": 2,
+  "firstYear": 2022,
+  "lastYear": 2024,
+  "years": [
+    2022,
+    2024
+  ],
+  "shows": [
+    {
+      "year": 2022,
+      "month": 8,
+      "day": 27,
+      "dateStr": "8-27-2022",
+      "city": "Niagara Falls",
+      "state": "ONT",
+      "location": "Niagara Falls, ONT",
+      "venue": "Avalon Ballroom",
+      "notes": "CA14s · binko",
+      "version": "AUD",
+      "format": "FLAC 24"
+    },
+    {
+      "year": 2024,
+      "month": 9,
+      "day": 19,
+      "dateStr": "9-19-2024",
+      "city": "Los Angeles",
+      "state": "CA",
+      "location": "Los Angeles, CA",
+      "venue": "The Regent Theater",
+      "notes": "DPA 4081 · faninor",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
