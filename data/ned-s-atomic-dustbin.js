37c713e1fd33e151daf216f011ac950a0876c140
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 17:43:03 -0400

    Auto-export data: Ned's Atomic Dustbin

---
+++ data/ned-s-atomic-dustbin.js
@@ -0,0 +1,41 @@
+const ARTIST_DATA = {
+  "id": "ned-s-atomic-dustbin",
+  "name": "Ned's Atomic Dustbin",
+  "drive": "E",
+  "showCount": 2,
+  "firstYear": 1991,
+  "lastYear": 2023,
+  "years": [
+    1991,
+    2023
+  ],
+  "shows": [
+    {
+      "year": 1991,
+      "month": 7,
+      "day": 25,
+      "dateStr": "7-25-1991",
+      "city": "West Hollywood",
+      "state": "CA",
+      "location": "West Hollywood, CA",
+      "venue": "KROQ Noontime Free Concert at the Roxy Theatre",
+      "notes": "van",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2023,
+      "month": 12,
+      "day": 9,
+      "dateStr": "12-9-2023",
+      "city": "Wolverhampton",
+      "state": "UK",
+      "location": "Wolverhampton, UK",
+      "venue": "The Halls at the Civic",
+      "notes": "Roland R05 · motr1912",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
