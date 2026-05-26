c5fd76a8ee858435c4f896337204ad1dc075a202
dudethatsclassic <natescottsmith@icloud.com>
2026-05-26 18:56:50 -0400

    Auto-export data: Toby Keith

---
+++ data/toby-keith.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "toby-keith",
+  "name": "Toby Keith",
+  "drive": "C",
+  "showCount": 1,
+  "firstYear": 2002,
+  "lastYear": 2002,
+  "years": [
+    2002
+  ],
+  "shows": [
+    {
+      "year": 2002,
+      "month": 8,
+      "day": 17,
+      "dateStr": "8-17-2002",
+      "city": "Rosemont",
+      "state": "IL",
+      "location": "Rosemont, IL",
+      "venue": "Allstate Arena",
+      "notes": "Sony MZ-NF610 · roy martin",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
