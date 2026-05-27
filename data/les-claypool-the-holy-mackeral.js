5d56d115b1aaa6e04bc752ae34ac34ebcf2a099a
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:26:49 -0400

    Auto-export data: Les Claypool & the Holy Mackeral

---
+++ data/les-claypool-the-holy-mackeral.js
@@ -0,0 +1,92 @@
+const ARTIST_DATA = {
+  "id": "les-claypool-the-holy-mackeral",
+  "name": "Les Claypool & the Holy Mackeral",
+  "drive": "F",
+  "showCount": 6,
+  "firstYear": 1996,
+  "lastYear": 1996,
+  "years": [
+    1996
+  ],
+  "shows": [
+    {
+      "year": 1996,
+      "month": 8,
+      "day": 24,
+      "dateStr": "8-24-1996",
+      "city": "San Francisco",
+      "state": "CA",
+      "location": "San Francisco, CA",
+      "venue": "The Fillmore",
+      "notes": "unk · neekrive",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 9,
+      "day": 5,
+      "dateStr": "9-5-1996",
+      "city": "New York",
+      "state": "NY",
+      "location": "New York, NY",
+      "venue": "Supper Club",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 10,
+      "day": 3,
+      "dateStr": "10-3-1996",
+      "city": "San Diego",
+      "state": "CA",
+      "location": "San Diego, CA",
+      "venue": "Brick By Brick",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 10,
+      "day": 4,
+      "dateStr": "10-4-1996",
+      "city": "Tempe",
+      "state": "AZ",
+      "location": "Tempe, AZ",
+      "venue": "Electric Ballroom",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 10,
+      "day": 18,
+      "dateStr": "10-18-1996",
+      "city": "Vancouver",
+      "state": "Canada",
+      "location": "Vancouver, Canada",
+      "venue": "Town Pump",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 10,
+      "day": 20,
+      "dateStr": "10-20-1996",
+      "city": "Portland",
+      "state": "OR",
+      "location": "Portland, OR",
+      "venue": "La Luna",
+      "notes": "AKG C-522 · mark burgin",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
