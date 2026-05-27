4570172fb9c18d5870c8397e0223b5df02a36411
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 08:34:59 -0400

    Auto-export data: Hootie & the Blowfish

--- data/hootie-the-blowfish.js
+++ data/hootie-the-blowfish.js
@@ -2,11 +2,81 @@ const ARTIST_DATA = {
   "id": "hootie-the-blowfish",
   "name": "Hootie & the Blowfish",
   "drive": "C",
-  "showCount": 0,
-  "firstYear": null,
-  "lastYear": null,
-  "years": [],
-  "shows": [],
+  "showCount": 5,
+  "firstYear": 1995,
+  "lastYear": 2024,
+  "years": [
+    1995,
+    1999,
+    2024
+  ],
+  "shows": [
+    {
+      "year": 1995,
+      "month": 2,
+      "day": 13,
+      "dateStr": "2-13-1995",
+      "city": "Pittsburgh",
+      "state": "PA",
+      "location": "Pittsburgh, PA",
+      "venue": "Fat City Blues",
+      "notes": "",
+      "version": "Silver CD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1995,
+      "month": 8,
+      "day": 25,
+      "dateStr": "8-25-1995",
+      "city": "Knoxville",
+      "state": "TN",
+      "location": "Knoxville, TN",
+      "venue": "World's Fair Park",
+      "notes": "ANA(M) · GE VSP · bobc",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1995,
+      "month": 10,
+      "day": 28,
+      "dateStr": "10-28-1995",
+      "city": "Mountain View",
+      "state": "CA",
+      "location": "Mountain View, CA",
+      "venue": "Shoreline Amphitheater",
+      "notes": "Bridge · unk · neekrive",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1999,
+      "month": 4,
+      "day": 29,
+      "dateStr": "4-29-1999",
+      "city": "Wilkesboro",
+      "state": "NC",
+      "location": "Wilkesboro, NC",
+      "venue": "Merlefest",
+      "notes": "DAT(M) · Sony D8 · unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2024,
+      "month": 6,
+      "day": 27,
+      "dateStr": "6-27-2024",
+      "city": "Bethel",
+      "state": "NY",
+      "location": "Bethel, NY",
+      "venue": "Bethel Woods Center for the Arts",
+      "notes": "Schoeps MK41vs · radio_w_b",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
   "misc": [
     {
       "type": "label",
