e744e35a9804f7ffc4dad24a3546d0c1f8cb1f12
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:22:57 -0400

    Auto-export data: Dolores O'Riordan

---
+++ data/dolores-o-riordan.js
@@ -0,0 +1,66 @@
+const ARTIST_DATA = {
+  "id": "dolores-o-riordan",
+  "name": "Dolores O'Riordan",
+  "drive": "B",
+  "showCount": 4,
+  "firstYear": 2007,
+  "lastYear": 2007,
+  "years": [
+    2007
+  ],
+  "shows": [
+    {
+      "year": 2007,
+      "month": 6,
+      "day": 13,
+      "dateStr": "6-13-2007",
+      "city": "Cologne",
+      "state": "Germany",
+      "location": "Cologne, Germany",
+      "venue": "Gloria Theater",
+      "notes": "Sony ECM-CS10 · unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2007,
+      "month": 7,
+      "day": 9,
+      "dateStr": "7-9-2007",
+      "city": "Boston",
+      "state": "MA",
+      "location": "Boston, MA",
+      "venue": "Avalon",
+      "notes": "bennyblanco",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2007,
+      "month": 8,
+      "day": 16,
+      "dateStr": "8-16-2007",
+      "city": "Philadelphia",
+      "state": "PA",
+      "location": "Philadelphia, PA",
+      "venue": "WXPN Studio",
+      "notes": "World",
+      "version": "FM",
+      "format": "FLAC"
+    },
+    {
+      "year": 2007,
+      "month": 8,
+      "day": 20,
+      "dateStr": "8-20-2007",
+      "city": "Buenos Aires",
+      "state": "Argentina",
+      "location": "Buenos Aires, Argentina",
+      "venue": "FMM 100 Studios",
+      "notes": "",
+      "version": "WEB",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
