88748480ae1e28c083fc7e41e83457bc5e7bfc1d
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:25:00 -0400

    Auto-export data: Scott Lucas & the Married Men

---
+++ data/scott-lucas-the-married-men.js
@@ -0,0 +1,67 @@
+const ARTIST_DATA = {
+  "id": "scott-lucas-the-married-men",
+  "name": "Scott Lucas & the Married Men",
+  "drive": "B",
+  "showCount": 4,
+  "firstYear": 2011,
+  "lastYear": 2022,
+  "years": [
+    2011,
+    2022
+  ],
+  "shows": [
+    {
+      "year": 2011,
+      "month": 5,
+      "day": 26,
+      "dateStr": "5-26-2011",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "The Double Door",
+      "notes": "tapeworm48",
+      "version": "WEB",
+      "format": "FLAC"
+    },
+    {
+      "year": 2022,
+      "month": 3,
+      "day": 25,
+      "dateStr": "3-25-2022",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Empty Bottle",
+      "notes": "AT853U · charles dewey cole",
+      "version": "AUD",
+      "format": "FLAC 24"
+    },
+    {
+      "year": 2022,
+      "month": 3,
+      "day": 25,
+      "dateStr": "3-25-2022",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Empty Bottle",
+      "notes": "DPA 4060 · charles dewey cole",
+      "version": "AUD",
+      "format": "FLAC 24"
+    },
+    {
+      "year": 2022,
+      "month": 8,
+      "day": 27,
+      "dateStr": "8-27-2022",
+      "city": "Skokie",
+      "state": "IL",
+      "location": "Skokie, IL",
+      "venue": "Skokie Backlot Bash",
+      "notes": "Schoeps MK4s · tapeworm48",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
