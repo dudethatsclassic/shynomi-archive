ae685d453d696bc955598d82a2f483357e17ecdb
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:24:09 -0400

    Auto-export data: The Network

---
+++ data/the-network.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "the-network",
+  "name": "The Network",
+  "drive": "F",
+  "showCount": 1,
+  "firstYear": 2005,
+  "lastYear": 2005,
+  "years": [
+    2005
+  ],
+  "shows": [
+    {
+      "year": 2005,
+      "month": 10,
+      "day": 13,
+      "dateStr": "10-13-2005",
+      "city": "San Francisco",
+      "state": "CA",
+      "location": "San Francisco, CA",
+      "venue": "The Warfield",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
