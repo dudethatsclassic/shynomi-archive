36a7933eb0765fe6ca63511958873a2a09429fd0
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:26:43 -0400

    Auto-export data: Les Claypool & Friends

---
+++ data/les-claypool-friends.js
@@ -0,0 +1,41 @@
+const ARTIST_DATA = {
+  "id": "les-claypool-friends",
+  "name": "Les Claypool & Friends",
+  "drive": "F",
+  "showCount": 2,
+  "firstYear": 2024,
+  "lastYear": 2025,
+  "years": [
+    2024,
+    2025
+  ],
+  "shows": [
+    {
+      "year": 2024,
+      "month": 11,
+      "day": 23,
+      "dateStr": "11-23-2024",
+      "city": "Sebastopol",
+      "state": "CA",
+      "location": "Sebastopol, CA",
+      "venue": "HopMonk Tavern",
+      "notes": "CA-11 · leifh",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2025,
+      "month": 11,
+      "day": 22,
+      "dateStr": "11-22-2025",
+      "city": "Sebastopol",
+      "state": "CA",
+      "location": "Sebastopol, CA",
+      "venue": "Hopmonk",
+      "notes": "16th · Sennheiser + Pixel · leifh",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ],
+  "misc": []
+};
