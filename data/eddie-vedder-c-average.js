657d52ab157ab7f36ce6fba65bb09bff555df028
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:26:05 -0400

    Auto-export data: Eddie Vedder & C-Average

---
+++ data/eddie-vedder-c-average.js
@@ -0,0 +1,118 @@
+const ARTIST_DATA = {
+  "id": "eddie-vedder-c-average",
+  "name": "Eddie Vedder & C-Average",
+  "drive": "D",
+  "showCount": 8,
+  "firstYear": 1999,
+  "lastYear": 1999,
+  "years": [
+    1999
+  ],
+  "shows": [
+    {
+      "year": 1999,
+      "month": 6,
+      "day": 13,
+      "dateStr": "6-13-1999",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Alpine Valley Music Theatre",
+      "notes": "Tibetan · WW1 #99-50 (M",
+      "version": "MIX",
+      "format": "FLAC"
+    },
+    {
+      "year": 1999,
+      "month": 6,
+      "day": 13,
+      "dateStr": "6-13-1999",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Alpine Valley Music Theatre",
+      "notes": "Tibetan · unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1999,
+      "month": 6,
+      "day": 13,
+      "dateStr": "6-13-1999",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Alpine Valley Music Theatre",
+      "notes": "Tibetan · M",
+      "version": "AUD",
+      "format": "DVD"
+    },
+    {
+      "year": 1999,
+      "month": 6,
+      "day": 25,
+      "dateStr": "6-25-1999",
+      "city": "Hollywood",
+      "state": "CA",
+      "location": "Hollywood, CA",
+      "venue": "Palladium",
+      "notes": "unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1999,
+      "month": 7,
+      "day": 14,
+      "dateStr": "7-14-1999",
+      "city": "Washington",
+      "state": "",
+      "location": "Washington",
+      "venue": "510 Columbia Street Theater",
+      "notes": "unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1999,
+      "month": 11,
+      "day": 12,
+      "dateStr": "11-12-1999",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "House of Blues",
+      "notes": "unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1999,
+      "month": 11,
+      "day": 13,
+      "dateStr": "11-13-1999",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "House of Blues",
+      "notes": "unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1999,
+      "month": 11,
+      "day": 13,
+      "dateStr": "11-13-1999",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "House of Blues",
+      "notes": "global voodoo",
+      "version": "PRO",
+      "format": "DVD"
+    }
+  ],
+  "misc": []
+};
