e7661fe1d36f272a419fab06a6c664aca93e1f6a
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:26:53 -0400

    Auto-export data: Claypool Gold

---
+++ data/claypool-gold.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "claypool-gold",
+  "name": "Claypool Gold",
+  "drive": "F",
+  "showCount": 1,
+  "firstYear": 2024,
+  "lastYear": 2024,
+  "years": [
+    2024
+  ],
+  "shows": [
+    {
+      "year": 2024,
+      "month": 12,
+      "day": 31,
+      "dateStr": "12-31-2024",
+      "city": "Oakland CA",
+      "state": "",
+      "location": "Oakland CA",
+      "venue": "Fox Theater",
+      "notes": "CA-11 · leifh",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
