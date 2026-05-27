bdf7441088fd0f1e295a3d4655dc18ea2ea618ea
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 19:47:14 -0400

    Auto-export data: Jeff Schroeder

---
+++ data/jeff-schroeder.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "jeff-schroeder",
+  "name": "Jeff Schroeder",
+  "drive": "B",
+  "showCount": 1,
+  "firstYear": 2019,
+  "lastYear": 2019,
+  "years": [
+    2019
+  ],
+  "shows": [
+    {
+      "year": 2019,
+      "month": 11,
+      "day": 30,
+      "dateStr": "11-30-2019",
+      "city": "YB w/ Jeff Schroeder",
+      "state": "",
+      "location": "YB w/ Jeff Schroeder",
+      "venue": "",
+      "notes": "",
+      "version": "",
+      "format": "MKV"
+    }
+  ],
+  "misc": []
+};
