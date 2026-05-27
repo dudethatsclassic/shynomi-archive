3ab8e0cb0c0dd1b3bcb364282b0a325b19824f2e
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:26:55 -0400

    Auto-export data: Les Claypool and the Fancy Band

---
+++ data/les-claypool-and-the-fancy-band.js
@@ -0,0 +1,67 @@
+const ARTIST_DATA = {
+  "id": "les-claypool-and-the-fancy-band",
+  "name": "Les Claypool and the Fancy Band",
+  "drive": "F",
+  "showCount": 4,
+  "firstYear": 2005,
+  "lastYear": 2007,
+  "years": [
+    2005,
+    2007
+  ],
+  "shows": [
+    {
+      "year": 2005,
+      "month": 7,
+      "day": 7,
+      "dateStr": "7-7-2005",
+      "city": "Boulder",
+      "state": "CO",
+      "location": "Boulder, CO",
+      "venue": "Fox Theatre",
+      "notes": "DPA 4011 · unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2005,
+      "month": 7,
+      "day": 8,
+      "dateStr": "7-8-2005",
+      "city": "Boulder",
+      "state": "CO",
+      "location": "Boulder, CO",
+      "venue": "Fox Theatre",
+      "notes": "Schopes MK4 · unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2007,
+      "month": 6,
+      "day": 1,
+      "dateStr": "6-1-2007",
+      "city": "Boston",
+      "state": "MA",
+      "location": "Boston, MA",
+      "venue": "The Roxy",
+      "notes": "CSB · ghostwheel",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2007,
+      "month": 6,
+      "day": 3,
+      "dateStr": "6-3-2007",
+      "city": "Philadelphia",
+      "state": "PA",
+      "location": "Philadelphia, PA",
+      "venue": "The Electric Factory",
+      "notes": "CSB · ghostwheel",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
