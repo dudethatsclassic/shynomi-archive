9541167d1b13dce38beb81323668d1e985a445f9
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:26:08 -0400

    Auto-export data: Eddie Vedder & Neil Finn

---
+++ data/eddie-vedder-neil-finn.js
@@ -0,0 +1,67 @@
+const ARTIST_DATA = {
+  "id": "eddie-vedder-neil-finn",
+  "name": "Eddie Vedder & Neil Finn",
+  "drive": "D",
+  "showCount": 4,
+  "firstYear": 1998,
+  "lastYear": 2001,
+  "years": [
+    1998,
+    2001
+  ],
+  "shows": [
+    {
+      "year": 1998,
+      "month": 11,
+      "day": 6,
+      "dateStr": "11-6-1998",
+      "city": "Boston",
+      "state": "MA",
+      "location": "Boston, MA",
+      "venue": "Karma Club",
+      "notes": "marchesano transfer",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2001,
+      "month": 4,
+      "day": 2,
+      "dateStr": "4-2-2001",
+      "city": "New Zealand",
+      "state": "",
+      "location": "New Zealand",
+      "venue": "St. James Theater",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2001,
+      "month": 4,
+      "day": 4,
+      "dateStr": "4-4-2001",
+      "city": "New Zealand",
+      "state": "",
+      "location": "New Zealand",
+      "venue": "St. James Theater",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2001,
+      "month": 4,
+      "day": 5,
+      "dateStr": "4-5-2001",
+      "city": "New Zealand",
+      "state": "",
+      "location": "New Zealand",
+      "venue": "St. James Theater",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
