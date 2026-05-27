c1a194ce3c2381f7091b352ee6774ef01f768fa7
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 09:26:25 -0400

    Auto-export data: Shadow

--- data/shadow.js
+++ data/shadow.js
@@ -19,9 +19,9 @@ const ARTIST_DATA = {
       "state": "WA",
       "location": "Seattle, WA",
       "venue": "The Showbox",
+      "notes": "Countryman Isomax II",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "Countryman Isomax II"
+      "format": "FLAC"
     },
     {
       "year": 2013,
@@ -32,9 +32,10 @@ const ARTIST_DATA = {
       "state": "WA",
       "location": "Seattle, WA",
       "venue": "Easy Street Records",
+      "notes": "Zoom H2",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "Zoom H2"
+      "format": "FLAC"
     }
-  ]
+  ],
+  "misc": []
 };
