a6d46b360b53e72c9d21f3e7b3be81acdd833c66
dudethatsclassic <natescottsmith@icloud.com>
2026-05-27 19:47:19 -0400

    Auto-export data: Starchildren

---
+++ data/starchildren.js
@@ -0,0 +1,34 @@
+const ARTIST_DATA = {
+  "id": "starchildren",
+  "name": "Starchildren",
+  "drive": "B",
+  "showCount": 1,
+  "firstYear": 1994,
+  "lastYear": 1994,
+  "years": [
+    1994
+  ],
+  "shows": [
+    {
+      "year": 1994,
+      "month": 6,
+      "day": 18,
+      "dateStr": "6-18-1994",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Double Door (NSPAA)",
+      "notes": "ANA(M) · Realistic",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": [
+    {
+      "type": "release",
+      "label": "Starchildren & Catherine - Delusions Of Candor (Flight Of The Eagle) - Chante Des Chansons Sur Les Filles! (Songs About Girls) (Single)",
+      "version": "Official",
+      "format": "FLAC"
+    }
+  ]
+};
