a19964bebcb7ea46c0b977fdd0caa52f246d90f8
dudethatsclassic <natescottsmith@icloud.com>
2026-06-06 07:46:39 -0400

    Auto-export data: Mike D

---
+++ data/mike-d.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "mike-d",
+  "name": "Mike D",
+  "drive": "D",
+  "showCount": 1,
+  "firstYear": 2007,
+  "lastYear": 2007,
+  "years": [
+    2007
+  ],
+  "shows": [
+    {
+      "year": 2007,
+      "month": 6,
+      "day": 24,
+      "dateStr": "6-24-2007",
+      "city": "Neuhausen Ob Eck",
+      "state": "Allemagne",
+      "location": "Neuhausen Ob Eck, Allemagne",
+      "venue": "Southside",
+      "notes": "Sony ECM 719 · elysium",
+      "version": "",
+      "format": ""
+    }
+  ],
+  "misc": []
+};
