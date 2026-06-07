eb02a1c8b9839acfedd5bde680054a6a64c22be0
dudethatsclassic <natescottsmith@icloud.com>
2026-06-07 13:45:06 -0400

    Auto-export data: Claypool Gold

--- data/claypool-gold.js
+++ data/claypool-gold.js
@@ -2,11 +2,12 @@ const ARTIST_DATA = {
   "id": "claypool-gold",
   "name": "Claypool Gold",
   "drive": "F",
-  "showCount": 1,
+  "showCount": 4,
   "firstYear": 2024,
-  "lastYear": 2024,
+  "lastYear": 2026,
   "years": [
-    2024
+    2024,
+    2026
   ],
   "shows": [
     {
@@ -21,6 +22,45 @@ const ARTIST_DATA = {
       "notes": "CA-11 · leifh",
       "version": "AUD",
       "format": "FLAC"
+    },
+    {
+      "year": 2026,
+      "month": 5,
+      "day": 20,
+      "dateStr": "5-20-2026",
+      "city": "Reno",
+      "state": "NV",
+      "location": "Reno, NV",
+      "venue": "Reno Events Center",
+      "notes": "Motorola Moto G · dpupenya",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2026,
+      "month": 5,
+      "day": 22,
+      "dateStr": "5-22-2026",
+      "city": "Bend",
+      "state": "OR",
+      "location": "Bend, OR",
+      "venue": "Hayden Homes Amphitheatre",
+      "notes": "Senn Ambeo · grizzly adams",
+      "version": "AUD",
+      "format": "FLAC 24"
+    },
+    {
+      "year": 2026,
+      "month": 5,
+      "day": 23,
+      "dateStr": "5-23-2026",
+      "city": "Redmond",
+      "state": "WA",
+      "location": "Redmond, WA",
+      "venue": "Marymoor Park",
+      "notes": "Senn Ambeo · grizzly adams",
+      "version": "AUD",
+      "format": "FLAC 24"
     }
   ],
   "misc": []
