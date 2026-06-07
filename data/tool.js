d0ef1887fef888ed7f045b803fa1fe729d27fcca
dudethatsclassic <natescottsmith@icloud.com>
2026-06-07 11:24:10 -0400

    Auto-export data: Tool

--- data/tool.js
+++ data/tool.js
@@ -1918,7 +1918,7 @@ const ARTIST_DATA = {
       "state": "CA",
       "location": "Ventura, CA",
       "venue": "Ventura Theater",
-      "notes": "SS-DSM-6 · unk · Remaster",
+      "notes": "SS-DSM-6 · unk",
       "version": "AUD",
       "format": "FLAC"
     },
