413cc78dcd711bda63a06947e892e9206a099ec1
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 17:30:26 -0400

    Auto-export data: Black Flag

--- data/black-flag.js
+++ data/black-flag.js
@@ -19,11 +19,10 @@ const ARTIST_DATA = {
       "city": "Santa Monica",
       "state": "CA",
       "location": "Santa Monica, CA",
-      "venue": "Santa Monica Civic Auditorium - ANA",
+      "venue": "Santa Monica Civic Auditorium",
+      "notes": "ANA(M) · unk",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "M \u00b7 Unknown",
-      "textFile": "1981-06-19 - Santa Monica, CA - Santa Monica Civic Auditorium - ANA(M) (unk) [AUD].txt"
+      "format": "FLAC"
     },
     {
       "year": 1981,
@@ -34,9 +33,9 @@ const ARTIST_DATA = {
       "state": "DC",
       "location": "Washington, DC",
       "venue": "930 Club",
+      "notes": "Early & Late Shows · djm5000",
       "version": "AUD",
       "format": "FLAC",
-      "notes": "Early & Late Shows \u00b7 djm5000",
       "textFile": "1981-12-03 - Washington, DC - 930 Club (Early & Late Shows) (djm5000) [AUD].txt"
     },
     {
@@ -47,11 +46,10 @@ const ARTIST_DATA = {
       "city": "New Orleans",
       "state": "LA",
       "location": "New Orleans, LA",
-      "venue": "Dream Palace - ANA(M)",
+      "venue": "Dream Palace",
+      "notes": "ANA(M) · Sony TC-D5 · reed hayward",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sony TC-D5 \u00b7 reed hayward",
-      "textFile": "1984-11-03 - New Orleans, LA - Dream Palace - ANA(M) (Sony TC-D5) (reed hayward) [AUD].txt"
+      "format": "FLAC"
     },
     {
       "year": 1985,
@@ -61,11 +59,11 @@ const ARTIST_DATA = {
       "city": "New Orleans",
       "state": "LA",
       "location": "New Orleans, LA",
-      "venue": "St. Theresa's - ANA(M)",
+      "venue": "St. Theresa's",
+      "notes": "ANA(M) · Sony TC-D5 · reed hayward",
       "version": "AUD",
-      "format": "FLAC",
-      "notes": "Sony TC-D5 \u00b7 reed hayward",
-      "textFile": "1985-06-06 - New Orleans, LA - St. Theresa's - ANA(M) (Sony TC-D5) (reed hayward) [AUD].txt"
+      "format": "FLAC"
     }
-  ]
+  ],
+  "misc": []
 };
