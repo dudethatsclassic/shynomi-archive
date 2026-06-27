5a3ae21d2b8a13089bbc361651fa617e4cbd9930
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 13:10:48 -0400

    Auto-export data: Jonny Greenwood

---
+++ data/jonny-greenwood.js
@@ -0,0 +1,40 @@
+const ARTIST_DATA = {
+  "id": "jonny-greenwood",
+  "name": "Jonny Greenwood",
+  "drive": "",
+  "showCount": 2,
+  "firstYear": 2014,
+  "lastYear": 2014,
+  "years": [
+    2014
+  ],
+  "shows": [
+    {
+      "year": 2014,
+      "month": 3,
+      "day": 29,
+      "dateStr": "3-29-2014",
+      "city": "Knoxville",
+      "state": "TN",
+      "location": "Knoxville, TN",
+      "venue": "Wordless Music Orchestra",
+      "notes": "Tascam DR-2d · funkitblog",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2014,
+      "month": 5,
+      "day": 18,
+      "dateStr": "5-18-2014",
+      "city": "London",
+      "state": "UK",
+      "location": "London, UK",
+      "venue": "Barbican Theatre",
+      "notes": "SP-CMC-2 · austin brock",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
