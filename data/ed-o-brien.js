9d4ffcc443730b659d7512a0fa05b5e0c9c6b7ac
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 13:10:46 -0400

    Auto-export data: Ed O'Brien

---
+++ data/ed-o-brien.js
@@ -0,0 +1,53 @@
+const ARTIST_DATA = {
+  "id": "ed-o-brien",
+  "name": "Ed O'Brien",
+  "drive": "",
+  "showCount": 3,
+  "firstYear": 2020,
+  "lastYear": 2020,
+  "years": [
+    2020
+  ],
+  "shows": [
+    {
+      "year": 2020,
+      "month": 2,
+      "day": 10,
+      "dateStr": "2-10-2020",
+      "city": "New York",
+      "state": "NY",
+      "location": "New York, NY",
+      "venue": "Poisson Rouge",
+      "notes": "unk · joe breslin",
+      "version": "AUD",
+      "format": "FLAC 24"
+    },
+    {
+      "year": 2020,
+      "month": 2,
+      "day": 13,
+      "dateStr": "2-13-2020",
+      "city": "Los Angeles",
+      "state": "CA",
+      "location": "Los Angeles, CA",
+      "venue": "Masonic Lodge at Hollywood Forever",
+      "notes": "DPA 4081 · faninor",
+      "version": "AUD",
+      "format": "FLAC 24"
+    },
+    {
+      "year": 2020,
+      "month": 3,
+      "day": 10,
+      "dateStr": "3-10-2020",
+      "city": "Paris",
+      "state": "France",
+      "location": "Paris, France",
+      "venue": "Yoyo",
+      "notes": "UHD",
+      "version": "TV",
+      "format": "MKV"
+    }
+  ],
+  "misc": []
+};
