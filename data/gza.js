3113b2dcc44968cdbd9e4308b002ee5834c95cbc
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 19:50:49 -0400

    Auto-export data: GZA

---
+++ data/gza.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "gza",
+  "name": "GZA",
+  "drive": "D",
+  "showCount": 1,
+  "firstYear": 2025,
+  "lastYear": 2025,
+  "years": [
+    2025
+  ],
+  "shows": [
+    {
+      "year": 2025,
+      "month": 5,
+      "day": 14,
+      "dateStr": "5-14-2025",
+      "city": "Cincinnati",
+      "state": "OH",
+      "location": "Cincinnati, OH",
+      "venue": "The Taft Theatre",
+      "notes": "Schoeps MK4 · funk it blog",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
