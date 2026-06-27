ce3c74f47c277504ca4e33b7c6d6de8c928f3a2b
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 19:57:47 -0400

    Auto-export data: Sade

---
+++ data/sade.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "sade",
+  "name": "Sade",
+  "drive": "D",
+  "showCount": 1,
+  "firstYear": 1984,
+  "lastYear": 1984,
+  "years": [
+    1984
+  ],
+  "shows": [
+    {
+      "year": 1984,
+      "month": 10,
+      "day": null,
+      "dateStr": "10-XX-1984",
+      "city": "Milano",
+      "state": "Italy",
+      "location": "Milano, Italy",
+      "venue": "RAI 3",
+      "notes": "unk",
+      "version": "SBD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
