9d1d5548b03f515422dcf2e72fa20f5c24e28b60
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 17:32:45 -0400

    Auto-export data: All American Rejects

--- data/all-american-rejects.js
+++ data/all-american-rejects.js
@@ -18,7 +18,7 @@ const ARTIST_DATA = {
       "state": "WI",
       "location": "Green Bay, WI",
       "venue": "Phoenix Park",
-      "notes": "Shure MV88, Ambeo, Zoom H2 \u00b7 unk",
+      "notes": "Shure MV88, Ambeo, Zoom H2 · unk",
       "version": "MIX",
       "format": "FLAC 24",
       "textFile": "2025-05-14 - Green Bay, WI - Phoenix Park (Shure MV88, Ambeo, Zoom H2) (unk) [MIX24].txt"
