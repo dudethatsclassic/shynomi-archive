675743aad6e87dc1eaaf239822c942d3e44245bc
Nathan Smith <natescottsmith@Nathans-iMac.local>
2026-06-27 17:39:37 -0400

    Add Patrick Droney (1 show, Hyde Park 2022)

---
+++ data/patrick-droney.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "patrick-droney",
+  "name": "Patrick Droney",
+  "drive": "F",
+  "showCount": 1,
+  "firstYear": 2022,
+  "lastYear": 2022,
+  "years": [
+    2022
+  ],
+  "shows": [
+    {
+      "year": 2022,
+      "month": 6,
+      "day": 26,
+      "dateStr": "6-26-2022",
+      "city": "London",
+      "state": "UK",
+      "location": "London, UK",
+      "venue": "Hyde Park",
+      "notes": "British Summer Time festival",
+      "version": "AUD",
+      "format": "FLAC",
+      "textFile": "2022-06-26 - London, UK - Hyde Park (Schoeps MK41) (daspyknows) [AUD].wav.txt"
+    }
+  ]
+};
