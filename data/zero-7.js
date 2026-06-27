5ffad8c37ff5b2b0150c7bb964c28b64b461a429
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 17:35:19 -0400

    Auto-export data: Zero 7

--- data/zero-7.js
+++ data/zero-7.js
@@ -19,7 +19,7 @@ const ARTIST_DATA = {
       "state": "CA",
       "location": "San Francisco, CA",
       "venue": "Bimbo's 365 Club",
-      "notes": "CCM4s \u00b7 drew",
+      "notes": "CCM4s · drew",
       "version": "SBD",
       "format": "FLAC",
       "textFile": "2002-05-01 - San Francisco, CA - Bimbo's 365 Club (CCM4s) (drew) [SBD].txt"
@@ -33,7 +33,7 @@ const ARTIST_DATA = {
       "state": "CA",
       "location": "San Francisco, CA",
       "venue": "The Fillmore",
-      "notes": "Schoeps CCMV \u00b7 drew",
+      "notes": "Schoeps CCMV · drew",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2006-08-29 - San Francisco, CA - The Fillmore (Schoeps CCMV) (drew) [AUD].txt"
@@ -47,7 +47,7 @@ const ARTIST_DATA = {
       "state": "WA",
       "location": "Seattle, WA",
       "venue": "Bumbershoot Festival",
-      "notes": "Backyard \u00b7 Schoeps CCMV \u00b7 drew",
+      "notes": "Backyard · Schoeps CCMV · drew",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2006-09-03 - Seattle, WA - Bumbershoot Festival - Backyard Stage (Schoeps CCMV) (drew) [AUD].txt"
