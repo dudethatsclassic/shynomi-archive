e209e3369e98cdf7ce0d9f610588515c130d79f2
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 17:35:12 -0400

    Auto-export data: Ugly Kid Joe

--- data/ugly-kid-joe.js
+++ data/ugly-kid-joe.js
@@ -37,7 +37,7 @@ const ARTIST_DATA = {
       "state": "IL",
       "location": "Chicago, IL",
       "venue": "Avalon",
-      "notes": "ANA(x) \u00b7 unk",
+      "notes": "ANA(x) · unk",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "1992-03-04 - Chicago, IL - Avalon - ANA(x) (unk) [AUD].txt"
@@ -65,7 +65,7 @@ const ARTIST_DATA = {
       "state": "PA",
       "location": "Allentown, PA",
       "venue": "Fairgrounds",
-      "notes": "Sony D3 \u00b7 koondog",
+      "notes": "Sony D3 · koondog",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "1993-08-03 - Allentown, PA - Fairgrounds (Sony D3) (koondog) [AUD].txt"
@@ -79,7 +79,7 @@ const ARTIST_DATA = {
       "state": "AUS",
       "location": "Melbourne, AUS",
       "venue": "Palace Theatre",
-      "notes": "DPA MMC4011s \u00b7 roman79",
+      "notes": "DPA MMC4011s · roman79",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2014-04-25 - Melbourne, AUS - Palace Theatre (DPA MMC4011s) (roman79) [AUD].txt"
@@ -93,7 +93,7 @@ const ARTIST_DATA = {
       "state": "UK",
       "location": "Glasgow, UK",
       "venue": "SEC Armadillo",
-      "notes": "Olympus LS-P1 \u00b7 keiths34",
+      "notes": "Olympus LS-P1 · keiths34",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2022-05-21 - Glasgow, UK - SEC Armadillo (Olympus LS-P1) (keiths34) [AUD].txt"
@@ -107,7 +107,7 @@ const ARTIST_DATA = {
       "state": "UK",
       "location": "Birmingham, UK",
       "venue": "The Resorts World Arena",
-      "notes": "SP-CMC-8 \u00b7 unk",
+      "notes": "SP-CMC-8 · unk",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2022-05-27 Birmingham, UK - The Resorts World Arena (SP-CMC-8) (unk) [AUD].txt"
@@ -121,7 +121,7 @@ const ARTIST_DATA = {
       "state": "",
       "location": "London UK",
       "venue": "Wembley OVO Arena",
-      "notes": "Olympus LS-P1 \u00b7 kieths34",
+      "notes": "Olympus LS-P1 · kieths34",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2022-05-28 - London UK - Wembley OVO Arena (Olympus LS-P1) (kieths34) [AUD].txt"
@@ -135,7 +135,7 @@ const ARTIST_DATA = {
       "state": "NV",
       "location": "Las Vegas, NV",
       "venue": "Bakkt Theatre",
-      "notes": "SP-CMC-4U \u00b7 unk",
+      "notes": "SP-CMC-4U · unk",
       "version": "AUD",
       "format": "FLAC 24",
       "textFile": "2024-04-18 - Las Vegas, NV - Bakkt Theatre (SP-CMC-4U) (unk) [AUD24].txt"
@@ -149,7 +149,7 @@ const ARTIST_DATA = {
       "state": "Germany",
       "location": "Wacken, Germany",
       "venue": "Hauptstrasse",
-      "notes": "1080p \u00b7 Magentamusik",
+      "notes": "1080p · Magentamusik",
       "version": "PRO",
       "format": "MP4",
       "textFile": "2025-07-31 - Wacken, Germany - Hauptstrasse (1080p) (Magentamusik) [WEB].txt"
