b60784025f809258dc015f452c09bfb9cd0b2eee
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 17:30:34 -0400

    Auto-export data: Sex Pistols

---
+++ data/sex-pistols.js
@@ -0,0 +1,121 @@
+const ARTIST_DATA = {
+  "id": "sex-pistols",
+  "name": "Sex Pistols",
+  "drive": "F",
+  "showCount": 8,
+  "firstYear": 1976,
+  "lastYear": 1996,
+  "years": [
+    1976,
+    1977,
+    1978,
+    1996
+  ],
+  "shows": [
+    {
+      "year": 1976,
+      "month": 8,
+      "day": 31,
+      "dateStr": "8-31-1976",
+      "city": "London",
+      "state": "UK",
+      "location": "London, UK",
+      "venue": "The 100 Club",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1976,
+      "month": 9,
+      "day": 17,
+      "dateStr": "9-17-1976",
+      "city": "Chelmsford",
+      "state": "England",
+      "location": "Chelmsford, England",
+      "venue": "Chelmsford Maximum Security Prison",
+      "notes": "",
+      "version": "SBD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1976,
+      "month": 12,
+      "day": 9,
+      "dateStr": "12-9-1976",
+      "city": "Manchester",
+      "state": "UK",
+      "location": "Manchester, UK",
+      "venue": "Electric Circus",
+      "notes": "",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1977,
+      "month": 12,
+      "day": 19,
+      "dateStr": "12-19-1977",
+      "city": "Keighley",
+      "state": "UK",
+      "location": "Keighley, UK",
+      "venue": "Nikkers Club",
+      "notes": "unk",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1978,
+      "month": 1,
+      "day": 5,
+      "dateStr": "1-5-1978",
+      "city": "Atlanta",
+      "state": "GA",
+      "location": "Atlanta, GA",
+      "venue": "Great Southeast Music Hall *Kill The Hippies",
+      "notes": "",
+      "version": "Silver CD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 7,
+      "day": 4,
+      "dateStr": "7-4-1996",
+      "city": "Paris",
+      "state": "France",
+      "location": "Paris, France",
+      "venue": "The Zenith",
+      "notes": "",
+      "version": "SBD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 8,
+      "day": 23,
+      "dateStr": "8-23-1996",
+      "city": "Hollywood",
+      "state": "CA",
+      "location": "Hollywood, CA",
+      "venue": "Hollywood Palladium",
+      "notes": "CASS(M)",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 1996,
+      "month": 8,
+      "day": 25,
+      "dateStr": "8-25-1996",
+      "city": "Hollywood",
+      "state": "CA",
+      "location": "Hollywood, CA",
+      "venue": "Hollywood Palladium",
+      "notes": "CASS(M)",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
