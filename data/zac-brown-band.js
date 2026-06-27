83c400fa1240b14c23fb356cec3d7c4c34451b95
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 09:04:24 -0400

    Auto-export data: Zac Brown Band

--- data/zac-brown-band.js
+++ data/zac-brown-band.js
@@ -18,10 +18,9 @@ const ARTIST_DATA = {
       "state": "ON",
       "location": "Ottawa, ON",
       "venue": "Lebreton Flats",
-      "notes": "iPhone 11 \u00b7 palm0014",
+      "notes": "iPhone 11 · palm0014",
       "version": "AUD",
-      "format": "FLAC 24",
-      "textFile": "2024-07-10 - Ottawa, ON - Lebreton Flats (iPhone 11) (palm0014) [AUD24].txt"
+      "format": "FLAC 24"
     }
   ],
   "misc": []
