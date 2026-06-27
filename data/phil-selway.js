217250001e16ffb1d716c2d84bf151e00ae62f7a
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 13:11:00 -0400

    Auto-export data: Phil Selway

---
+++ data/phil-selway.js
@@ -0,0 +1,54 @@
+const ARTIST_DATA = {
+  "id": "phil-selway",
+  "name": "Phil Selway",
+  "drive": "",
+  "showCount": 3,
+  "firstYear": 2010,
+  "lastYear": 2015,
+  "years": [
+    2010,
+    2015
+  ],
+  "shows": [
+    {
+      "year": 2010,
+      "month": 4,
+      "day": 1,
+      "dateStr": "4-1-2010",
+      "city": "Milan",
+      "state": "Italy",
+      "location": "Milan, Italy",
+      "venue": "Tunnel",
+      "notes": "Phil and Lisa Germano · Tascam DR1 · loveandsoul",
+      "version": "AUD",
+      "format": "FLAC"
+    },
+    {
+      "year": 2010,
+      "month": 8,
+      "day": null,
+      "dateStr": "8-XX-2010",
+      "city": "BBC - Phil on Radcliffe and Maconie (unk)",
+      "state": "",
+      "location": "BBC - Phil on Radcliffe and Maconie (unk)",
+      "venue": "",
+      "notes": "",
+      "version": "FM",
+      "format": "FLAC"
+    },
+    {
+      "year": 2015,
+      "month": 8,
+      "day": 6,
+      "dateStr": "8-6-2015",
+      "city": "Brooklyn",
+      "state": "NY",
+      "location": "Brooklyn, NY",
+      "venue": "Music Hall of Williamsburg",
+      "notes": "AT831 · inez",
+      "version": "AUD",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
