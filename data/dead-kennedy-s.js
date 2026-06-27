6467bfbb99a01f7a6f9b21f968928ce9b0509007
dudethatsclassic <natescottsmith@icloud.com>
2026-06-27 17:30:27 -0400

    Auto-export data: Dead Kennedy's

---
+++ data/dead-kennedy-s.js
@@ -0,0 +1,27 @@
+const ARTIST_DATA = {
+  "id": "dead-kennedy-s",
+  "name": "Dead Kennedy's",
+  "drive": "F",
+  "showCount": 1,
+  "firstYear": 1981,
+  "lastYear": 1981,
+  "years": [
+    1981
+  ],
+  "shows": [
+    {
+      "year": 1981,
+      "month": 12,
+      "day": 31,
+      "dateStr": "12-31-1981",
+      "city": "San Francisco",
+      "state": "CA",
+      "location": "San Francisco, CA",
+      "venue": "Factory",
+      "notes": "CASS(1) · KALX FM · cps transfer",
+      "version": "FM",
+      "format": "FLAC"
+    }
+  ],
+  "misc": []
+};
