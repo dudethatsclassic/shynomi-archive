b3a87cd54432ce410504b1e560fb4ec45cd2ed09
dudethatsclassic <natescottsmith@icloud.com>
2026-06-30 10:57:31 -0400

    Auto-export misc: golden-smog

--- data/misc/golden-smog-misc.js
+++ data/misc/golden-smog-misc.js
@@ -1,6 +1,8 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
-    "label": "Golden Smog - 1996-1998 - Various Promotional Appearances [SBD]"
+    "type": "release",
+    "label": "Golden Smog - 1996-1998 - Various Promotional Appearances [SBD]",
+    "year": 1996,
+    "category": "OTHER"
   }
 ];
