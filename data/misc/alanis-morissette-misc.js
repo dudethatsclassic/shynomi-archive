b57b8b0f5c7aeaa81c20717f72aeaf08c5a89370
dudethatsclassic <natescottsmith@icloud.com>
2026-06-30 08:25:30 -0400

    Auto-export misc: alanis-morissette

--- data/misc/alanis-morissette-misc.js
+++ data/misc/alanis-morissette-misc.js
@@ -127,8 +127,8 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "Alanis Morissette - Jagged Little Pill Live {7599-38476-2} (1997)",
-    "year": 1997,
     "format": "DVD",
+    "year": 1997,
     "category": "OFFICIAL RELEASES"
   },
   {
@@ -174,5 +174,196 @@ const ARTIST_MISC_DATA = [
     "year": 2001,
     "format": "FLAC HD",
     "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Alanis {742916 T} (1991)",
+    "format": "FLAC",
+    "year": 1991,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Now Is the Time (1992)",
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Jagged Little Pill (Original 1995 European Pressing) (24/96 Vinyl Transfer)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Jagged Little Pill (9362-45901-2) (1995)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - The Singles Box [5CD] (1996)",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Supposed Former Infatuation Junkie (Maverick-Reprise 9 47094-2) (1998)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Supposed Former Infatuation Junkie (1998) 2015 (24/96)",
+    "format": "FLAC HD",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - MTV Unplugged (1999) (24/96)",
+    "format": "FLAC HD",
+    "year": 1999,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - So-Called Chaos (2004, Maverick, 9362-48555, EU) (2004)",
+    "format": "FLAC",
+    "year": 2004,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Flavors Of Entanglement (2008) {269308-2 US}",
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Live at Montreux 2012 (2013) (24/96)",
+    "format": "FLAC HD",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Jagged Little Pill - 1995 (2015) Collector's Edition",
+    "format": "FLAC HD",
+    "year": 1995,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Jagged Little Pill (2015) (24/44.1)",
+    "format": "FLAC HD",
+    "year": 2015,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Jagged Little Pill (Acoustic) (2015) (24/96)",
+    "format": "FLAC HD",
+    "year": 2015,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Jagged Little Pill (20th Anniversary Target Deluxe Edition) (2015)",
+    "format": "FLAC",
+    "year": 2015,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Jagged Little Pill (2015 Collector's Edition)",
+    "format": "FLAC",
+    "year": 2015,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Hard to Swallow {KTS 515} (1995)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Right Through You {PD124} (1995)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Makes The Point! {TUCD 015} (1996)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette  - You Can Kiss It {KTS 591} (1996)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Going North {MOONRAKER 041} (1996)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Who's That Girl {SP001/2} (1996)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Live Junkie {TB 074} (1998)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Thank U Toronto {TSP-CD 251} (1999)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1999,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - The Hands Of Love {p.rec 03} (2002)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 2002,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Alanis Morissette - Testing {Testing 012}",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "category": "SILVERS"
   }
 ];
