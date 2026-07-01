dc3e7da96c251171a874f19da72e3aacd3a28ff7
dudethatsclassic <natescottsmith@icloud.com>
2026-07-01 16:30:17 -0400

    Auto-export misc: blind-melon

--- data/misc/blind-melon-misc.js
+++ data/misc/blind-melon-misc.js
@@ -1,56 +1,23 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
-    "label": "The Goodfoot Workshop (1990 Demos)",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "Shannon Hoon Acoustic Demos - Kingsway 1994",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "The Headtrain Demos (1990)",
-    "format": "FLAC"
-  },
-  {
     "type": "release",
-    "label": "The Head Train Demos",
+    "label": "Blind Melon - The Goodfoot Workshop Demos (1990)",
+    "format": "FLAC",
     "year": 1990,
-    "format": "SBD"
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "The Head Train Demos",
+    "label": "Shannon Hoon - Acoustic Demos (Kingsway 1994)",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Blind Melon - The Headtrain Demos (1990)",
+    "format": "FLAC",
     "year": 1990,
-    "format": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "Shannon Hoon Acoustic Demos - Kingsway (1995)",
-    "year": 1995
-  },
-  {
-    "type": "release",
-    "label": "The Goodfoot Workshop - 1990 Demos"
-  },
-  {
-    "type": "release",
-    "label": "The Head Train Demos",
-    "year": 1990,
-    "format": "SBD",
-    "textFile": "MISC - 1990 - The Head Train Demos [SBD].txt"
-  },
-  {
-    "type": "release",
-    "label": "Shannon Hoon Acoustic Demos - Kingsway (1995)",
-    "year": 1995,
-    "textFile": "MISC - Shannon Hoon Acoustic Demos - Kingsway (1995).txt"
-  },
-  {
-    "type": "release",
-    "label": "The Goodfoot Workshop - 1990 Demos",
-    "textFile": "MISC - The Goodfoot Workshop - 1990 Demos.txt"
+    "category": "OTHER"
   }
 ];
