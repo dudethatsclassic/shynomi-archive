a3fdfa7f15feb193ae9b9fc789cedd8de55a0a45
dudethatsclassic <natescottsmith@icloud.com>
2026-07-01 13:31:25 -0400

    Auto-export misc: aphex-twin

--- data/misc/aphex-twin-misc.js
+++ data/misc/aphex-twin-misc.js
@@ -1,42 +1,51 @@
 const ARTIST_MISC_DATA = [
   {
+    "type": "release",
     "label": "Aphex Twin - Analogue Bubblebath 2",
     "format": "FLAC",
     "category": "OTHER"
   },
   {
+    "type": "release",
     "label": "Aphex Twin - Analogue Bubblebath 5 (1995)",
-    "year": 1995,
     "format": "FLAC",
+    "year": 1995,
     "category": "OTHER"
   },
   {
+    "type": "release",
     "label": "Aphex Twin - Melodies From Mars (Unreleased Album) (1995)",
-    "year": 1995,
     "format": "FLAC",
+    "year": 1995,
     "category": "OTHER"
   },
   {
+    "type": "release",
     "label": "Aphex Twin - Hangable Auto Bulb",
     "format": "FLAC",
     "category": "OTHER"
   },
   {
+    "type": "release",
     "label": "Aphex Twin - Hangable Auto Bulb 2",
     "format": "FLAC",
     "category": "OTHER"
   },
   {
+    "type": "release",
     "label": "Aphex Twin - Peel Sessions",
     "format": "FLAC",
     "category": "OTHER"
   },
   {
+    "type": "release",
     "label": "Aphex Twin - Unreleased Live Tracks (2016-2019)",
     "format": "FLAC",
+    "year": 2016,
     "category": "OTHER"
   },
   {
+    "type": "release",
     "label": "Aphex Twin - All User18081971 Soundcloud Tracks",
     "format": "MP3 + FLAC",
     "category": "OTHER"
