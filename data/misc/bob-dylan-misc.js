25e36b0d242d042f94dfa6719fe78fa78fb1d08c
dudethatsclassic <natescottsmith@icloud.com>
2026-07-01 19:32:50 -0400

    Auto-export misc: bob-dylan

--- data/misc/bob-dylan-misc.js
+++ data/misc/bob-dylan-misc.js
@@ -3,23 +3,31 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "1961-1965 Bob Dylan - Solid Road",
     "version": "SBD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1961,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "1974 Soundboard Collection (10 CD) Limited Edition 352/500",
     "version": "SBD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1974,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "XX-XX-1974 - Build A Ladder To The Stars (Pink Panther Records)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1974,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "XX-XX-1978 - World Tour Anthology (LB-01755)",
     "format": "SHN",
+    "year": 1978,
+    "category": "OTHER",
     "textFile": "MISC - 1978-XX-XX - World Tour Anthology (LB-01755) .txt"
   },
   {
@@ -27,317 +35,134 @@ const ARTIST_MISC_DATA = [
     "label": "XX-XX-1987 / Real Cuts at Last"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Abandonded Desire (2CD)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Blood on the Tapes",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan & the Band - Basement Tapes (Acetate)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan - 1965 - Long Distance Operator",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1965,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan - First Gaslight Tape {remaster}",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan - The Minnesota Tapes 1961 (Minneapolis Recordings)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1961,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan - Renaldo and Clara Soundtrack (soniclovenoize)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Dimestore Medicine (WC65)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Eat the Document (1966) - Documentary",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 1966,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Isle of Wight (1969)",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 1969,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Witmark Years (Capricorn 2021-22)",
     "format": "FLAC",
+    "year": 2021,
+    "category": "OTHER",
     "textFile": "MISC - The Witmark Years (Capricorn 2021-22).txt"
   },
   {
     "type": "release",
     "label": "What Are You Trying To Say - Performing Artist Series - Vol. 6 (2 shows from 1965)",
     "version": "SBD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1965,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan - 1963-65 - The Ultimate Emmett Grogan Acetates (DIYE 65-66)",
     "format": "FLAC",
+    "year": 1963,
+    "category": "OTHER",
     "textFile": "MISC - Bob Dylan - 1963-65 - The Ultimate Emmett Grogan Acetates (DIYE 65-66).rtf"
   },
   {
     "type": "release",
     "label": "Bob Dylan - Blonde On Blonde (Original Stereo Mixes)",
     "version": "SBD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan - Highway 61 Demo Outtakes",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Bob Dylan & Traveling Wilburys - The Secret Tapes of Lucky Boo",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bob Dylan - The Ghost of Woody Guthrie (1961)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1961,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Bob Dylan - New York Sessions (1974)",
     "version": "SBD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1974,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Bob Dylan and Mark Knopfler - The Jokermen (Scorpio-UK79)",
     "version": "Silver CD",
     "format": "FLAC",
+    "category": "OTHER",
     "textFile": "MISC - Bob Dylan and Mark Knopfler - The Jokermen (Scorpio-UK79) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Highway 61 Demo Outtakes"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - New York Sessions (1974)",
-    "year": 1974,
-    "format": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "1965 - Bob Dylan - Solid Road",
-    "year": 1961
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Bob Dylan Minnesota Tapes",
-    "year": 1961
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Eat The Document",
-    "year": 1966,
-    "format": "DVD"
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Build A Ladder To The Stars (Pink Panther Records)",
-    "year": 1974
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Real Cuts At Last",
-    "year": 1987,
-    "format": "Vinyl"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan & Traveling Wilburys - The Secret Tapes of Lucky Boo"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan -  Blonde On Blonde (Original Stereo Mixes)"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - 1974 Soundboard Collection"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Abandoned Desire"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Blood On the Tapes.shn"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Dimestore Medicine",
-    "version": "Silver CD"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - First Gaslight Tape (Remaster)"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Highway 61 Demo Outtakes"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Isle of Wight (1969)",
-    "year": 1969
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Long Distance Operator (1965)",
-    "year": 1965
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - New York Sessions (1974)",
-    "year": 1974,
-    "format": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Renaldo and Clara Soundtrack (soniclovenoize)"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - The Ghost of Woody Guthrie (1961)",
-    "year": 1961
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - What Are You Trying To Say - Performing Artist Series - Vol. 6"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan and the Band - Basement Tapes (Acetate)"
-  },
-  {
-    "type": "release",
-    "label": "1965 - Bob Dylan - Solid Road",
-    "year": 1961,
-    "textFile": "MISC - 1961-1965 - Bob Dylan - Solid Road.txt"
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Bob Dylan Minnesota Tapes",
-    "year": 1961
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Eat The Document",
-    "year": 1966,
-    "format": "DVD",
-    "textFile": "MISC - 1966-XX-XX - Eat The Document [DVD].txt"
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Build A Ladder To The Stars (Pink Panther Records)",
-    "year": 1974,
-    "textFile": "MISC - 1974-XX-XX - Build A Ladder To The Stars (Pink Panther Records).txt"
-  },
-  {
-    "type": "release",
-    "label": "XX-XX - Real Cuts At Last",
-    "year": 1987,
-    "format": "Vinyl",
-    "textFile": "MISC - 1987-XX-XX - Real Cuts At Last [Vinyl].txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan & Traveling Wilburys - The Secret Tapes of Lucky Boo",
-    "textFile": "MISC - Bob Dylan & Traveling Wilburys - The Secret Tapes of Lucky Boo.txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan -  Blonde On Blonde (Original Stereo Mixes)",
-    "textFile": "MISC - Bob Dylan -  Blonde On Blonde (Original Stereo Mixes).txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - 1974 Soundboard Collection",
-    "textFile": "MISC - Bob Dylan - 1974 Soundboard Collection.txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Abandoned Desire",
-    "textFile": "MISC - Bob Dylan - Abandoned Desire.txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Blood On the Tapes.shn",
-    "textFile": "MISC - Bob Dylan - Blood On the Tapes.shn.txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Dimestore Medicine",
-    "version": "Silver CD",
-    "textFile": "MISC - Bob Dylan - Dimestore Medicine [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - First Gaslight Tape (Remaster)"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Highway 61 Demo Outtakes",
-    "textFile": "MISC - Bob Dylan - Highway 61 Demo Outtakes.txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Isle of Wight (1969)",
-    "year": 1969,
-    "textFile": "MISC - Bob Dylan - Isle of Wight (1969).txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Long Distance Operator (1965)",
-    "year": 1965
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - New York Sessions (1974)",
-    "year": 1974,
-    "format": "SBD",
-    "textFile": "MISC - Bob Dylan - New York Sessions (1974) [SBD].txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - Renaldo and Clara Soundtrack (soniclovenoize)",
-    "textFile": "MISC - Bob Dylan - Renaldo and Clara Soundtrack (soniclovenoize).txt"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - The Ghost of Woody Guthrie (1961)",
-    "year": 1961
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan - What Are You Trying To Say - Performing Artist Series - Vol. 6"
-  },
-  {
-    "type": "release",
-    "label": "Bob Dylan and the Band - Basement Tapes (Acetate)",
-    "textFile": "MISC - Bob Dylan and the Band - Basement Tapes (Acetate).txt"
   }
 ];
