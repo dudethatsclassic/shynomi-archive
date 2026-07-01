43bca480376beeb82f94cb4dac93ad8ffbf8dd21
dudethatsclassic <natescottsmith@icloud.com>
2026-07-01 13:37:19 -0400

    Auto-export misc: beastie-boys

--- data/misc/beastie-boys-misc.js
+++ data/misc/beastie-boys-misc.js
@@ -1,84 +1,94 @@
 const ARTIST_MISC_DATA = [
   {
     "type": "release",
-    "label": "Beastie Boys - Turning Japanese (1994-1995) (RTT-005)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
     "label": "Beastie Boys - Paul's Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Beastie Boys - Original Ill (1984-1987) (BM3000)",
-    "version": "Silver CD",
-    "format": "FLAC"
+    "label": "Beastie Boys - XX-XX-1988 / Desperado (Unreleased Single) (1988)",
+    "format": "FLAC",
+    "year": 1988,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Beastie Boys - Hip-Hop Sampler (Bootleg) - 1994 (SPRO 79481)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Beastie Boys - XX-XX-1988 / Desperado (Unreleased Single)",
-    "version": "SBD",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
     "label": "Beastie Boys - 1994 Demos",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Beastie Boys - Beastie Show Breaks (2003) (BB-SV-5)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Beastie Boys - Demos and Outtakes (Bootleg) (1994) (AK052)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Beastie Boys - Fight for Tibet (BLZD 148)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Beastie Boys - Hip-Hop Sampler (SPRO 79481) (1994)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Beastie Boys - Nastie Beastie Bootie (Hellfire TMOQ) (1998)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Beastie Boys - Ultra Tuff Tracks (1995) (BMCD38)",
-    "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Beastie Boys - Shake Your Rump {SE-460} (1994)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Beastie Boys - Hip-Hop Sampler {SPRO 79481} (1994)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Beastie Boys - Demos and Outtakes {AK052} (1994)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "SILVERS"
   },
   {
     "type": "release",
     "label": "Beastie Boys - Ultra Tuff Tracks {BMCD38} (1995)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1995,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Beastie Boys - Fight for Tibet {BLZD 148} (1996)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Beastie Boys - Original Ill (1984-1987) {BM3000} (1997)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1984,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Beastie Boys - Nastie Beastie Bootie {Hellfire TMOQ} (1998)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Beastie Boys - Beastie Show Breaks {BB-SV-5} (2003)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "SILVERS"
+  },
+  {
+    "type": "release",
+    "label": "Beastie Boys - Turning Japanese (1994-1995) {RTT-005} (1995)",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "SILVERS"
   }
 ];
