e3982442dd345d3dcf91eaa52e1976a3357f1754
dudethatsclassic <natescottsmith@icloud.com>
2026-07-01 16:28:47 -0400

    Auto-export misc: bjork

--- data/misc/bjork-misc.js
+++ data/misc/bjork-misc.js
@@ -1,19 +1,25 @@
 const ARTIST_MISC_DATA = [
   {
     "type": "release",
-    "label": "1980-1990 - Early and Rare (Assorted unreleased and live tracks)",
+    "label": "Bjork - 1980-1990 - Early and Rare (Assorted unreleased and live tracks)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1980,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Bjork - Violently Live (1993)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bjork - Atta Raddir - Icelandic Versions (Reykjavik, Iceland 2009)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2009,
+    "category": "OTHER"
   }
 ];
