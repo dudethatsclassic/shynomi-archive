f4ef4d080fcb3bfb1dfa69a2cb4a6ad3145a6945
dudethatsclassic <natescottsmith@icloud.com>
2026-07-01 18:39:46 -0400

    Auto-export misc: beck

--- data/misc/beck-misc.js
+++ data/misc/beck-misc.js
@@ -1,54 +1,71 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
-    "label": "1992 Demo (includes Metal Demos) - Complete",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "1988 Banjo Story",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "Beck and Dava 1990",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "1992 Demo Tape - Like the Beer",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "Don't Get Bent Out of Shape (Version 3)",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "Fresh Meat and Old Slabs",
-    "format": "FLAC"
+    "type": "release",
+    "label": "Beck - 1992 Demo (includes Metal Demos)",
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "The Mutations Conversations *Interview CD",
-    "version": "Silver CD",
-    "format": "FLAC"
+    "label": "Beck - Banjo Story (1988)",
+    "format": "FLAC",
+    "year": 1988,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
+    "label": "Beck - Beck and Dava (1990)",
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Beck - Demo Tape - Like the Beer (1992)",
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Beck - Don't Get Bent Out of Shape (v3)",
+    "format": "FLAC",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Beck - Fresh Meat and Old Slabs",
+    "format": "FLAC",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
     "label": "Beck - Early Rarities (2017)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2017,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Beck - Kill The Moonlight (Live 1994) (2020)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Beck - The Mutations Conversations *Interview CD",
+    "version": "Silver CD",
+    "format": "FLAC",
+    "category": "SILVERS"
   },
   {
     "type": "release",
     "label": "Beck - Quodlibet {B-DAT 025} (1996)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SILVERS"
   }
 ];
