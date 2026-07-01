99e9cbe7a175ef5d1534b1adcd7668c599e3104b
dudethatsclassic <natescottsmith@icloud.com>
2026-07-01 19:37:38 -0400

    Auto-export misc: bob-dylan

--- data/misc/bob-dylan-misc.js
+++ data/misc/bob-dylan-misc.js
@@ -1,7 +1,7 @@
 const ARTIST_MISC_DATA = [
   {
     "type": "release",
-    "label": "1961-1965 Bob Dylan - Solid Road",
+    "label": "Bob Dylan - Solid Road (1961-1965)",
     "version": "SBD",
     "format": "FLAC",
     "year": 1961,
@@ -9,7 +9,7 @@ const ARTIST_MISC_DATA = [
   },
   {
     "type": "release",
-    "label": "1974 Soundboard Collection (10 CD) Limited Edition 352/500",
+    "label": "Bob Dylan - Soundboard Collection (1974)",
     "version": "SBD",
     "format": "FLAC",
     "year": 1974,
@@ -17,32 +17,35 @@ const ARTIST_MISC_DATA = [
   },
   {
     "type": "release",
-    "label": "XX-XX-1974 - Build A Ladder To The Stars (Pink Panther Records)",
+    "label": "Bob Dylan - Build A Ladder To The Stars (Pink Panther Records) (1974)",
     "format": "FLAC",
     "year": 1974,
     "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "XX-XX-1978 - World Tour Anthology (LB-01755)",
+    "label": "Bob Dylan - World Tour Anthology {LB-01755} (1978)",
     "format": "SHN",
     "year": 1978,
-    "category": "OTHER",
-    "textFile": "MISC - 1978-XX-XX - World Tour Anthology (LB-01755) .txt"
-  },
-  {
-    "type": "label",
-    "label": "XX-XX-1987 / Real Cuts at Last"
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Abandonded Desire (2CD)",
+    "label": "Bob Dylan - Real Cuts at Last (1987)",
+    "version": "Vinyl",
+    "format": "FLAC",
+    "year": 1987,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Bob Dylan - Abandonded Desire",
     "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Blood on the Tapes",
+    "label": "Bob Dylan - Blood on the Tapes",
     "format": "FLAC",
     "category": "OTHER"
   },
@@ -54,20 +57,20 @@ const ARTIST_MISC_DATA = [
   },
   {
     "type": "release",
-    "label": "Bob Dylan - 1965 - Long Distance Operator",
+    "label": "Bob Dylan - Long Distance Operator (1965)",
     "format": "FLAC",
     "year": 1965,
     "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Bob Dylan - First Gaslight Tape {remaster}",
+    "label": "Bob Dylan - First Gaslight Tape (Remaster)",
     "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Bob Dylan - The Minnesota Tapes 1961 (Minneapolis Recordings)",
+    "label": "Bob Dylan - The Minnesota Tapes (Minneapolis Recordings) (1961)",
     "format": "FLAC",
     "year": 1961,
     "category": "OTHER"
@@ -80,38 +83,36 @@ const ARTIST_MISC_DATA = [
   },
   {
     "type": "release",
-    "label": "Dimestore Medicine (WC65)",
+    "label": "Bob Dylan - Dimestore Medicine {WC65}",
     "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Eat the Document (1966) - Documentary",
+    "label": "Bob Dylan - Eat the Document (1966)",
     "format": "DVD",
     "year": 1966,
     "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Isle of Wight (1969)",
+    "label": "Bob Dylan - Isle of Wight (1969)",
     "format": "DVD",
     "year": 1969,
     "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "The Witmark Years (Capricorn 2021-22)",
+    "label": "Bob Dylan - The Witmark Years (Capricorn 2021-22)",
     "format": "FLAC",
     "year": 2021,
-    "category": "OTHER",
-    "textFile": "MISC - The Witmark Years (Capricorn 2021-22).txt"
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "What Are You Trying To Say - Performing Artist Series - Vol. 6 (2 shows from 1965)",
+    "label": "Bob Dylan - What Are You Trying To Say - Performing Artist Series - Vol. 6",
     "version": "SBD",
     "format": "FLAC",
-    "year": 1965,
     "category": "OTHER"
   },
   {
@@ -159,10 +160,9 @@ const ARTIST_MISC_DATA = [
   },
   {
     "type": "release",
-    "label": "Bob Dylan and Mark Knopfler - The Jokermen (Scorpio-UK79)",
+    "label": "Bob Dylan and Mark Knopfler - The Jokermen {Scorpio-UK79}",
     "version": "Silver CD",
     "format": "FLAC",
-    "category": "OTHER",
-    "textFile": "MISC - Bob Dylan and Mark Knopfler - The Jokermen (Scorpio-UK79) [Silver].txt"
+    "category": "OTHER"
   }
 ];
