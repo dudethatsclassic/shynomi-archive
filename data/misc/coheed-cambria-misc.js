a83344008e6d07f26e1e711f4a28ab96a1f0ceb9
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:37:23 -0400

    Auto-export misc: coheed-cambria

--- data/misc/coheed-cambria-misc.js
+++ data/misc/coheed-cambria-misc.js
@@ -3,21 +3,7 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Live at the Starland Ballroom (DVD)",
     "version": "Official",
-    "format": "DVD"
-  },
-  {
-    "type": "release",
-    "label": "Coheed and Cambria - Live at the Starland Ballroom",
-    "format": "DVD"
-  },
-  {
-    "type": "release",
-    "label": "Coheed and Cambria - Live at the Starland Ballroom",
-    "format": "DVD"
-  },
-  {
-    "type": "release",
-    "label": "Coheed and Cambria - Live at the Starland Ballroom",
-    "format": "DVD"
+    "format": "DVD",
+    "category": "OTHER"
   }
 ];
