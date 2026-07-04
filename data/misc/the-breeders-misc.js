87a5cceaefface09e1e3b936dfdb5a729c1d04f4
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:41:15 -0400

    Auto-export misc: the-breeders

--- data/misc/the-breeders-misc.js
+++ data/misc/the-breeders-misc.js
@@ -3,54 +3,66 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "The Breeders - Baton Rouge 1994",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Cannonball 1993",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Cannonball Live",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Don't Need No Fat Man",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Double Trouble",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Europe 1993",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Hello Baton Rouge!",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Never Mind The Pixies Here's The Breeders",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Breeders - Sonic Ecstasy",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
