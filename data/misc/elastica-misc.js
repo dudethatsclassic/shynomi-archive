bd4e53adb3c208562abc4aa4f9b4c83031857fec
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:34:41 -0400

    Auto-export misc: elastica

--- data/misc/elastica-misc.js
+++ data/misc/elastica-misc.js
@@ -3,6 +3,8 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Elastica - 1994-XX-XX - Angels With Dirty Faces",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   }
 ];
