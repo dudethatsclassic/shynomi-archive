d22c346823e2be7e9929f37163ce1514b69989fb
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:39:01 -0400

    Auto-export misc: lynyrd-skynyrd

--- data/misc/lynyrd-skynyrd-misc.js
+++ data/misc/lynyrd-skynyrd-misc.js
@@ -3,27 +3,36 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Lynyrd Skynyrd - Double Trouble (11-23-1973 & 12-3-1973)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1973,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Lynyrd Skynyrd - The Quinvy Demos & More",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Lynyrd Skynyrd - Living Room Rehearsals (1976)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1976,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Lynyrd Skynyrd - Muscle Shoals Demos (1990-1991)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Lynyrd Skynrd - Old Grey Whistle Test (1975)",
     "version": "TV",
-    "format": "TS"
+    "format": "TS",
+    "year": 1975,
+    "category": "OTHER"
   }
 ];
