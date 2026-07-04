c58fb408c3794ad7304dbe75a6cfd0766f0340f1
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:34:44 -0400

    Auto-export misc: everclear

--- data/misc/everclear-misc.js
+++ data/misc/everclear-misc.js
@@ -3,12 +3,16 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Everclear - 3-9-2003 - Loveline",
     "version": "FM",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 2003,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Everclear - 7-16-2000 - Loveline",
     "version": "FM",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 2000,
+    "category": "OTHER"
   }
 ];
