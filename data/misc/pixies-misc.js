9f5f3ddba3b80e8b94a3b957ae77af4f9d0f44f9
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:35:37 -0400

    Auto-export misc: pixies

--- data/misc/pixies-misc.js
+++ data/misc/pixies-misc.js
@@ -1,115 +1,148 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Pixies - 1986 Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1986,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Give Me Ecstasy",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Souls On Fire",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - All Over The World (1989)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Black Hunter (1989) (CRC mismatch)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Bone Machine (1989)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Jaap Edenhall (1991)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1991,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Live (Aulica) (1989)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Planet Of Sound (1992)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Rough Diamonds (1987-1991)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1987,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Situation Red (1992)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Subbacultcha (1991)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1991,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - The B-Side File",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - The B-Side File 2nd Edition",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - The B-Side File 3rd Edition",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - This Was My Life (1989) (CRC mismatch)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Timeless Stars (1989)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Velouria Live (1990)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pixies - Zurich 1989",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   }
 ];
