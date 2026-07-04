ffd2a90c037c1583ea2f4ad71c0e8213509c0566
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:40:49 -0400

    Auto-export misc: staind

--- data/misc/staind-misc.js
+++ data/misc/staind-misc.js
@@ -3,6 +3,7 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Staind & Godsmack Westwood One Absolutely Live In Concert #03-40 (Pre-FM)",
     "version": "preFM",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
