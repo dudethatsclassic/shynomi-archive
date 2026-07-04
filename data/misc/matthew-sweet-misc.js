fd0438e3c919fe88274c5fdfaabb5445ff68172a
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:35:26 -0400

    Auto-export misc: matthew-sweet

--- data/misc/matthew-sweet-misc.js
+++ data/misc/matthew-sweet-misc.js
@@ -1,43 +1,52 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Matthew Sweet - Demos (1989-1991)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Matthew Sweet - 100% Fun Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Matthew Sweet - Earth Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Matthew Sweet - Sweet Demos 2",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Matthew Sweet - Superdeformed #1",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Matthew Sweet - Superdeformed #2",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Matthew Sweet - Supervixen",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Matthew Sweet - Goodfriend - Another Take On Girlfriend (Promo CD)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
