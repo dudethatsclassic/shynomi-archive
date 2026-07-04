f0c012f5a0b9e6c937d0eb3a6eedb6b7c62364c2
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:35:13 -0400

    Auto-export data: K's Choice

--- data/k-s-choice.js
+++ data/k-s-choice.js
@@ -20,7 +20,7 @@ const ARTIST_DATA = {
       "state": "MA",
       "location": "Waltham, MA",
       "venue": "Brandeis University",
-      "notes": "Sennheiser MKE-2002 \u00b7 stevemtl",
+      "notes": "Sennheiser MKE-2002 · stevemtl",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "1994-12-10 - Waltham, MA - Brandeis University (Sennheiser MKE-2002) (stevemtl) [AUD].txt"
@@ -47,7 +47,7 @@ const ARTIST_DATA = {
       "state": "The Netherlands",
       "location": "Maastricht, The Netherlands",
       "venue": "Muziekgieterij",
-      "notes": "DPA 4061 \u00b7 stl",
+      "notes": "DPA 4061 · stl",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2023-05-12 - Maastricht, The Netherlands - Muziekgieterij (DPA 4061) (stl) [AUD].txt"
@@ -61,7 +61,7 @@ const ARTIST_DATA = {
       "state": "Belgium",
       "location": "Brussels, Belgium",
       "venue": "Ancienne Belgique",
-      "notes": "DPA 4061SG \u00b7 stl",
+      "notes": "DPA 4061SG · stl",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2023-05-17 - Brussels, Belgium - Ancienne Belgique (DPA 4061SG) (stl) [AUD].txt"
@@ -71,14 +71,14 @@ const ARTIST_DATA = {
       "month": 5,
       "day": 27,
       "dateStr": "5-27-2023",
-      "city": "Lie\u0300ge",
+      "city": "Liège",
       "state": "Belgium",
-      "location": "Lie\u0300ge, Belgium",
+      "location": "Liège, Belgium",
       "venue": "Classic 21 Festival 2023",
       "notes": "Country",
       "version": "FM",
       "format": "FLAC",
-      "textFile": "2023-05-27 - Lie\u0300ge, Belgium - Classic 21 Festival 2023 - Country Hall [FM].txt"
+      "textFile": "2023-05-27 - Liège, Belgium - Classic 21 Festival 2023 - Country Hall [FM].txt"
     },
     {
       "year": 2023,
@@ -89,7 +89,7 @@ const ARTIST_DATA = {
       "state": "Leuven",
       "location": "Oude Markt, Leuven, Belgium",
       "venue": "Beleuvenissen",
-      "notes": "CA-14 \u00b7 trebor",
+      "notes": "CA-14 · trebor",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2023-07-07 - Oude Markt, Leuven, Belgium - Beleuvenissen (CA-14) (trebor) [AUD].txt"
@@ -103,7 +103,7 @@ const ARTIST_DATA = {
       "state": "Belgium",
       "location": "Deurne, Belgium",
       "venue": "OLT Rivierenhof",
-      "notes": "DPA 4061 \u00b7 stl",
+      "notes": "DPA 4061 · stl",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2023-08-22 - Deurne, Belgium - OLT Rivierenhof (DPA 4061) (stl) [AUD].txt"
@@ -117,7 +117,7 @@ const ARTIST_DATA = {
       "state": "Belgium",
       "location": "Deurne, Belgium",
       "venue": "Openluchttheater Rivierenhof",
-      "notes": "Sony ECM 717 \u00b7 tisnix",
+      "notes": "Sony ECM 717 · tisnix",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2023-08-23 - Deurne, Belgium - Openluchttheater Rivierenhof  (Sony ECM 717) (tisnix) [AUD].txt"
@@ -131,11 +131,10 @@ const ARTIST_DATA = {
       "state": "Belgium",
       "location": "Deurne, Belgium",
       "venue": "OLT Rivierenhof",
-      "notes": "DPA 4061 \u00b7 stl",
+      "notes": "DPA 4061 · stl",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "2023-08-23 - Deurne, Belgium - OLT Rivierenhof (DPA 4061) (stl) [AUD].txt"
     }
-  ],
-  "misc": []
+  ]
 };
