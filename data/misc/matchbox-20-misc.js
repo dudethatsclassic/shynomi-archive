254bd755cd0c55bfea54ca31706651465eb543e6
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:39:12 -0400

    Auto-export misc: matchbox-20

--- data/misc/matchbox-20-misc.js
+++ data/misc/matchbox-20-misc.js
@@ -1,12 +1,39 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
-    "label": "Matchbox Twenty - 666 AM"
+    "type": "release",
+    "label": "Matchbox Twenty - 666 AM",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Matchbox Twenty - Hamburg, Germany - Grosse Freheit {DP6} (2000)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2000,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "1998 - Matchbox Twenty - Back 2 Good (CDS) {7567955392}",
+    "version": "Official",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2001 - Matchbox Twenty - Last Beautiful Girl {7567-85167-2AT0120CD} (CDS)",
+    "version": "Official",
+    "format": "FLAC",
+    "year": 2001,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2007 - Matchbox Twenty - All Your Reasons {7567899530} (CDS)",
+    "version": "Official",
+    "format": "FLAC",
+    "year": 2007,
+    "category": "SINGLES"
   }
 ];
