73447b095f9ce33649f5dfe0a1afc4e5d36493f1
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:41:10 -0400

    Auto-export misc: taylor-swift

--- data/misc/taylor-swift-misc.js
+++ data/misc/taylor-swift-misc.js
@@ -3,6 +3,8 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Taylor Swift- Today Show 2010 [TV]",
     "version": "TV",
-    "format": "TS"
+    "format": "TS",
+    "year": 2010,
+    "category": "OTHER"
   }
 ];
