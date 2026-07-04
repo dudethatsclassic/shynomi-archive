6fdd399e1e7b21d6d81782b329106654180f2e29
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:37:52 -0400

    Auto-export misc: elton-john

--- data/misc/elton-john-misc.js
+++ data/misc/elton-john-misc.js
@@ -3,11 +3,15 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Elton John - Tumbleweed Collection 1970-1971",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1970,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Elton John - Piano Demos 1968-1971",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1968,
+    "category": "OTHER"
   }
 ];
