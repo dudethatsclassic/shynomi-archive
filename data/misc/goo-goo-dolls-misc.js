f69170691397bbd500f8885d4c280ea9dd8ac114
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:38:08 -0400

    Auto-export misc: goo-goo-dolls

--- data/misc/goo-goo-dolls-misc.js
+++ data/misc/goo-goo-dolls-misc.js
@@ -3,17 +3,44 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Live In Buffalo 2004",
     "version": "PRO",
-    "format": "MKV"
+    "format": "MKV",
+    "year": 2004,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Goo Goo Dolls - Superstar Car Wash Demos (1992)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Goo Goo Dolls - Gaa Gaa {D2P 009} (1996)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "1995 Goo Goo Dolls - Name (CDS) {W0333CD} (1995)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1995 Goo Goo Dolls - Only One (CDS) {W0293CD} (1995)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2000 Goo Goo Dolls - Broadway (CDS) {D2089} (2000)",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "SINGLES"
   }
 ];
