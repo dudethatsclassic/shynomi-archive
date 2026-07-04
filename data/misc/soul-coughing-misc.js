6e3fdea470bf8caee5df86bcb01cae21104a6069
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:36:13 -0400

    Auto-export misc: soul-coughing

--- data/misc/soul-coughing-misc.js
+++ data/misc/soul-coughing-misc.js
@@ -1,52 +1,114 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Ruby Vroom Roughs 1994-05-04",
-    "format": "FLAC 24"
+    "format": "FLAC 24",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Down to This {Slash - D997} (1995)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Hello Recording Club EP (1996)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Made Especially for You by Soul Coughing {PRO-CD-9127} (1997)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Lust in Phaze The Best of Soul Coughing {R2 78281} (2002}",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2002,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Rennes, France 12.03.94 {KUF 0023} (2003)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Tokyo, Japan 02.03.1997 {KUF 0024} (2003)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - New York, NY 08.16.99 {KUF 0025} (2005)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2005,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Live Rarities {KUF 0027} (2005)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2005,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Soul Coughing - Berlin-Amsterdam 1997 (2005)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Soul Coughing - Sugar Free Jazz - Screenwriter's Blue's (Promo) {PRO-CD-7741-R} (1994)",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Soul Coughing - Sugar Free Jazz {943582-2} (1995)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Soul Coughing - Super Bon Bon {LASCD60} (1996)",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Soul Coughing - Soft Serve {PRO-CD-8818} (1997)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Soul Coughing - Circles (Promo) {PRO-CD-9555-R} (1998)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Soul Coughing - Rolling {PRO-CD-9676} (1999)",
+    "format": "FLAC",
+    "year": 1999,
+    "category": "SINGLES"
   }
 ];
