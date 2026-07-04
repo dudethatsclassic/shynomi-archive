95050a01dd25739881362afc0e0e97c6b1eeddf2
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:38:19 -0400

    Auto-export misc: hootie-the-blowfish

--- data/misc/hootie-the-blowfish-misc.js
+++ data/misc/hootie-the-blowfish-misc.js
@@ -1,25 +1,9 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
-    "label": "Hootie & the Blowfish - Live in Charleston (2006)",
-    "format": "FLAC"
-  },
-  {
     "type": "release",
     "label": "Hootie & the Blowfish - Live in Charleston (2006)",
+    "format": "FLAC",
     "year": 2006,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Hootie & the Blowfish - Live in Charleston (2006)",
-    "year": 2006,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Hootie & the Blowfish - Live in Charleston (2006)",
-    "year": 2006,
-    "format": "FLAC"
+    "category": "OTHER"
   }
 ];
