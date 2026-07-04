012f78f17078b1a7a155918c31bcf182291e7dd6
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:36:10 -0400

    Auto-export misc: soul-asylum

--- data/misc/soul-asylum-misc.js
+++ data/misc/soul-asylum-misc.js
@@ -3,6 +3,8 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Soul Asylum - The Lost Album (MO10004) (1993)",
     "version": "Silver CD",
-    "format": "APE"
+    "format": "APE",
+    "year": 1993,
+    "category": "OTHER"
   }
 ];
