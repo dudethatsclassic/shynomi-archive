84c89581ea1fa164afd0be464f2995f647ae4602
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:37:27 -0400

    Auto-export misc: collective-soul

--- data/misc/collective-soul-misc.js
+++ data/misc/collective-soul-misc.js
@@ -3,12 +3,16 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Collective Soul - 2004 Youth Movement Broadcast",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2004,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Collective Soul - Live At The Print Shop (2024) (24/48)",
     "version": "WEB",
-    "format": "FLAC HD"
+    "format": "FLAC HD",
+    "year": 2024,
+    "category": "OTHER"
   }
 ];
