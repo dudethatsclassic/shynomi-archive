1de7e33e1a25a594486e3114fa1ff50acb4386ae
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:34:26 -0400

    Auto-export misc: belly

--- data/misc/belly-misc.js
+++ data/misc/belly-misc.js
@@ -3,6 +3,8 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Belly - Chicago 1995 & Annapolis 1996 - CASS(M) (stevemtl transfer)",
     "version": "FM",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OTHER"
   }
 ];
