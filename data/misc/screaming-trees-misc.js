e2ff986d30eda7bfd7fa672df9f25ef98f233eae
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:36:00 -0400

    Auto-export misc: screaming-trees

--- data/misc/screaming-trees-misc.js
+++ data/misc/screaming-trees-misc.js
@@ -1,13 +1,16 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Screaming Trees - 1985-1988 Demos (Cassette)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1985,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Screaming Trees - 10 Tons of Fun (KTS180)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
