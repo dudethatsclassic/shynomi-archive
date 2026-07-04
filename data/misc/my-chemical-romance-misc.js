93597405814dec7d7da2baeae89c41e9a210a344
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:39:31 -0400

    Auto-export misc: my-chemical-romance

--- data/misc/my-chemical-romance-misc.js
+++ data/misc/my-chemical-romance-misc.js
@@ -3,6 +3,8 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "My Chemical Romance - Life on the Murder Scene (2006) [DVD]",
     "version": "Original Release",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 2006,
+    "category": "OTHER"
   }
 ];
