29d5b4d842a1ff1cd46be7562c8d2e14ff9b2c69
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 11:42:24 -0400

    Auto-export misc: guster

--- data/misc/guster-misc.js
+++ data/misc/guster-misc.js
@@ -1,87 +1,37 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Guster - The Pasty Tapes (1999)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1999,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Guster - The Meowstro Sings- Guster's Keep It Together (2003)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Guster - MTV2 Album Covers (2004)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2004,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Guster - 3 Live Sets (Limited Edition) (2014)",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "Guster - Live With The Redacted Symphony (2013)",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - 3 Live Sets (2014)",
+    "format": "FLAC",
     "year": 2014,
-    "format": "FLAC"
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Guster - Live With The Redacted Symphony (2013)",
+    "format": "FLAC",
     "year": 2013,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - MTV2 Album Covers (2004)",
-    "year": 2004,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - The Meowstro Sings- Guster's Keep It Together (2003)",
-    "year": 2003,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - The Pasty Tapes (1999)",
-    "year": 1999,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - 3 Live Sets (2014)",
-    "year": 2014,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - Live With The Redacted Symphony (2013)",
-    "year": 2013,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - MTV2 Album Covers (2004)",
-    "year": 2004,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - The Meowstro Sings- Guster's Keep It Together (2003)",
-    "year": 2003,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - The Pasty Tapes (1999)",
-    "year": 1999,
-    "format": "FLAC"
+    "category": "OTHER"
   }
 ];
