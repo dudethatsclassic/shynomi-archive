bb00dc93c564f9e85da3b50889186ed5e3b2318f
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:37:30 -0400

    Auto-export misc: crash-test-dummies

--- data/misc/crash-test-dummies-misc.js
+++ data/misc/crash-test-dummies-misc.js
@@ -1,25 +1,32 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Crash Test Dummies - 1988 - 1989 Demos (Original Recipe and Demo Tape 2)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1988,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Crash Test Dummies - XX-XX-1990 / Indie Street Spotlight (1988-90)",
     "version": "TV",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 1990,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Crash Test Dummies - TV Compilation - SNL, Jay Leno, Letterman, Conan",
     "version": "TV",
-    "format": "DVD"
+    "format": "DVD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Crash Test Dummies 1992-1993 - TV Compilations",
     "version": "TV",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 1992,
+    "category": "OTHER"
   }
 ];
