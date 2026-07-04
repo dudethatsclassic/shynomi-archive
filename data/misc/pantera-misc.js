a6de5cb06c8eb6080a389ced320c1de94e0175ff
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:39:56 -0400

    Auto-export misc: pantera

--- data/misc/pantera-misc.js
+++ data/misc/pantera-misc.js
@@ -3,60 +3,79 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Pantera - 1992 - WW1 Absolutely Live High Voltage (preFM)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - 1992-1994 preFM Radio (1992-06-26 & 1994-05-04)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - 1992-1993 - Live USA (LSDCD 152)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - Castle Domination 1994 (OCTO 010)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - Damnation (1994) (BC 030)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - Europe 1993 (LSCD 51537)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - Four Cow-Boys From Hell (1993) (MECD 1126)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - Fucking Hostile Cowboys From Hell (1993) (PAN 001)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Pantera - In The Skin (1993) (DE 012)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Dimebag Darrell - Whiskey Road Demo",
     "version": "WEB",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
