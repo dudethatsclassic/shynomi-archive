bad8fa184f9843d35e72b3a0ff324e7fc6dcd28b
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:39:35 -0400

    Auto-export misc: nada-surf

--- data/misc/nada-surf-misc.js
+++ data/misc/nada-surf-misc.js
@@ -1,17 +1,72 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Nada Surf - The MySpace Transmissions {bark70.5} (2008)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Nada Surf - Standing at the Gates - The Songs of Nada Surf's Let Go (2018)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2018,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Nada Surf - Peaceful Ghosts (Live) (2016)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2016,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Nada Surf - Popular (CDS) {7559-64259-9} (1996)",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Nada Surf - Deeper Well (CDS) {7559-64187-9} (1997)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Nada Surf - Black & White - Spooky {PRO 1012} (1998) {Promo EP}",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Nada Surf - Inside of Love {HVN 133CD} (2002)",
+    "format": "FLAC",
+    "year": 2002,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Nada Surf - Inside Of Love (CDS) {LIBSP5075.2} (2002)",
+    "format": "FLAC",
+    "year": 2002,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Nada Surf - L'Aventurier {72435473842} (2003)",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Nada Surf - Bank Robber Music Compilation Volume 16.5 (2008)",
+    "format": "FLAC",
+    "year": 2008,
+    "category": "SINGLES"
   }
 ];
