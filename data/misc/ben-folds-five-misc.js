0cb98a422220328490f32e82c3913fcf14ffbdb2
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:36:52 -0400

    Auto-export misc: ben-folds-five

--- data/misc/ben-folds-five-misc.js
+++ data/misc/ben-folds-five-misc.js
@@ -3,33 +3,40 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Ben Folds Five - Complete Sessions at West 54th - 2001",
     "version": "Official",
-    "format": "MKV"
+    "format": "MKV",
+    "year": 2001,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Ben Folds Five - Playlist - The Very Best Of Ben Folds Five",
     "version": "Official",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Ben Folds Five - Rare",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Ben Folds Five - Rare 2 + EXTRA",
-    "format": "FLAC + MP4"
+    "format": "FLAC + MP4",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Ben Folds Five - Beginnings",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Ben Folds Five - Uncut (VH1)",
     "version": "TV",
-    "format": "DVD"
+    "format": "DVD",
+    "category": "OTHER"
   }
 ];
