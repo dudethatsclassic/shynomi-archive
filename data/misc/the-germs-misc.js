ccda0bb66b5e0f20d294178bf0023289d81ea018
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:34:10 -0400

    Auto-export misc: the-germs

--- data/misc/the-germs-misc.js
+++ data/misc/the-germs-misc.js
@@ -3,18 +3,23 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "The Germs - Media Blitz (Cleopatra CLP 2289) (2008)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Germs - Germicide - Live At The Whisky 1977 (BOMP! BCD4070)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1977,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Germs - Rock N' Rule",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
