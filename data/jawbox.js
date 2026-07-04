cfb0b139cfcd1003a922df4908802168fb030f27
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:35:06 -0400

    Auto-export data: Jawbox

--- data/jawbox.js
+++ data/jawbox.js
@@ -18,11 +18,10 @@ const ARTIST_DATA = {
       "state": "MA",
       "location": "Cambridge, MA",
       "venue": "Middle East",
-      "notes": "dan \u00b7 zootype transfer",
+      "notes": "dan · zootype transfer",
       "version": "AUD",
       "format": "FLAC",
       "textFile": "1993-02-21 - Cambridge, MA - Middle East (dan) (zootype transfer) [AUD].txt"
     }
-  ],
-  "misc": []
+  ]
 };
