00fbb4e544c8f2cad2fb8749271ca812383c746e
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:37:08 -0400

    Auto-export misc: buddy-holly

--- data/misc/buddy-holly-misc.js
+++ data/misc/buddy-holly-misc.js
@@ -1,12 +1,16 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "1955-1959 / Buddy Holly - Apartment Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1955,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Buddy Holly - The Complete Buddy Holly Vol. 1-10 (1949-1956)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1949,
+    "category": "OTHER"
   }
 ];
