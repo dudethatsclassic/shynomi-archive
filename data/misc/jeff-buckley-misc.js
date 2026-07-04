92d14bc420e0c0c1fb2c8d9b5e1eae52a0f6a771
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:38:29 -0400

    Auto-export misc: jeff-buckley

--- data/misc/jeff-buckley-misc.js
+++ data/misc/jeff-buckley-misc.js
@@ -1,47 +1,60 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Jeff Buckley - Babylon Dungeon Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Jeff Buckley - Babylon Dungeon Demos (1990)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Jeff Buckley - Every Monday Night",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Jeff Buckley - Garbage Can Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Jeff Buckley - Grace Outtakes",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Jeff Buckley - Grace Sessions & Outtakes",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Jeff Buckley - Demos (1990-1997) - Babylon Dungeons Sessions (Sept 1990) / Trash Can Tapes (circa '96)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Various Dates - Every Monday Night 1992 & 1993 Sin-e Cafe",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "1993-09-XX - Grace Outtakes",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   }
 ];
