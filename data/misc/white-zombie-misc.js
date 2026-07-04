573d7f6477c7639a5c3eb825dd7a3b3fe4d5a396
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:41:44 -0400

    Auto-export misc: white-zombie

--- data/misc/white-zombie-misc.js
+++ data/misc/white-zombie-misc.js
@@ -3,18 +3,24 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "White Zombie - Astro-Junkies (1992-1995)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "White Zombie - Resurrection Day (Live 1992-1993)  (KTS 217)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "White Zombie - Kicking In Heaven's Door {KTS-516} (1996)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OTHER"
   }
 ];
