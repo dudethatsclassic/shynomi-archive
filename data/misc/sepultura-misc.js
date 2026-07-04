245efaa3dad0c558aa3a94f1e1d1b8c237afb34c
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:40:22 -0400

    Auto-export misc: sepultura

--- data/misc/sepultura-misc.js
+++ data/misc/sepultura-misc.js
@@ -3,12 +3,16 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Sepultura - Pain & Hate (1996) (Cobra Records)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Sepultura - Sony Music TV (1994)",
     "version": "TV",
-    "format": "MPG"
+    "format": "MPG",
+    "year": 1994,
+    "category": "OTHER"
   }
 ];
