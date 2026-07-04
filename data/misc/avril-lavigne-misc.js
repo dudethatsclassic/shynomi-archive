a40747c87aff50174d6dec97bfbb996bc9790482
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:36:48 -0400

    Auto-export misc: avril-lavigne

--- data/misc/avril-lavigne-misc.js
+++ data/misc/avril-lavigne-misc.js
@@ -3,54 +3,71 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Avril Lavigne - Do The Best",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne - F**kin' Sk8er Girl {W.B. 065} (2003)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne - Sk8er Girl Live {SG211102} (2003)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne - Summer Sonic 2014 Tokyo (2014)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2014,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne - Summer Sonic 2014 Tokyo (PRO CDR)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2014,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne - The Bonez-Tour 2004 {AL270904} (2004)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2004,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne - Unprotected {N.W. 025} (2003)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne - Yokohama 2022 (PRO CDR)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2022,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Avril Lavigne- Yokohama 2014 (PRO CDR)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2014,
+    "category": "OTHER"
   }
 ];
