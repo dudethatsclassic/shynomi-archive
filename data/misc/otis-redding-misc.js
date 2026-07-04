505262707838addd4526a72bde8f81262d6e275e
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:39:53 -0400

    Auto-export misc: otis-redding

--- data/misc/otis-redding-misc.js
+++ data/misc/otis-redding-misc.js
@@ -3,11 +3,13 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "X-X-1967 / A Soupcan of Soul",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1967,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Otis Redding - (Sittin' On) the Dock of the Bay (HDTracks) (24:192)",
+    "label": "Otis Redding - (Sittin' On) the Dock of the Bay (HDTracks) (24/192)",
     "format": "FLAC HD",
     "category": "OFFICIAL RELEASES"
   }
