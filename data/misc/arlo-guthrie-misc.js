39481dab98607f35e62d64eb7f782f435cd384ea
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:36:43 -0400

    Auto-export misc: arlo-guthrie

--- data/misc/arlo-guthrie-misc.js
+++ data/misc/arlo-guthrie-misc.js
@@ -3,11 +3,14 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Alice's Restaurant 1966-67",
     "version": "FM",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1966,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Alice's Restaurant  (10 Versions)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
