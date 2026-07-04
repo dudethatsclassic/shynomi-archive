7e30cd4c5fe9ffe3b7aeb341f055d7e459ee06e5
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 12:07:17 -0400

    Auto-export misc: the-offspring

--- data/misc/the-offspring-misc.js
+++ data/misc/the-offspring-misc.js
@@ -3,436 +3,436 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "The Offspring - Baghdad EP (1991)",
     "version": "Vinyl",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1991,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Offspring - Live Smash (PRINT-010)",
     "version": "Silver CD",
     "format": "FLAC",
+    "category": "OTHER",
     "textFile": "MISC - The Offspring - Live Smash (PRINT-010) [Silver].txt"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Offspring - 1986-87 Songs (Demo Cassette) (24-96)",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "The Offspring - Meaning Of Job (STTP 063)",
-    "version": "Silver CD",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "The Offspring - Raw & Down Under In 1995 (Live) {8717662588822} (1995)",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "The Offspring (86460-2)",
-    "year": 1989,
     "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "The Offspring (EICP 11) Japan",
-    "year": 1989,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Ignition (6424-2)",
-    "year": 1992,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Ignition (ESCA 6144) Japan",
-    "year": 1992,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Smash (86432-2)",
-    "year": 1994,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Club Me",
-    "year": 1997,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Ixnay On The Hombre (6487-2)",
-    "year": 1997,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Americana (CK 69661)",
-    "year": 1998,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Americana (ESCA 7393) Japan",
-    "year": 1998,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Conspiracy Of One (498481-2)",
-    "year": 2000,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Original Prankster (ESCA 8241) Japan",
-    "year": 2000,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Splinter (512201-2)",
-    "year": 2003,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Splinter (SICP 500) Japan",
-    "year": 2003,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Greatest Hits (COL 518746 2)",
-    "year": 2005,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Rise And Fall, Rage And Grace (88697-02908-2)",
-    "year": 2008,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Rise And Fall, Rage And Grace (SICP-1578~9) Japan",
-    "year": 2008,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Happy Hour!",
-    "year": 2010,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Days Go By (88697-64763-2)",
-    "year": 2012,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Days Go By (SICP 3500) Japan",
-    "year": 2012,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES",
-    "textFile": "MISC - 2012 Days Go By (SICP 3500) Japan [FLAC].txt"
-  },
-  {
-    "type": "release",
-    "label": "Summer Nationals",
-    "year": 2014,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Let The Bad Times Roll (2021) (24:96)",
-    "year": 2021,
-    "format": "FLAC HD",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Come Out And Play",
-    "year": 1994,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Self Esteem",
-    "year": 1994,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Gotta Get Away",
-    "year": 1995,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "All I Want (664077 2)",
-    "year": 1997,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Gone Away (6498-2)",
-    "year": 1997,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Pretty Fly (For A White Guy) (Australia)",
-    "year": 1998,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "She's Got Issues (667867 2)",
-    "year": 1999,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "The Kids Aren't Alright",
-    "year": 1999,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Why Don't You Get A Job (ESCA 8020)",
-    "year": 1999,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Totalimmortal",
-    "year": 2000,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Defy You",
-    "year": 2001,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Million Miles Away (6712012)",
-    "year": 2001,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Want You Bad (ESCA 8280) Japan",
-    "year": 2001,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Hit That",
-    "year": 2003,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "(Can't Get My) Head Around You (SICP 604) Japan",
-    "year": 2004,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Can't Repeat",
-    "year": 2005,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Hammerhead",
-    "year": 2008,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "You're Gonna Go Far, Kid",
-    "year": 2008,
-    "format": "FLAC",
-    "category": "SINGLES"
+    "year": 1986,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Offspring - Meaning Of Job {STTP 063}",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Americana (ESCA 7393) Japan",
+    "label": "The Offspring - Raw & Down Under In 1995 (Live) {8717662588822} (1995)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "1989 The Offspring (86460-2)",
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1989 The Offspring (EICP 11) Japan",
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1992 Ignition (6424-2)",
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1992 Ignition (ESCA 6144) Japan",
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1994 Smash (86432-2)",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1994 Smash (ESCA 6075) Japan",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1997 Ixnay On The Hombre (6487-2)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1997 Ixnay On The Hombre (ESCA 6570) Japan",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1997 Club Me EP",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1998 Americana (CK 69661)",
+    "format": "FLAC",
     "year": 1998,
-    "format": "FLAC",
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Original Prankster (ESCA 8241) Japan",
+    "label": "1998 Americana (ESCA 7393) Japan",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2000 Conspiracy Of One (498481-2)",
+    "format": "FLAC",
     "year": 2000,
-    "format": "FLAC",
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Rise And Fall, Rage And Grace (SICP-1578~9) Japan",
+    "label": "2000 Conspiracy Of One (ESCA 8247) Japan",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2003 Splinter (512201-2)",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2003 - Splinter (SICP 500) Japan",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2005 Greatest Hits (COL 518746 2) [FLAC]",
+    "format": "FLAC",
+    "year": 2005,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2008 Rise And Fall, Rage And Grace (88697-02908-2)",
+    "format": "FLAC",
     "year": 2008,
-    "format": "FLAC",
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Happy Hour!",
+    "label": "2008 - Rise And Fall, Rage And Grace (SICP 2049) Japan",
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2008 - Rise And Fall, Rage And Grace (SICP-1578~9) Japan",
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2010 Happy Hour!",
+    "format": "FLAC",
     "year": 2010,
-    "format": "FLAC",
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Let The Bad Times Roll (Japan Edition)",
+    "label": "2010 Happy Hour! (SICP 2727) Japan",
+    "format": "FLAC",
+    "year": 2010,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2012 Days Go By (88697-64763-2)",
+    "format": "FLAC",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2012 Days Go By (SICP 3500) Japan",
+    "format": "FLAC",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2014 Summer Nationals EP",
+    "format": "FLAC",
+    "year": 2014,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "2021 Let The Bad Times Roll (2021) (24/96)",
+    "format": "FLAC HD",
     "year": 2021,
-    "format": "FLAC",
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "All I Want (CSK 9315)",
-    "year": 1997,
+    "label": "2021 Let The Bad Times Roll (2021) (Japan Edition)",
     "format": "FLAC",
+    "year": 2021,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "1994 Come Out And Play",
+    "format": "FLAC",
+    "year": 1994,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Gone Away (664338-2)",
-    "year": 1997,
+    "label": "1994 Self Esteem",
     "format": "FLAC",
+    "year": 1994,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Pretty Fly (For A White Guy) (USA)",
+    "label": "1995 Gotta Get Away",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1997 All I Want {664077 2}",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1997 All I Want {CSK 9315}",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1997 Gone Away {6498-2}",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1997 Gone Away {664338-2}",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1997 The Meaning of Life {JPN ESCA 6817}",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1998 Pretty Fly (For A White Guy) (Australia)",
+    "format": "FLAC",
     "year": 1998,
-    "format": "FLAC",
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "She's Got Issues (668377 2)",
+    "label": "1998 Pretty Fly (For A White Guy) (USA)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "1999 She's Got Issues {667867 2}",
+    "format": "FLAC",
     "year": 1999,
-    "format": "FLAC",
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Why Don't You Get A Job",
+    "label": "1999 She's Got Issues {668377 2}",
+    "format": "FLAC",
     "year": 1999,
-    "format": "FLAC",
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Million Miles Away (6713632)",
-    "year": 2001,
+    "label": "1999 Why Don't You Get A Job {ESCA 8020} Japan",
     "format": "FLAC",
+    "year": 1999,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Want You Bad",
-    "year": 2001,
+    "label": "1999 The Kids Aren't Alright",
     "format": "FLAC",
+    "year": 1999,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Coming For You",
-    "year": 2015,
+    "label": "1999 Why Don't You Get A Job",
     "format": "FLAC",
+    "year": 1999,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Original Prankster (ESCA 8241) Japan",
+    "label": "1999 Why Don't You Get A Job {ESCA 8020} Japan",
+    "format": "FLAC",
+    "year": 1999,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2000 Original Prankster",
+    "format": "FLAC",
     "year": 2000,
-    "format": "FLAC",
-    "category": "OFFICIAL RELEASES"
-  },
-  {
-    "type": "release",
-    "label": "Can't Get My Head Around You",
-    "year": 2004,
-    "format": "FLAC",
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "The Offspring - 1986-7 Songs (Demo Cassette) (24-96)",
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "The Offspring - Baghdad EP (1991)",
-    "year": 1991,
-    "format": "Vinyl"
-  },
-  {
-    "type": "release",
-    "label": "The Offspring - 1986-7 Songs (Demo Cassette) (24-96)",
+    "label": "2000 Original Prankster {ESCA 8241} Japan",
     "format": "FLAC",
-    "textFile": "MISC - The Offspring - 1986-7 Songs (Demo Cassette) (24-96) [FLAC].txt"
+    "year": 2000,
+    "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "The Offspring - Baghdad EP (1991)",
-    "year": 1991,
-    "format": "Vinyl"
+    "label": "2000 Totalimmortal",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2001 Defy You",
+    "format": "FLAC",
+    "year": 2001,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2001 Million Miles Away {6712012}",
+    "format": "FLAC",
+    "year": 2001,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2001 Million Miles Away {6713632}",
+    "format": "FLAC",
+    "year": 2001,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2001 Want You Bad",
+    "format": "FLAC",
+    "year": 2001,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2001 Want You Bad {ESCA 8280} Japan",
+    "format": "FLAC",
+    "year": 2001,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2003 Hit That",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2004 (Can't Get My) Head Around You {SICP 604} Japan",
+    "format": "FLAC",
+    "year": 2004,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2004 Can't Get My Head Around You",
+    "format": "FLAC",
+    "year": 2004,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2005 Can't Repeat",
+    "format": "FLAC",
+    "year": 2005,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2008 Hammerhead",
+    "format": "FLAC",
+    "year": 2008,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2008 You're Gonna Go Far, Kid",
+    "format": "FLAC",
+    "year": 2008,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "2015 Coming For You",
+    "format": "FLAC",
+    "year": 2015,
+    "category": "SINGLES"
   }
 ];
