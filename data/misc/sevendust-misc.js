fdcd5aad5cb659896cb704ae87cf7035426240ab
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 13:40:28 -0400

    Auto-export misc: sevendust

--- data/misc/sevendust-misc.js
+++ data/misc/sevendust-misc.js
@@ -2,13 +2,15 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "Sevendust - Southside Double-Wide Acoustic Live {CD, TV-6050-2} (2004)",
-    "version": "Official Release",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2004,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "2005-05-04 - Southside Double-Wide - Acoustic Live (2004) [DVD]",
-    "version": "Official Release",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 2005,
+    "category": "OTHER"
   }
 ];
