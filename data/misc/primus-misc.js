fefe5385000037599b3fc94fc7dcf7203d52543d
dudethatsclassic <natescottsmith@icloud.com>
2026-07-04 22:40:21 -0400

    Auto-export misc: primus

--- data/misc/primus-misc.js
+++ data/misc/primus-misc.js
@@ -3,95 +3,290 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Primus - Primus Sucks Demo Tape (1986) (24-96)",
     "version": "Cassette",
-    "format": "FLAC HD"
+    "format": "FLAC HD",
+    "year": 1986,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - Kiss My Dead Bloated Ass (Limp Donkey LD-1)",
     "version": "Vinyl",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "XX-XX-1988 - Sausage Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1988,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Well On the Way to Sucking - Live and Demos (1987-1988)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1987,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - Madhouse (KTS219) - Holland 93",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - Does Primus Really Fry? (1992) (TWR 04)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - Master of Puppies - Rome, Italy / 11-XX-1990",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - '91 Tour - Rome, Italy / 11-XX-1990",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Rarities Compilation",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Primus - Beaver Trappin' (1995)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - Freak Out {HAWK041} (1994)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - You Can't Party Without Me! {HR 5915-8} (1994)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - Love & Money (AKA Master of Puppies)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Primus - Acid Trip",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Primus - Videoplasty (VHS Rip) [WMV]",
-    "format": "WMV"
+    "format": "WMV",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Primus - Hallucino-Genetics (2004)",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 2004,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Primus - Blame It On The Fish (2006)",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 2006,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Suck On This (1989)",
+    "format": "FLAC",
+    "year": 1989,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Frizzle Fry (1990) (CAROL CD 1619)",
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Frizzle Fry (1990) [2002, RMed ∕ Bonus Track, PSR-0004-2]",
+    "format": "FLAC",
+    "year": 1990,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Making Plans For Nigel - Cheesy EP 1 (1992)",
+    "format": "FLAC",
+    "year": 1992,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Miscellaneous Debris (1992) (2018) (24/192)",
+    "format": "FLAC HD",
+    "year": 1992,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Pork Soda (1993) [2005, RE, INTD-92257]",
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Pork Soda (24/48)",
+    "format": "FLAC HD",
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Pork Soda (1993) (24/192)",
+    "format": "FLAC HD",
+    "year": 1993,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Wynona's Big Brown Beaver (CDS) (1995)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Tales from the Punchbowl (1995) [92553-2]",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Brown Album (1997) [INT2-90126]",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Rhinoplasty (1998)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Videoplasty (1998) (VHS)",
+    "format": "MKV",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Antipop (1999) [with \"The Heckler\" Hidden Track, 490414-2]",
+    "format": "FLAC",
+    "year": 1999,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Animals Should Not Try to Act like People (EP) (2003)",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - They Can't All Be Zingers (2006)",
+    "format": "FLAC",
+    "year": 2006,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - June 2010 Rehearsal (2010)",
+    "format": "FLAC",
+    "year": 2010,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Sailing The Seas Of Cheese (2013) (24/96)",
+    "format": "FLAC HD",
+    "year": 2013,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Sailing The Seas Of Cheese (Deluxe Edition) (2013) HDtracks (24/96)",
+    "format": "FLAC HD",
+    "year": 2013,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Primus & the Chocolate Factory With the Fungi Ensemble (2014) [ATO 0250]",
+    "format": "FLAC",
+    "year": 2014,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Green Naugahyde (2011) [ATO 0113]",
+    "format": "FLAC",
+    "year": 2011,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - Primus & the Chocolate Factory With the Fungi Ensemble (2014) (24/44.1)",
+    "format": "FLAC HD",
+    "year": 2014,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - The Desaturating Seven (2017) [ATO 0404]",
+    "format": "FLAC",
+    "year": 2017,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Primus - The Desaturating Seven (2017) (24/44.1)",
+    "format": "FLAC HD",
+    "year": 2017,
+    "category": "OFFICIAL RELEASES"
   }
 ];
