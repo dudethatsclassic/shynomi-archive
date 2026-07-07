4d41c01db21d47a4106ca22597d81bc2eeb161a8
dudethatsclassic <natescottsmith@icloud.com>
2026-07-06 21:06:31 -0400

    Auto-export misc: john-coltrane

--- data/misc/john-coltrane-misc.js
+++ data/misc/john-coltrane-misc.js
@@ -13,11 +13,5 @@ const ARTIST_MISC_DATA = [
     "format": "FLAC",
     "year": 1956,
     "category": "OTHER"
-  },
-  {
-    "type": "release",
-    "label": "62-63 - Coltrane Rarities",
-    "year": 1956,
-    "textFile": "MISC - 1956-62-63 - Coltrane Rarities.txt"
   }
 ];
