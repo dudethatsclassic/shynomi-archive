b9535188279de0a98e872bd67a44698e38d897ce
dudethatsclassic <natescottsmith@icloud.com>
2026-07-06 20:45:39 -0400

    Auto-export misc: death-cab-for-cutie

--- data/misc/death-cab-for-cutie-misc.js
+++ data/misc/death-cab-for-cutie-misc.js
@@ -1,22 +1,29 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Death Cab for Cutie - First Show - Acoustic at The Pacer House -  Bellingham WA 11-22-97",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Death Cab for Cutie - Live from Las Vegas At the Palms (2008-09-16)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Death Cab For Cutie - Transatlanticism Demos (2013) (24-88)",
-    "format": "FLAC HD"
+    "format": "FLAC HD",
+    "year": 2013,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Death Cab For Cutie & MagikMagik Orchestra - Live 2012 (2014) (24-96)",
-    "format": "VINYL"
+    "format": "VINYL",
+    "year": 2012,
+    "category": "OTHER"
   }
 ];
