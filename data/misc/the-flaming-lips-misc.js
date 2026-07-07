b80985fcdab397022aa4d882fe589119f7df9678
dudethatsclassic <natescottsmith@icloud.com>
2026-07-06 20:53:53 -0400

    Auto-export misc: the-flaming-lips

--- data/misc/the-flaming-lips-misc.js
+++ data/misc/the-flaming-lips-misc.js
@@ -1,240 +1,202 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - At War With The Mystics (Instrumentals)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Clouds Taste Metallic 1995 Unmastered Tracks",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Flaming Lips - Fearless Freaks Bootleg",
     "version": "Promo",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Hit To Death In The Future Head Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Flaming Lips - I Want To Become The Aurora Borealis [RTW-043]",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Flaming Lips, the - Trippy Lip Shtick {HR6053-0} (1995)",
     "version": "Silver CD",
     "format": "FLAC",
+    "year": 1995,
+    "category": "OTHER",
     "textFile": "MISC - Flaming Lips, the - Trippy Lip Shtick {HR6053-0} (1995) [Silver].txt"
   },
   {
     "type": "release",
     "label": "The Flaming Lips - Jonathon Ross On Radio 2",
     "version": "FM",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - March Of A 1000 Flaming Skeletons",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Mushroom Demos & Priest Demos",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Okie Noodling",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Soft Bulletin Companion_v1.shnf",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Soft Bulletin Companion_v2.shnf",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Sweet Jesus [TOR 16]",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Transmissions From The Satellite Heart 1993 Unmastered Tracks",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Waterbugs (In Studio Demo Tape)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - YBTPR (instrumental version)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Yoshimi Listening Party CD [SHN]",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Flaming Lips - Yoshimi Wins! - Live Radio Sessions SHN",
     "version": "Promo",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Flaming Lips - Zaireeka and 1997-98 Soft Bulletin Unmastered Tracks",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "I Am Not On Drugs...Yet (Compliation)",
-    "format": "DVD"
+    "format": "DVD",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Circuit 3 - Spontaneous Composition in Oklahoma Room #3 (in A minor)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Duck Dodgers - Theme Song",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Duck Dodgers Promo CD",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Imagene Peise Atlas Eets Christmas [RTW-044]",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zero to a Million (single track)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Yoshimi Battles The Pink Robots (Instrumental Version CD)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "BBC 6 Music Interviews and Sessions",
     "version": "WEB",
-    "format": "SHN"
+    "format": "SHN",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "The Flaming Lips - KCRW Studios - Morning Becomes Eclectic *Brave New World",
     "version": "FM",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Documentary - Boombox Experiment",
-    "format": "MOV"
+    "format": "MOV",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Documentary - Parking Lot Experiment",
-    "format": "MPG"
+    "format": "MPG",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Documentary - The Flaming Lips Have Landed A Documentary",
-    "format": "FLV"
-  },
-  {
-    "type": "release",
-    "label": "Duck Dodgers (Promo CD)"
-  },
-  {
-    "type": "release",
-    "label": "Flaming Lips - BBC 6 Music Interviews and Sessions",
-    "textFile": "MISC - Flaming Lips - BBC 6 Music Interviews and Sessions.txt"
-  },
-  {
-    "type": "release",
-    "label": "I am not on Drugs... Yet (Comp. DVD)",
-    "format": "DVD",
-    "textFile": "MISC - I am not on Drugs... Yet (Comp. DVD).txt"
-  },
-  {
-    "type": "release",
-    "label": "Imagene Peise Atlas Eets Christmas",
-    "format": "RTW-044",
-    "textFile": "MISC - Imagene Peise Atlas Eets Christmas [RTW-044].txt"
-  },
-  {
-    "type": "release",
-    "label": "KCRW's - Brave New World",
-    "format": "1996",
-    "textFile": "MISC - KCRW's - Brave New World [1996].txt"
-  },
-  {
-    "type": "release",
-    "label": "Mushroom & Priest Demos",
-    "textFile": "MISC - Mushroom & Priest Demos.txt"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - At War With The Mystics (Instrumentals) - (SBD)"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - Circuit 3 (Audio)",
-    "textFile": "MISC - The Flaming Lips - Circuit 3 (Audio).txt"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - Documentaries"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - I Want To Become The Aurora Borealis",
-    "format": "RTW-043"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - Sweet Jesus",
-    "textFile": "MISC - The Flaming Lips - Sweet Jesus.txt"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - Transmissions From The Satellite Heart Unmastered Tracks"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - YBTPR instrumental version"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - Yoshimi Listening Party CD",
-    "format": "SHN"
-  },
-  {
-    "type": "release",
-    "label": "The Flaming Lips - Zaireeka and Soft Bulletin Unmastered Tracks",
-    "textFile": "MISC - The Flaming Lips - Zaireeka and Soft Bulletin Unmastered Tracks.txt"
+    "format": "FLV",
+    "category": "OTHER"
   }
 ];
