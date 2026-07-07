a3e2af0b50cd05dbb30c88a0674ba7b3a8492d1b
dudethatsclassic <natescottsmith@icloud.com>
2026-07-06 20:56:36 -0400

    Auto-export misc: tool

--- data/misc/tool-misc.js
+++ data/misc/tool-misc.js
@@ -9,7 +9,7 @@ const ARTIST_MISC_DATA = [
     "state": "CA",
     "location": "Van Nuys, CA",
     "venue": "The Sylvia Massy Outtakes",
-    "notes": "Bottom \u00b7 M",
+    "notes": "Bottom · M",
     "version": "SBD",
     "format": "FLAC"
   },
@@ -30,19 +30,25 @@ const ARTIST_MISC_DATA = [
     "label": "XX-XX-1995 / D'Amour Demos"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Tool - Demo Tape 1991 (Authentic)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1991,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Tool - Demo Tape 1991 (Diff Version)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1991,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Tool - Demo Tape 1991 (Remastered)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1991,
+    "category": "OTHER"
   },
   {
     "type": "label",
@@ -52,121 +58,159 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Tool - 1993 Limited Edition Promo",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1993 Opium Den",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1993 Prison Sex Single (Germany)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1993 Sleep Forever",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1993 Tales from the Darkside",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1993 Tool's Power",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1993 Tooltime",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1994 Dragging Me Down",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1994 Sober - Tales from the Darkside (Dutch Edition)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1994 Sober 94",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1996 Dirge",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1996 Third Eye",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 1997 Monkey Wrench",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - Live Covers + Alternate Versions (Compilation)",
     "version": "Compilation",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - Prison Sex",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - Loving the Alien (2006)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2006,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 10,000 Nights {TSP 500-9-1} (2008)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - 2002-05-23 - Antwerp, Belgium - Sportpaleis (2002)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2002,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - Psychogenic Amnesia (2008)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2008,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tool - Live & Rare v1 1997-2006 (EC666) (24-48)",
     "version": "Compilation",
     "format": "FLAC 24",
+    "year": 1997,
+    "category": "OTHER",
     "textFile": "MISC - Tool - Live & Rare v1 1997-2006 (EC666) (24-48).txt"
   },
   {
@@ -174,6 +218,8 @@ const ARTIST_MISC_DATA = [
     "label": "Tool - Live & Rare v2 2006-2019 (EC666) (24-48)",
     "version": "Compilation",
     "format": "FLAC 24",
+    "year": 2006,
+    "category": "OTHER",
     "textFile": "MISC - Tool - Live & Rare v2 2006-2019 (EC666) (24-48).txt"
   },
   {
@@ -181,63 +227,78 @@ const ARTIST_MISC_DATA = [
     "label": "Tool - Live & Rare v3 2006-2019 (EC666) (24-48)",
     "version": "Compilation",
     "format": "FLAC 24",
+    "year": 2006,
+    "category": "OTHER",
     "textFile": "MISC - Tool - Live & Rare v3 2006-2019 (EC666) (24-48).txt"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Tool - Limited Editon Promo (Live) (1993)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "07-XX - Sylvia Massey Video",
-    "year": 1992,
-    "format": "DVD",
-    "textFile": "MISC - 1992-07-XX - Sylvia Massey Video [DVD].txt"
+    "label": "Tool - Opiate (1991) (2019) 24/96",
+    "format": "FLAC HD",
+    "year": 1991,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "XX-XX - Undertow Outtakes",
-    "year": 1992
+    "label": "Tool - Undertow (1993) (2019) 24/96",
+    "format": "FLAC HD",
+    "year": 1993,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "XX-XX - Van Nuys, CA - The Sylvia Massy Outtakes - Bottom at Sound City Studios - DAT(M)",
-    "year": 1992,
-    "format": "SBD",
-    "textFile": "MISC - 1992-XX-XX - Van Nuys, CA - The Sylvia Massy Outtakes - Bottom at Sound City Studios - DAT(M) [SBD].txt"
+    "label": "Tool - In Store Play Promo {ZP-17144-2} (1993)",
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "XX-XX - Van Nuys, CA - The Sylvia Massy Outtakes - Guitar Drum at Sound City Studios - DAT(M)",
-    "year": 1992,
-    "format": "SBD",
-    "textFile": "MISC - 1992-XX-XX - Van Nuys, CA - The Sylvia Massy Outtakes - Guitar Drum at Sound City Studios - DAT(M) [SBD].txt"
+    "label": "Tool - Selections from Ænima Promo {ZP-172102} (1996)",
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "XX-XX - Van Nuys, CA - The Sylvia Massy Outtakes - Sound FX at Sound City Studios - DAT(M)",
-    "year": 1992,
-    "format": "SBD",
-    "textFile": "MISC - 1992-XX-XX - Van Nuys, CA - The Sylvia Massy Outtakes - Sound FX at Sound City Studios - DAT(M) [SBD].txt"
+    "label": "Tool - Ænima (1996) (2019) 24/96",
+    "format": "FLAC HD",
+    "year": 1996,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "XX-XX - D'Amour Demos",
-    "year": 1995,
-    "textFile": "MISC - 1995-XX-XX - D'Amour Demos.txt"
+    "label": "Tool - Lateralus (2001) (2019) 24/96",
+    "format": "FLAC HD",
+    "year": 2001,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Silvers"
+    "label": "Tool - 10,000 Days (2006) (2019) HDTracks (24/96)",
+    "format": "FLAC HD",
+    "year": 2006,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Tool - Live Covers + Alternate Versions",
-    "textFile": "MISC - Tool - Live Covers + Alternate Versions.txt"
+    "label": "Tool - Fear Inoculum (2019) HDTracks (24/96)",
+    "format": "FLAC HD",
+    "year": 2019,
+    "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Tool Art"
+    "label": "Tool - Opiate² (Single) (2022)",
+    "format": "FLAC HD",
+    "year": 2022,
+    "category": "OFFICIAL RELEASES"
   }
 ];
