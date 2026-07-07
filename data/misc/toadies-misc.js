e8ea0afcc098cce1306be88ad793cc5b66c224e2
dudethatsclassic <natescottsmith@icloud.com>
2026-07-06 21:04:47 -0400

    Auto-export misc: toadies

--- data/misc/toadies-misc.js
+++ data/misc/toadies-misc.js
@@ -18,21 +18,5 @@ const ARTIST_MISC_DATA = [
     "format": "FLAC",
     "year": 2018,
     "category": "OTHER"
-  },
-  {
-    "type": "release",
-    "label": "Toadies - Best Of Toadies Live From Paradise (2002)",
-    "year": 2002,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Toadies - Live at Billy Bob's Texas (Deluxe Edition) (2018)",
-    "year": 2018,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Toadies - Slaphead"
   }
 ];
