1f1fb0f9373baeb7999ff1a42aabf9949aeefcd8
dudethatsclassic <natescottsmith@icloud.com>
2026-07-12 17:48:04 -0400

    Auto-export misc: r-e-m

--- data/misc/r-e-m-misc.js
+++ data/misc/r-e-m-misc.js
@@ -1,281 +1,5 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
-    "label": "1980-1981 - Garage Years"
-  },
-  {
-    "type": "release",
-    "label": "1981-1982-XX-XX - Down South",
-    "version": "Vinyl"
-  },
-  {
-    "type": "label",
-    "label": "XX-XX-1984 / Little America"
-  },
-  {
-    "type": "label",
-    "label": "REM - Cassette Set - April 1991 (Studio)"
-  },
-  {
-    "type": "label",
-    "label": "1988-1996 - Fanclub Singles V1"
-  },
-  {
-    "type": "label",
-    "label": "1997-2004 - Fanclub Singles V2"
-  },
-  {
-    "type": "release",
-    "label": "REM - Accelerating Backwards",
-    "version": "PRO"
-  },
-  {
-    "type": "label",
-    "label": "**OUTTAKES**"
-  },
-  {
-    "type": "release",
-    "label": "REM - Chronic Town - Rough Mixes",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REM - Chronic Murmurings (1981)",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REM - Murmur Studio Outtakes",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REM - Out of Time - Studio Outtakes (Edited)",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REM - New York - Bearsville Studios - Outtakes of Time (Studio)",
-    "version": "SBD"
-  },
-  {
-    "type": "label",
-    "label": "REM - Time of Outtakes (Studio)"
-  },
-  {
-    "type": "label",
-    "label": "**SILVERS**"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - 1984-XX-XX - Little America",
-    "version": "Silver CD"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - Acoustic '87 (1992) (NUN 004)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - Acoustic '87 (1992) (NUN 004) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - Freak Marmalade (1996) (KRCD 12)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - Freak Marmalade (1996) (KRCD 12) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - Hitting The Note (1994) (BKCD076)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - Hitting The Note (1994) (BKCD076) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - Losing My Religion (Vol. 3) (1993) (BAN-042-C)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - Losing My Religion (Vol. 3) (1993) (BAN-042-C) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - Rock Perugia (1991) (IST 11)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - Rock Perugia (1991) (IST 11) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - The Dream (1991) (RPCD 1028)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - The Dream (1991) (RPCD 1028) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - The Real Ultra Rare Tracks (NBN 1)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - The Real Ultra Rare Tracks (NBN 1) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - Unfettered (1998) (UFCD 1098)",
-    "version": "Silver CD",
-    "textFile": "MISC - R.E.M. - Unfettered (1998) (UFCD 1098) [Silver].txt"
-  },
-  {
-    "type": "release",
-    "label": "R.E.M. - Unreleased Live LP",
-    "version": "Silver CD"
-  },
-  {
-    "type": "label",
-    "label": "**DEMOS**"
-  },
-  {
-    "type": "label",
-    "label": "4-XX-1981 - Bonanza Demos"
-  },
-  {
-    "type": "label",
-    "label": "1981-1984 - Demos Volume 1"
-  },
-  {
-    "type": "label",
-    "label": "1988-1989 - Finest Worksongs Demos (Studio)"
-  },
-  {
-    "type": "label",
-    "label": "XX-XX-1986 / Athens, GA - Life's Rich Pageant & More [Demos]"
-  },
-  {
-    "type": "release",
-    "label": "REM - The Essential Demos Vol. 1",
-    "version": "Silver CD"
-  },
-  {
-    "type": "release",
-    "label": "REM - Document Demos (1987)",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REM - Elliot Mazer Demos (1983)",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REM - Rhythmic Studios Demos (1983)",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REM - Green Demos (1986) - Atlanta, GA / John Keane Studios",
-    "version": "SBD"
-  },
-  {
-    "type": "label",
-    "label": "XX-XX-1988 / The Complete Green Demos (Upgraded)"
-  },
-  {
-    "type": "label",
-    "label": "XX-XX-1986 / Atlanta, GA - John Keane Studios (Green Demos Upgrade)"
-  },
-  {
-    "type": "release",
-    "label": "REM - Up Demos (4/1998) - San Francisco, CA / Toast Studio (Remastered)",
-    "version": "SBD"
-  },
-  {
-    "type": "label",
-    "label": "**SINGLES**"
-  },
-  {
-    "type": "label",
-    "label": "R.E.M. - Everybody Hurts {9 40989-2} (1993)"
-  },
-  {
-    "type": "label",
-    "label": "R.E.M. - Everybody Hurts {9362-40867-9} (1993)"
-  },
-  {
-    "type": "label",
-    "label": "R.E.M. - Everybody Hurts {W0169CD1} (1993)"
-  },
-  {
-    "type": "label",
-    "label": "**VARIOUS**"
-  },
-  {
-    "type": "label",
-    "label": "Black Session (WCRS-2001-001)",
-    "textFile": "MISC - Black Session (WCRS-2001-001).txt"
-  },
-  {
-    "type": "label",
-    "label": "Cassette Set (1981)"
-  },
-  {
-    "type": "label",
-    "label": "Covering Them (PSCD-1260)",
-    "textFile": "MISC - Covering Them (PSCD-1260).txt"
-  },
-  {
-    "type": "label",
-    "label": "2-17-1983 / Reflection Studio"
-  },
-  {
-    "type": "label",
-    "label": "Half A World Away Part 1: 1988-1991 (MONG CD007A)",
-    "textFile": "MISC - Half A World Away Part One 1988-1991 (MONG CD007A).txt"
-  },
-  {
-    "type": "label",
-    "label": "Life's Rich Pageant and More (WAI 022)",
-    "textFile": "MISC - Lifes Rich Pageant And More (WAI 022).txt"
-  },
-  {
-    "type": "label",
-    "label": "Low"
-  },
-  {
-    "type": "label",
-    "label": "Monster - Completely Live Through the Years (1995-2008)"
-  },
-  {
-    "type": "label",
-    "label": "Murmur - Completely Live Through the Years (1981-2008)"
-  },
-  {
-    "type": "release",
-    "label": "Night Time Session (3-13-1991)",
-    "version": "SBD"
-  },
-  {
-    "type": "label",
-    "label": "Nightswimming - Greatest Hits Live"
-  },
-  {
-    "type": "release",
-    "label": "Poets of the Wheat (4-29-1989) Miami + Orlando, FL",
-    "version": "preFM"
-  },
-  {
-    "type": "label",
-    "label": "R.E.M.nants - The Complete Rarities Collection (12CD)"
-  },
-  {
-    "type": "label",
-    "label": "Ultra Rare Trax"
-  },
-  {
-    "type": "release",
-    "label": "Unreleased Live LP (1984)",
-    "version": "SBD"
-  },
-  {
-    "type": "release",
-    "label": "REMTV (6xDVD) 2014-1",
-    "version": "Official Release",
-    "textFile": "MISC - REMTV (6xDVD) 2014-1 [DVD].txt"
-  },
-  {
     "type": "release",
     "label": "04-XX - Bonzanza Demos",
     "year": 1981,
@@ -420,9 +144,9 @@ const ARTIST_MISC_DATA = [
   },
   {
     "type": "release",
-    "label": "R.E.M.nants The Complete Rarities Collection 1981\u20132004 (12 CDs)",
+    "label": "R.E.M.nants The Complete Rarities Collection 1981–2004 (12 CDs)",
     "category": "VARIOUS",
-    "textFile": "MISC - R.E.M.nants The Complete Rarities Collection 1981\u20132004 (12 CDs).txt"
+    "textFile": "MISC - R.E.M.nants The Complete Rarities Collection 1981–2004 (12 CDs).txt"
   },
   {
     "type": "release",
@@ -431,37 +155,363 @@ const ARTIST_MISC_DATA = [
   },
   {
     "type": "release",
-    "label": "1982-XX-XX - Down South",
+    "label": "R.E.M. - 1980-1981 - Garage Years",
+    "year": 1980,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - 1981-1982-XX-XX - Down South",
+    "version": "Vinyl",
     "year": 1981,
-    "format": "Vinyl"
+    "category": "OTHER"
+  },
+  {
+    "type": "label",
+    "label": "XX-XX-1984 / Little America"
   },
   {
     "type": "release",
-    "label": "02-17 - Reflection Studio",
+    "label": "R.E.M. Cassette Set - April 1991 (Studio)",
+    "year": 1991,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - 1988-1996 - Fanclub Singles V1",
+    "year": 1988,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - 1997-2004 - Fanclub Singles V2",
+    "year": 1997,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Accelerating Backwards",
+    "version": "PRO",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "**OUTTAKES**",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Chronic Town - Rough Mixes",
+    "version": "SBD",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Chronic Murmurings (1981)",
+    "version": "SBD",
+    "year": 1981,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Murmur Studio Outtakes",
+    "version": "SBD",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Out of Time - Studio Outtakes (Edited)",
+    "version": "SBD",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - New York - Bearsville Studios - Outtakes of Time (Studio)",
+    "version": "SBD",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Time of Outtakes (Studio)",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "**SILVERS**",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Little America (1984)",
+    "version": "Silver CD",
+    "year": 1984,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Rock Perugia {IST 11} (1991)",
+    "version": "Silver CD",
+    "year": 1991,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - The Dream {RPCD 1028} (1991)",
+    "version": "Silver CD",
+    "year": 1991,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Acoustic '87 {NUN 004} (1992)",
+    "version": "Silver CD",
+    "year": 1992,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Covering Them {PSCD-1260} (1992)",
+    "version": "Silver CD",
+    "year": 1992,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Losing My Religion (Vol. 3) {BAN-042-C} (1993)",
+    "version": "Silver CD",
+    "year": 1993,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Hitting The Note {BKCD076} (1994)",
+    "version": "Silver CD",
+    "year": 1994,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Freak Marmalade {KRCD 12} (1996)",
+    "version": "Silver CD",
+    "year": 1996,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - The Real Ultra Rare Tracks {NBN 1}",
+    "version": "Silver CD",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Unfettered {UFCD 1098} (1998)",
+    "version": "Silver CD",
+    "year": 1998,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Unreleased Live LP",
+    "version": "Silver CD",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "**DEMOS**",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - 4-XX-1981 - Bonanza Demos",
+    "year": 1981,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - 1981-1984 - Demos Volume 1",
+    "year": 1981,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - 1988-1989 - Finest Worksongs Demos (Studio)",
+    "year": 1988,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - XX-XX-1986 / Athens, GA - Life's Rich Pageant & More (Demos)",
+    "year": 1986,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - The Essential Demos Vol. 1",
+    "version": "Silver CD",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Document Demos (1987)",
+    "version": "SBD",
+    "year": 1987,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Elliot Mazer Demos (1983)",
+    "version": "SBD",
     "year": 1983,
-    "format": "FLAC"
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "1996 - Fanclub Singles V1",
-    "year": 1988
+    "label": "R.E.M. - Rhythmic Studios Demos (1983)",
+    "version": "SBD",
+    "year": 1983,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "2004 - Fanclub Singles v2",
-    "year": 1997
+    "label": "R.E.M. - Green Demos - Atlanta, GA - John Keane Studios (1986)",
+    "version": "SBD",
+    "year": 1986,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "R.E.M. - 1980-1981 - Garage Years"
+    "label": "R.E.M. - XX-XX-1988 / The Complete Green Demos (Upgraded)",
+    "year": 1988,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "R.E.M. - Accelerating Backwards DVD",
-    "format": "PRO"
+    "label": "R.E.M. - XX-XX-1986 / Atlanta, GA - John Keane Studios (Green Demos Upgrade)",
+    "year": 1986,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "R.E.M. - Cassette Set - April 1991 (Studio)"
+    "label": "R.E.M. - Up Demos - San Francisco, CA / Toast Studio - April 1998 (Remastered)",
+    "version": "SBD",
+    "year": 1998,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "**SINGLES**",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Everybody Hurts {9 40989-2} (1993)",
+    "year": 1993,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Everybody Hurts {9362-40867-9} (1993)",
+    "year": 1993,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Everybody Hurts {W0169CD1} (1993)",
+    "year": 1993,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "**VARIOUS**",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Black Session (WCRS-2001-001)",
+    "year": 2001,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M. - Cassette Set (1981)",
+    "year": 1981,
+    "category": "OTHER"
+  },
+  {
+    "type": "label",
+    "label": "2-17-1983 / Reflection Studio"
+  },
+  {
+    "type": "release",
+    "label": "Half A World Away Part 1: 1988-1991 (MONG CD007A)",
+    "year": 1988,
+    "category": "OTHER",
+    "textFile": "MISC - Half A World Away Part One 1988-1991 (MONG CD007A).txt"
+  },
+  {
+    "type": "release",
+    "label": "Life's Rich Pageant and More (WAI 022)",
+    "category": "OTHER",
+    "textFile": "MISC - Lifes Rich Pageant And More (WAI 022).txt"
+  },
+  {
+    "type": "release",
+    "label": "Low",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Monster - Completely Live Through the Years (1995-2008)",
+    "year": 1995,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Murmur - Completely Live Through the Years (1981-2008)",
+    "year": 1981,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Night Time Session (3-13-1991)",
+    "version": "SBD",
+    "year": 1991,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Nightswimming - Greatest Hits Live",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Poets of the Wheat (4-29-1989) Miami + Orlando, FL",
+    "version": "preFM",
+    "year": 1989,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "R.E.M.nants - The Complete Rarities Collection (12CD)",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Ultra Rare Trax",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Unreleased Live LP (1984)",
+    "version": "SBD",
+    "year": 1984,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "REMTV (6xDVD) 2014-1",
+    "year": 2014,
+    "category": "OTHER",
+    "textFile": "MISC - REMTV (6xDVD) 2014-1 [DVD].txt"
   }
 ];
