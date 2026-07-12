cc4ae95ae3a607a1ca81a739b0147913208189c6
dudethatsclassic <natescottsmith@icloud.com>
2026-07-12 09:23:27 -0400

    Auto-export misc: candlebox

--- data/misc/candlebox-misc.js
+++ data/misc/candlebox-misc.js
@@ -3,22 +3,29 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Into the Flame (1993 Demos) (KTS 355)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Both Ends Burning (Various Locations 1994 Tour)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Candlebox - Both Ends Burning",
-    "version": "Silver CD"
+    "label": "Candlebox - Far Behind (CDS) {W0258CD} [1993]",
+    "year": 1993,
+    "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Candlebox - Into The Flame",
-    "version": "Silver CD"
+    "label": "Candlebox - You (CDS) {9 18304-2} (1993)",
+    "format": "FLAC",
+    "year": 1993,
+    "category": "SINGLES"
   }
 ];
