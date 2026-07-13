1ece1f9abd348a52e7e9045154efcf1accd42c78
dudethatsclassic <natescottsmith@icloud.com>
2026-07-12 21:33:40 -0400

    Auto-export data: Michael Stipe

--- data/michael-stipe.js
+++ data/michael-stipe.js
@@ -21,8 +21,7 @@ const ARTIST_DATA = {
       "venue": "Webster Hall",
       "notes": "AKG Blueline's SE 300B · mike b.",
       "version": "AUD #1",
-      "format": "FLAC",
-      "textFile": "2014-12-29 - New, York, NY - Webster Hall [AUD1].txt"
+      "format": "FLAC"
     },
     {
       "year": 2014,
@@ -35,8 +34,7 @@ const ARTIST_DATA = {
       "venue": "Webster Hall",
       "notes": "iPhone · unk",
       "version": "AUD",
-      "format": "FLAC",
-      "textFile": "2014-12-30 - New, York, NY - Webster Hall (iPhone) [AUD].txt"
+      "format": "FLAC"
     },
     {
       "year": 2014,
@@ -62,8 +60,7 @@ const ARTIST_DATA = {
       "venue": "Fillmore",
       "notes": "CA-11 · leifh",
       "version": "AUD",
-      "format": "FLAC",
-      "textFile": "2015-12-29 - San Francisco, CA - Fillmore [AUD].txt"
+      "format": "FLAC"
     },
     {
       "year": 2015,
@@ -76,8 +73,7 @@ const ARTIST_DATA = {
       "venue": "Fillmore",
       "notes": "Schoeps CCM4s · unk · blg mastering",
       "version": "AUD",
-      "format": "FLAC",
-      "textFile": "2015-12-30 - San Francisco, CA - Fillmore [AUD].txt"
+      "format": "FLAC"
     }
   ]
 };
