64922797a1dab6e013b6eedea1f7a01fe11a838d
dudethatsclassic <natescottsmith@icloud.com>
2026-07-15 08:14:30 -0400

    Auto-export misc: jeff-tweedy

--- data/misc/jeff-tweedy-misc.js
+++ data/misc/jeff-tweedy-misc.js
@@ -2,12 +2,14 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "1997-2003 - The Collected Jeff Tweedy (Compilation)",
+    "format": "FLAC",
     "year": 1997,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Jeff Tweedy - Live Is The King {51497297633} (2021)",
+    "format": "FLAC HD",
     "year": 2021,
     "category": "OFFICIAL RELEASES",
     "textFile": "MISC - Jeff Tweedy - Live Is The King {51497297633} (2021) [FLAC HD].txt"
