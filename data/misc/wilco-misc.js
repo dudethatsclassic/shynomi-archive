397b30b08afdb144ba2b3758270ceebeb0f58b19
dudethatsclassic <natescottsmith@icloud.com>
2026-07-15 08:14:27 -0400

    Auto-export misc: wilco

--- data/misc/wilco-misc.js
+++ data/misc/wilco-misc.js
@@ -1104,6 +1104,7 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "2004 \"A Ghost Is Born\" Tour Compilation (4CD)",
+    "format": "FLAC",
     "year": 2004,
     "category": "OTHER",
     "textFile": "MISC - Wilco - The Collected Wilco 1995-2002 (4 CD) [FLAC].txt"
@@ -1111,74 +1112,87 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "Wilco - Alpha Romeo Tango (3CD)",
+    "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Summerteeth Demos",
+    "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Yankee Hotel Foxtrot - Super Deluxe (Version 2)",
+    "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Yankee Hotel Foxtrot - Complete Demos",
+    "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Yankee Hotel Foxtrot Demos (2002)",
+    "format": "FLAC",
     "year": 2002,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Yankee Hotel Foxtrot Engineer Demos (2002)",
+    "format": "FLAC",
     "year": 2002,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Billy Bragg and Wilco - Mermaid Avenue Demos - Chicago, IL 1998",
+    "format": "FLAC",
     "year": 1998,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - MSN On Air (1997)",
+    "format": "FLAC",
     "year": 1997,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Live (Orange) (2025)",
+    "format": "FLAC",
     "year": 2025,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Live (Blue) (2025)",
+    "format": "FLAC",
     "year": 2025,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - Live (Yellow) (2026)",
+    "format": "FLAC",
     "year": 2026,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - A Ghost Is Born Fundamentals (Tracked) (2025)",
+    "format": "FLAC",
     "year": 2025,
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Wilco - The Collected Wilco 1995-2002 (4 CD)",
+    "format": "FLAC",
     "year": 1995,
     "category": "OTHER"
   },
@@ -1186,6 +1200,7 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Wilco - Carol's Pub, Cruel Country, October 9, 2022 - 7 PM",
     "version": "Cassette",
+    "format": "FLAC",
     "year": 2022,
     "category": "OTHER"
   },
@@ -1193,6 +1208,7 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Wilco - Carol's Pub, Cruel Country, October 9, 2022 - 9 PM",
     "version": "Cassette",
+    "format": "FLAC",
     "year": 2022,
     "category": "OTHER"
   },
@@ -1200,6 +1216,7 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Wilco - Carol's Pub, Cruel Country, October 9, 2022 - 11 PM",
     "version": "Cassette",
+    "format": "FLAC",
     "year": 2022,
     "category": "OTHER"
   },
@@ -1207,6 +1224,7 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Wilco - Live at The Troubadour L.A. 1996 {R1-566246} (24-96)",
     "version": "Vinyl",
+    "format": "FLAC HD",
     "year": 1996,
     "category": "OTHER",
     "textFile": "MISC - Wilco - Live at The Troubadour L.A. 1996 {R1-566246} [Vinyl].txt"
@@ -1214,6 +1232,7 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "Wilco - The Wilco Book (2004)",
+    "format": "FLAC",
     "year": 2004,
     "category": "OFFICIAL RELEASES"
   },
@@ -1221,60 +1240,70 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Wilco - Speak Into The Rose (2011)",
     "version": "Vinyl",
+    "format": "FLAC",
     "year": 2011,
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
     "label": "Wilco - The Capitol Theater 2014 (2021)",
+    "format": "FLAC",
     "year": 2014,
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
     "label": "Wilco - A Ghost Is Born (075597899016) (Deluxe Edition) (2025)",
+    "format": "FLAC",
     "year": 2025,
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
     "label": "I Am Trying to Break Your Heart - A Film About Wilco (2002)",
+    "format": "DVD",
     "year": 2002,
     "category": "OFFICIAL RELEASES"
   },
   {
     "type": "release",
-    "label": "Wilco - Box Full of Letters (CDS) {W0306CD} (1995)",
+    "label": "Wilco - Box Full of Letters {W0306CD} (1995)",
+    "format": "FLAC",
     "year": 1995,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Wilco - Monday (CDS) {PRO-CD-8443} (1997)",
+    "label": "Wilco - Monday {PRO-CD-8443} (1997)",
+    "format": "FLAC",
     "year": 1997,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Wilco - Can't Stand It (CDS) {W475CD1} (1999)",
+    "label": "Wilco - Can't Stand It {W475CD1} (1999)",
+    "format": "FLAC",
     "year": 1999,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Wilco - Can't Stand It (CDS) {W475CD2} (1999)",
+    "label": "Wilco - Can't Stand It {W475CD2} (1999)",
+    "format": "FLAC",
     "year": 1999,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Wilco - A Shot in the Arm (CDS) {W496CD1} (1999)",
+    "label": "Wilco - A Shot in the Arm {W496CD1} (1999)",
+    "format": "FLAC",
     "year": 1999,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Wilco - War on War (CDS) {75597 96402-8} (2002)",
+    "label": "Wilco - War on War {75597 96402-8} (2002)",
+    "format": "FLAC",
     "year": 2002,
     "category": "SINGLES"
   }
