f57efabd48af42e95e15b58770a99d4defac1c06
dudethatsclassic <natescottsmith@icloud.com>
2026-07-15 08:14:36 -0400

    Auto-export misc: uncle-tupelo

--- data/misc/uncle-tupelo-misc.js
+++ data/misc/uncle-tupelo-misc.js
@@ -2,12 +2,14 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "Uncle Tupelo - Rare Essence (Various Dates)",
+    "format": "FLAC",
     "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Uncle Tupelo - Cover Story (1989-1993)",
     "version": "Silver CD",
+    "format": "FLAC",
     "year": 1989,
     "category": "OTHER"
   }
