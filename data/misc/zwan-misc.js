57a0087ff956f3c1cca9f845a731b1c1411226ae
dudethatsclassic <natescottsmith@icloud.com>
2026-07-21 22:27:39 -0400

    Auto-export misc: zwan

--- data/misc/zwan-misc.js
+++ data/misc/zwan-misc.js
@@ -1,77 +1,108 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwan - Mary Star of the Sea Rough Mixes (2002)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2002,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwan - Album Rough Mixes (Alt. Source)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Zwan - The Hideout Compilation Vol. 1 (9-9-2002 + 11-4-2002) Chicago, IL / The Hideout",
     "version": "SBD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2002,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Zwan - The Hideout (Revisited)",
     "version": "AUD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Zwan - The True Poets of Zwan",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwan - Going to Riverview - The Zwan Anthology (Complilation)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwan - Crusaders of Rock",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwan - Djali Zwan II",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Zwan - End of Zwan",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwan - True Poets of Zwan",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Zwan - For Your Love",
-    "version": "Official Release",
-    "format": "DVD"
+    "format": "DVD",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwan - For Your Love (DVD Audio Rip)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwanthology I - Fall 2001 Club Shows",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2001,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Zwanthology II - Winter 2001 Acoustic Shows",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2001,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Zwan - Honestly (W600CD, 9362 42609-2) 2003",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Zwan - Lyric (093624262428, Reprise Records – W607CD, Europe 2003)",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "SINGLES"
   }
 ];
