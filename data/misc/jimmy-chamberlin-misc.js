7585af786232cf71ea03316020da2a5440b61847
dudethatsclassic <natescottsmith@icloud.com>
2026-07-21 22:27:48 -0400

    Auto-export misc: jimmy-chamberlin

--- data/misc/jimmy-chamberlin-misc.js
+++ data/misc/jimmy-chamberlin-misc.js
@@ -1,17 +1,22 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "The Art of Drumming with Jimmy Chamberlin",
-    "format": "FLV"
+    "format": "FLV",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Last Hard Men (1997) [MP3]",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 1997,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Last Hard Men (2001 Re-issue Pressing)",
-    "format": "WAV"
+    "format": "WAV",
+    "year": 2001,
+    "category": "OTHER"
   }
 ];
