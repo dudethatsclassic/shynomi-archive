f99c768211ef3ebbae12b7cc736cc684d7dcf44c
dudethatsclassic <natescottsmith@icloud.com>
2026-07-21 21:51:19 -0400

    Auto-export misc: guster

--- data/misc/guster-misc.js
+++ data/misc/guster-misc.js
@@ -33,35 +33,5 @@ const ARTIST_MISC_DATA = [
     "format": "FLAC",
     "year": 2013,
     "category": "OTHER"
-  },
-  {
-    "type": "release",
-    "label": "Guster - 3 Live Sets (2014)",
-    "year": 2014,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - Live With The Redacted Symphony (2013)",
-    "year": 2013,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - MTV2 Album Covers (2004)",
-    "year": 2004,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - The Meowstro Sings- Guster's Keep It Together (2003)",
-    "year": 2003,
-    "format": "FLAC"
-  },
-  {
-    "type": "release",
-    "label": "Guster - The Pasty Tapes (1999)",
-    "year": 1999,
-    "format": "FLAC"
   }
 ];
