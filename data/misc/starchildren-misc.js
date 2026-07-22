251774bf0b54d3cbfcc0baed85a5504a7c7a9705
dudethatsclassic <natescottsmith@icloud.com>
2026-07-21 22:27:57 -0400

    Auto-export misc: starchildren

--- data/misc/starchildren-misc.js
+++ data/misc/starchildren-misc.js
@@ -3,6 +3,7 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Starchildren & Catherine - Delusions Of Candor (Flight Of The Eagle) - Chante Des Chansons Sur Les Filles! (Songs About Girls) (Single)",
     "version": "Official",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
