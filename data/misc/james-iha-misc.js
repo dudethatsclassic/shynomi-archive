98812b62eca2d510ab8e59df79c4c57b63ac5238
dudethatsclassic <natescottsmith@icloud.com>
2026-07-21 22:27:34 -0400

    Auto-export misc: james-iha

--- data/misc/james-iha-misc.js
+++ data/misc/james-iha-misc.js
@@ -3,36 +3,101 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "James Iha - Be Strong Now (1998-02-05 - Virgin VHS)",
     "version": "VHS",
-    "format": "DVD"
+    "format": "DVD",
+    "year": 1998,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Britta Phillips w. James Iha - Shine Your Light",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "James Iha - Linda Linda Linda (Original Soundtrack)",
-    "format": "M4A"
+    "format": "M4A",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "James Iha- Let It Come Down (Interview CD)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Bason - 100 Floors Above Me (Candy Says feat. James Iha)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Snaketrain - Demo Tape",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "1988-XX-XX - Snaketrain Demo Tape (Source #2)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1988,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "James Iha - Let It Come Down {7243 8 45411 2 5} (1998)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "James Iha - Let It Come Down {Virgin VJCP-25373} (1998) (Japan)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "James Iha - Jealousy {IHADJ 98} (1998)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "James Iha - Let It Come Down (Japan Remaster) {TOCP-11501} (2012)",
+    "format": "FLAC",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Various Artists - Because Of Winn-Dixie - Original Motion Picture Soundtrack {0 6700 30400 2 0} (2005)",
+    "format": "FLAC",
+    "year": 2005,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "James Iha - Speed of Love (Single) (2012) {TE2991}",
+    "format": "FLAC",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "James Iha - Look to the Sky {TE-288 2} (2012)",
+    "format": "FLAC",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "James Iha - Look To The Sky {EMI TOCP-71265) (Japan) (2012)",
+    "format": "FLAC",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
   }
 ];
