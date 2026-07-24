f35af051dbf022ae987c71fb18123be9b126f1b6
dudethatsclassic <natescottsmith@icloud.com>
2026-07-24 06:50:20 -0400

    Auto-export misc: the-melvins

--- data/misc/the-melvins-misc.js
+++ data/misc/the-melvins-misc.js
@@ -3,6 +3,8 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Melvins - Leech (1996) (EGG ONE)",
     "version": "Silver CD",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1996,
+    "category": "OTHER"
   }
 ];
