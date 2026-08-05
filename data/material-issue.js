c17fb72d6deb2fcb8925ddb7cc6367895e188eb1
dudethatsclassic <natescottsmith@icloud.com>
2026-08-05 08:07:12 -0400

    Auto-export data: Material Issue

---
+++ data/material-issue.js
@@ -0,0 +1,26 @@
+const ARTIST_DATA = {
+  "id": "material-issue",
+  "name": "Material Issue",
+  "drive": "F",
+  "showCount": 1,
+  "firstYear": 1990,
+  "lastYear": 1990,
+  "years": [
+    1990
+  ],
+  "shows": [
+    {
+      "year": 1990,
+      "month": 2,
+      "day": 11,
+      "dateStr": "2-11-1990",
+      "city": "Chicago",
+      "state": "IL",
+      "location": "Chicago, IL",
+      "venue": "Lounge Ax",
+      "notes": "ANA(M) · aadam jacobs",
+      "version": "AUD",
+      "format": "FLAC 24"
+    }
+  ]
+};
