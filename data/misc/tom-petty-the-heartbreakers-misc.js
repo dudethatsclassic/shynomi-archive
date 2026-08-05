ef34c8b0b652dc3acfea856a003343d523c77fe3
dudethatsclassic <natescottsmith@icloud.com>
2026-08-05 07:58:26 -0400

    Auto-export misc: tom-petty-the-heartbreakers

--- data/misc/tom-petty-the-heartbreakers-misc.js
+++ data/misc/tom-petty-the-heartbreakers-misc.js
@@ -2,37 +2,52 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "Running Down A Dream (DVD)",
-    "version": "Official"
+    "version": "Official",
+    "category": "OTHER"
   },
   {
-    "type": "label",
-    "label": "The Old Grey Whistle Test (June, 1978)"
+    "type": "release",
+    "label": "The Old Grey Whistle Test (June, 1978)",
+    "year": 1978,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tom Petty - VH1 Documentaries (1995)",
-    "version": "TV"
+    "version": "TV",
+    "year": 1995,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tom Petty - The Bootleg Series Vol. 1-3 (Live 1976-1981)",
-    "version": "Compilation"
+    "version": "Compilation",
+    "year": 1976,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tom Petty - The Bootleg Series Vol. 4-8 (Live 1982-1993)",
-    "version": "Compilation"
+    "version": "Compilation",
+    "year": 1982,
+    "category": "OTHER"
   },
   {
-    "type": "label",
-    "label": "XX-XX-1981 - \"Hard Promises\" Demos"
+    "type": "release",
+    "label": "XX-XX-1981 - \"Hard Promises\" Demos",
+    "year": 1981,
+    "category": "OTHER"
   },
   {
-    "type": "label",
-    "label": "Tom Petty - Somewhere You Feel Free (Documentary) (2021)"
+    "type": "release",
+    "label": "Tom Petty - Somewhere You Feel Free (Documentary) (2021)",
+    "year": 2021,
+    "category": "OTHER"
   },
   {
-    "type": "label",
-    "label": "Tom Petty - Insomnia (1981 Outtake)"
+    "type": "release",
+    "label": "Tom Petty - Insomnia (1981 Outtake)",
+    "year": 1981,
+    "category": "OTHER"
   }
 ];
