62ca2bfe63fe357a3c3aa7c90621559eda98085f
dudethatsclassic <natescottsmith@icloud.com>
2026-08-05 07:52:14 -0400

    Auto-export misc: toad-the-wet-sprocket

--- data/misc/toad-the-wet-sprocket-misc.js
+++ data/misc/toad-the-wet-sprocket-misc.js
@@ -1,17 +1,23 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Toad The Wet Sprocket - In the Round at Revolver (2013)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2013,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Toad the Wet Sprocket - Live in the West (2013)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2013,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Toad The Wet Sprocket - Architect Of The Ruin (2015)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 2015,
+    "category": "OTHER"
   }
 ];
