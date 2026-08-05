e5941e70394ea460399006608b9d984416eb76b6
dudethatsclassic <natescottsmith@icloud.com>
2026-08-05 07:48:02 -0400

    Auto-export data: Far

--- data/far.js
+++ data/far.js
@@ -18,7 +18,7 @@ const ARTIST_DATA = {
       "state": "CA",
       "location": "San Francisco, CA",
       "venue": "Slim's",
-      "notes": "unk \u00b7 neekrive",
+      "notes": "unk · neekrive",
       "version": "AUD",
       "format": "FLAC"
     }
