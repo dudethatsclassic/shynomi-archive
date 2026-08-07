d3701ea9adf024bbe4357ab9d38d24fe33dbbd43
dudethatsclassic <natescottsmith@icloud.com>
2026-08-07 13:13:10 -0400

    Auto-export misc: jeff-buckley

--- data/misc/jeff-buckley-misc.js
+++ data/misc/jeff-buckley-misc.js
@@ -63,10 +63,5 @@ const ARTIST_MISC_DATA = [
     "year": 1993,
     "category": "OTHER",
     "textFile": "MISC - 1993-09-XX  - Grace Outtakes.txt"
-  },
-  {
-    "type": "release",
-    "label": "Jeff Buckley Demos (1990-1997)",
-    "textFile": "MISC - Jeff Buckley Demos (1990-1997).txt"
   }
 ];
