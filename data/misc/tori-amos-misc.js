3b5dc34862cc1e8445b3ec861fceb15f606723eb
dudethatsclassic <natescottsmith@icloud.com>
2026-08-07 13:20:16 -0400

    Auto-export misc: tori-amos

--- data/misc/tori-amos-misc.js
+++ data/misc/tori-amos-misc.js
@@ -2,50 +2,62 @@ const ARTIST_MISC_DATA = [
   {
     "type": "release",
     "label": "Tori Stories Box Set (4CD) - Rare and Unreleaed + 2 Live Shows",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - Acoustic Tales (2002)",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "year": 2002,
+    "category": "OTHER"
   },
   {
-    "type": "label",
-    "label": "1991 Little Earthquakes Demos & Outtakes"
+    "type": "release",
+    "label": "1991 Little Earthquakes Demos & Outtakes",
+    "year": 1991,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - After Burn (HAWK036)",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - In My Head (DM92-96)",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - In The Breeze",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - A Candle In the Wind",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - Back to Basics",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - Rhapsody in Pink (AK 040/41)",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Tori Amos - Dreaming (2CD)",
-    "version": "Silver CD"
+    "version": "Silver CD",
+    "category": "OTHER"
   }
 ];
