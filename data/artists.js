fba2ab6382a3d5dd8a518a76d576b8696f012d30
dudethatsclassic <natescottsmith@icloud.com>
2026-08-17 08:08:21 -0400

    Auto-update artists index

--- data/artists.js
+++ data/artists.js
@@ -3040,7 +3040,7 @@ const ARTISTS = [
   {
     "id": "toad-the-wet-sprocket",
     "name": "Toad the Wet Sprocket",
-    "showCount": 343,
+    "showCount": 348,
     "firstYear": 1986,
     "lastYear": 2026
   },
