867690401344d9e7b04f80bc6e1663e022de6c30
dudethatsclassic <natescottsmith@icloud.com>
2026-08-21 12:26:49 -0400

    Auto-export misc: pat-wilson

--- data/misc/pat-wilson-misc.js
+++ data/misc/pat-wilson-misc.js
@@ -1,77 +1,96 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "1994 - The Suburban Advantage EP",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 1994,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Pat Wilson - Untitled Demos",
-    "format": "MP3"
+    "format": "MP3",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Patrick Wilson",
-    "format": "MP3"
+    "format": "MP3",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Punk-O-Rama (2005)",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 2005,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Southern Fried Swing - Free Flight Through the Universe of Sound",
-    "format": "MP3"
+    "format": "MP3",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Franklin Mint",
-    "format": "MP3"
+    "format": "MP3",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Bunny Album",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Feb 2002 (Demo)",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 2002,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Land, Air, Sea (NOS/TCG 5) (Original Version)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Land, Air, Sea",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Life Goes By (Single)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Natural",
-    "format": "MP3"
+    "format": "MP3",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Pinecone Album",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "The Special Goodness - Sept 2001 (Demo)",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 2001,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "thespecialgoodness.com",
-    "format": "MP3"
+    "format": "MP3",
+    "category": "OTHER"
   }
 ];
