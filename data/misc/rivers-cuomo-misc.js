ecadd6cebe4429f5c40894ab0302271f1e77e877
dudethatsclassic <natescottsmith@icloud.com>
2026-08-21 12:26:46 -0400

    Auto-export misc: rivers-cuomo

--- data/misc/rivers-cuomo-misc.js
+++ data/misc/rivers-cuomo-misc.js
@@ -1,27 +1,33 @@
 const ARTIST_MISC_DATA = [
   {
-    "type": "label",
+    "type": "release",
     "label": "Not Alone - Rivers Cuomo & Friends Live at Fingerprints",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Random Rivers (Songs, Demos, etc)",
-    "format": "FLAC + MP3"
+    "format": "FLAC + MP3",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Some Children See Him (Self-Released)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Various Social Streams (Periscope 2019, Ustream 2012) [MP3]",
-    "format": "MP3"
+    "format": "MP3",
+    "year": 2019,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Collaborations (Various)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "category": "OTHER"
   }
 ];
