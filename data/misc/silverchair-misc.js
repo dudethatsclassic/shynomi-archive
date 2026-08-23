fc8233f624330ff817b04c5fd4d5e5205d79866a
dudethatsclassic <natescottsmith@icloud.com>
2026-08-23 09:05:46 -0400

    Auto-export misc: silverchair

--- data/misc/silverchair-misc.js
+++ data/misc/silverchair-misc.js
@@ -3,143 +3,191 @@ const ARTIST_MISC_DATA = [
     "type": "release",
     "label": "Silverchair - Live at the Cabaret Metro (1996)",
     "version": "Silver CD",
-    "format": "SBD"
+    "format": "SBD",
+    "year": 1996,
+    "category": "OTHER"
   },
   {
     "type": "release",
     "label": "Silverchair - Acoustic (1995-12-15)",
     "version": "SBD",
-    "format": "FLAC"
-  },
-  {
-    "type": "label",
-    "label": "Silverchair - Les Enfants Terribles (1995)",
-    "format": "MP3"
-  },
-  {
-    "type": "label",
-    "label": "Silverchair - Across the Great Divide Tour (2007)",
-    "format": "DVD"
-  },
-  {
-    "type": "label",
-    "label": "Silverchair - Complete Videology (2001)",
-    "format": "DVD"
-  },
-  {
-    "type": "label",
-    "label": "Silverchair - The Creation Of Diorama (2002)",
-    "format": "AVI"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - Live at the Cabaret Metro {SAMP 3217} (1996)",
-    "year": 1996,
-    "version": "Silver CD"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - Across The Night {elevencd15} (2003)",
-    "year": 2003,
     "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - After All These Years {elevenCDPRO13} (2002)",
-    "year": 2002,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - Ana's Song (Open Fire) {667345 5} (1999)",
-    "year": 1999,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - Ana's Song {MATTCD086} Australia (1999)",
-    "year": 1999,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - Anthem For The Year 2000 {EPC 666836 2} EU (1999)",
-    "year": 1999,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - If You Keep Losing Sleep {ELEVENCD76}  (2007)",
-    "year": 2007,
-    "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - Israel's Son {MATTCD012} (1995) Australia",
     "year": 1995,
-    "format": "FLAC",
-    "category": "SINGLES"
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Silverchair - Luv Your Life {ELEVENCD10} (2002)",
+    "label": "Silverchair - Les Enfants Terribles (1995)",
+    "format": "MP3",
+    "year": 1995,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Across the Great Divide Tour (2007)",
+    "format": "DVD",
+    "year": 2007,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Complete Videology (2001)",
+    "format": "DVD",
+    "year": 2001,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - The Creation Of Diorama (2002)",
+    "format": "AVI",
     "year": 2002,
-    "format": "FLAC",
-    "category": "SINGLES"
+    "category": "OTHER"
   },
   {
     "type": "release",
-    "label": "Silverchair - Miss You Love {MATTCD091}  (1999) Australia",
-    "year": 1999,
+    "label": "Silverchair - Tomorrow EP {MATTCD001} (1994)",
     "format": "FLAC",
-    "category": "SINGLES"
-  },
-  {
-    "type": "release",
-    "label": "Silverchair - Paint Pastel Princess {SAMP2174} Australia (1999)",
-    "year": 1999,
-    "format": "FLAC",
+    "year": 1994,
     "category": "SINGLES"
   },
   {
     "type": "release",
     "label": "Silverchair - Shade {MATTCD014} (1995)",
+    "format": "FLAC",
     "year": 1995,
-    "format": "FLAC",
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Silverchair - Straight Lines {ELEVENCD62} (2007)",
-    "year": 2007,
+    "label": "Silverchair - Pure Massacre {MATTCD005} (1995)",
     "format": "FLAC",
+    "year": 1995,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Silverchair - The Greastest View {elevenCD6} (2002)",
-    "year": 2002,
+    "label": "Silverchair - Israel's Son {MATTCD012} (1995) Australia",
     "format": "FLAC",
+    "year": 1995,
     "category": "SINGLES"
   },
   {
     "type": "release",
-    "label": "Silverchair - Tomorrow EP {MATTCD001} (1994)",
-    "year": 1994,
+    "label": "Silverchair - Ana's Song {MATTCD086} Australia (1999)",
     "format": "FLAC",
+    "year": 1999,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Anthem For The Year 2000 {EPC 666836 2} EU (1999)",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Ana's Song (Open Fire) {667345 5} (1999)",
+    "format": "FLAC",
+    "year": 1999,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Miss You Love {MATTCD091}  (1999) Australia",
+    "format": "FLAC",
+    "year": 1999,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Paint Pastel Princess {SAMP2174} Australia (1999)",
+    "format": "FLAC",
+    "year": 1999,
     "category": "SINGLES"
   },
   {
     "type": "release",
     "label": "Silverchair - Without You {elevenCD9} (2002)",
-    "year": 2002,
     "format": "FLAC",
+    "year": 2002,
     "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - The Greastest View {elevenCD6} (2002)",
+    "format": "FLAC",
+    "year": 2002,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - After All These Years {elevenCDPRO13} (2002)",
+    "format": "FLAC",
+    "year": 2002,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Luv Your Life {ELEVENCD10} (2002)",
+    "format": "FLAC",
+    "year": 2002,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Across The Night {elevencd15} (2003)",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Straight Lines {ELEVENCD62} (2007)",
+    "format": "FLAC",
+    "year": 2007,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - If You Keep Losing Sleep {ELEVENCD76} (2007)",
+    "format": "FLAC",
+    "year": 2007,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - The Freak Box {MATTCD064} (1997)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Neon Ballroom (Limited Edition) {493309 9} (1999)",
+    "format": "FLAC",
+    "year": 1999,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - The Best Of, Volume 1 (Limited Edition) {2-501300} (2000)",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - The Diorama Box (Limited Edition Boxset) {ELEVENCD14} (2002)",
+    "format": "FLAC",
+    "year": 2002,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Silverchair - Live From Faraway Stables {ELEVENCD19} (2003)",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OFFICIAL RELEASES"
   }
 ];
