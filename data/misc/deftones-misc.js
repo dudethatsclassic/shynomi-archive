ba289e457881682a9d9d2712fd0795421c4d3c7a
dudethatsclassic <natescottsmith@icloud.com>
2026-08-29 12:23:51 -0400

    Auto-export misc: deftones

--- data/misc/deftones-misc.js
+++ data/misc/deftones-misc.js
@@ -4,6 +4,8 @@ const ARTIST_MISC_DATA = [
     "label": "Deftones - Deftones (1992)",
     "version": "Cassette",
     "format": "FLAC",
+    "year": 1992,
+    "category": "OTHER",
     "textFile": "MISC - Deftones - Deftones (1992) [FLAC].txt"
   },
   {
@@ -11,28 +13,177 @@ const ARTIST_MISC_DATA = [
     "label": "Absolutely Live - High Voltage (California 1997 & Leeds 2000)",
     "version": "Silver CD",
     "format": "FLAC",
+    "year": 1997,
+    "category": "OTHER",
     "textFile": "MISC - Deftones - Absolutely Live - High Voltage (California 1997 & Leeds 2000) [Silver].txt"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Deftones - Engine Number Nine - 7 Words (Demo Tape) (1993)",
     "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER",
     "textFile": "MISC - Deftones - Engine Number Nine - 7 Words (Demo Tape) (1993) [FLAC].txt"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Deftones - (Like) Linus (1993)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1993,
+    "category": "OTHER"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Deftones - Root - Nosebleed (Advance Cassette) (1994)",
     "format": "FLAC",
+    "year": 1994,
+    "category": "OTHER",
     "textFile": "MISC - Deftones - Root - Nosebleed (Advance Cassette) (1994) [FLAC].txt"
   },
   {
-    "type": "label",
+    "type": "release",
     "label": "Deftones - Live At Dynamo Open Air (1998-2019) {0555020961} (2019)",
-    "format": "FLAC"
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Saturday Night Wrist (Bob Ezrin Produced Demos, Outtakes)",
+    "format": "FLAC",
+    "category": "OTHER"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Adrenaline {9 46054-2} (1995)",
+    "format": "FLAC",
+    "year": 1995,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Around The Fur {9362-46810-2} (1997)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Live {9362-46890-2} (1998)",
+    "format": "FLAC",
+    "year": 1998,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - White Pony {WPCR-10689} (2000) (Japan)",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - White Pony {Maverick 947705-2 RED LTD Bonus US} (2000)",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Back To School (Mini Maggit) {9362-48082-2} (2001)",
+    "format": "FLAC",
+    "year": 2001,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Deftones {US 48350-2} (2003)",
+    "format": "FLAC",
+    "year": 2003,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Saturday Night Wrist {US 43239-2} (2006)",
+    "format": "FLAC",
+    "year": 2006,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Diamond Eyes {511922-1} (2010)",
+    "format": "FLAC",
+    "year": 2010,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Koi No Yokan {093624947707} (2012)",
+    "format": "FLAC",
+    "year": 2012,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Gore {550092-2} (2016)",
+    "format": "FLAC",
+    "year": 2016,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Ohms {093624892137} (2020)",
+    "format": "FLAC",
+    "year": 2020,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - White Pony 20th Anniversary Deluxe Edition {093624893080} (2020)",
+    "format": "FLAC",
+    "year": 2020,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - private music {093624832249} (2025)",
+    "format": "FLAC",
+    "year": 2025,
+    "category": "OFFICIAL RELEASES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - 7 Words {PRO-CD-8450-R} (1994)",
+    "format": "FLAC",
+    "year": 1994,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - My Own Summer (Shove It) CD1 {WO432CDX} (1997)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Be Quiet and Drive {WO445CDX} (CD1) (1997)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Be Quiet and Drive {WO445CDX} (CD2) (1997)",
+    "format": "FLAC",
+    "year": 1997,
+    "category": "SINGLES"
+  },
+  {
+    "type": "release",
+    "label": "Deftones - Change (In the House of Flies) {44921 2} (2000)",
+    "format": "FLAC",
+    "year": 2000,
+    "category": "SINGLES"
   }
 ];
